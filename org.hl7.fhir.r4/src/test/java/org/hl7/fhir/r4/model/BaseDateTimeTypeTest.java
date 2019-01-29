e289ad1a868a675495a7f9d948260a6c109d5b31
Grahame Grieve <grahameg@gmail.com>
2019-01-29 14:04:36 +1100

    finish getting tests passed

--- org.hl7.fhir.r4/src/test/java/org/hl7/fhir/r4/model/BaseDateTimeTypeTest.java
+++ org.hl7.fhir.r4/src/test/java/org/hl7/fhir/r4/model/BaseDateTimeTypeTest.java
@@ -31,22 +31,32 @@ public class BaseDateTimeTypeTest {
         assertFalse(compareDateTimes("2001-01-02", "2001-01-03"));
         // Different instant - Same timezone
         assertFalse(compareDateTimes("2001-01-02T11:22:33.444Z", "2001-01-02T11:22:33.445Z"));
+        assertFalse(compareDateTimes("2001-01-02T11:22:33.445Z", "2001-01-02T11:22:33.444Z"));
 
         // FHIRPath tests:
         assertFalse(compareDateTimes("1974-12-25", "1974-12-25T12:34:00+10:00"));
+        assertFalse(compareDateTimes("1974-12-25T12:34:00+10:00", "1974-12-25"));
         assertFalse(compareDateTimes("1974-12-25", "1974-12-25T12:34:00-10:00"));
+        assertFalse(compareDateTimes("1974-12-25T12:34:00-10:00", "1974-12-25"));
         assertFalse(compareDateTimes("1974-12-25", "1974-12-25T12:34:00Z"));
+        assertFalse(compareDateTimes("1974-12-25T12:34:00Z", "1974-12-25"));
         assertFalse(compareDateTimes("2012-04-15", "2012-04-16"));
+        assertFalse(compareDateTimes("2012-04-16", "2012-04-15"));
         assertFalse(compareDateTimes("2012-04-15T15:00:00", "2012-04-15T10:00:00"));
+        assertFalse(compareDateTimes("2012-04-15T10:00:00", "2012-04-15T15:00:00"));
         assertFalse(compareDateTimes("2017-11-05T01:30:00.0-04:00", "2017-11-05T01:15:00.0-05:00"));
+        assertFalse(compareDateTimes("2017-11-05T01:15:00.0-05:00", "2017-11-05T01:30:00.0-04:00"));
         assertNull(compareDateTimes("1974-12-25", "1974-12-25T12:34:00"));
-        assertNull(compareDateTimes("2012-04-15", "2012-04-15T10:00:00"));
+        assertNull(compareDateTimes("1974-12-25T12:34:00", "1974-12-25"));
+        assertNull(compareDateTimes("2012-04-15T10:00:00", "2012-04-15"));
         assertNull(compareDateTimes("2012-04-15T15:00:00Z", "2012-04-15T10:00:00"));
+        assertNull(compareDateTimes("2012-04-15T10:00:00", "2012-04-15T15:00:00Z"));
         assertTrue(compareDateTimes("1974-12-25", "1974-12-25"));
         assertTrue(compareDateTimes("2012-04-15", "2012-04-15"));
         assertTrue(compareDateTimes("2012-04-15T15:00:00+02:00", "2012-04-15T16:00:00+03:00"));
-        assertTrue(compareDateTimes("2012-04-15T15:00:00+02:00", "2012-04-15T16:00:00+03:00"));
+        assertTrue(compareDateTimes("2012-04-15T16:00:00+03:00", "2012-04-15T15:00:00+02:00"));
         assertTrue(compareDateTimes("2017-11-05T01:30:00.0-04:00", "2017-11-05T00:30:00.0-05:00"));
+        assertTrue(compareDateTimes("2017-11-05T00:30:00.0-05:00", "2017-11-05T01:30:00.0-04:00"));
 
         assertFalse(compareDateTimes("2016-12-02T13:00:00Z", "2016-11-02T10:00:00")); // no timezone, but cannot be the same time
         assertNull(compareDateTimes("2016-12-02T13:00:00Z", "2016-12-02T10:00:00")); // no timezone, might be the same time
