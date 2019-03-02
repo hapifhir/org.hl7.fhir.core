c5f2e98c867047d5f07fd9deadb93f94fd27a2dc
Grahame Grieve <grahameg@gmail.com>
2019-03-03 06:45:15 +1100

    move openApi convertor code to .core

---
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/openapi/ResponsesWriter.java
@@ -0,0 +1,19 @@
+package org.hl7.fhir.r5.openapi;
+
+import com.google.gson.JsonObject;
+
+public class ResponsesWriter extends BaseWriter {
+
+  public ResponsesWriter(JsonObject object) {
+    super(object);
+  }
+
+  public ResponseObjectWriter defaultResponse() {
+    return new ResponseObjectWriter(ensureObject("default"));
+  }
+  
+  public ResponseObjectWriter httpResponse(String code) {
+    return new ResponseObjectWriter(ensureMapObject(code));
+  }
+  
+}
