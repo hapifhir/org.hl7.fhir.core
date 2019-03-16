dd9528cc7d8592a7a2ca652055cdb03de37ab4e5
Grahame Grieve <grahameg@gmail.com>
2019-03-17 09:23:24 +1100

    Release new version

--- release.bat
+++ release.bat
@@ -1,14 +1,15 @@
 REM replace versions before running
 REM make sure you are committed
 
+echo
+
 echo ===============================================================
-echo upgrade and release fhir.core from 3.7.15-SNAPSHOT to 3.7.16-SNAPSHOT
+echo upgrade and release fhir.core from 3.7.16-SNAPSHOT to 3.7.17-SNAPSHOT
 echo ===============================================================
 pause
 
-call mvn versions:set -DnewVersion=3.7.16-SNAPSHOT
+call mvn versions:set -DnewVersion=3.7.17-SNAPSHOT
 
-echo
 echo ===============================================================
 echo upgraded version number using maven
 echo next: do git commit / push
@@ -18,17 +19,15 @@ pause
 call git commit -a -m "Release new version"
 call git push origin master
 
-echo
 echo ===============================================================
 echo done git commit / push
 echo next: replace references in java code + ivy
 echo ===============================================================
 pause
 
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --find "3.7.15-SNAPSHOT" --replace "3.7.16-SNAPSHOT"
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --excludeFileMask "*.dll, *.exe" --find "3.7.15-SNAPSHOT" --replace "3.7.16-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --find "3.7.16-SNAPSHOT" --replace "3.7.17-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --excludeFileMask "*.dll, *.exe" --find "3.7.16-SNAPSHOT" --replace "3.7.17-SNAPSHOT"
 
-echo
 echo ===============================================================
 echo done replacing references
 echo next: do maven release
@@ -37,7 +36,6 @@ pause
 
 call mvn deploy
 
-echo
 echo ===============================================================
 echo all done
 echo ===============================================================
