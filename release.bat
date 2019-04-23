f3e32554cf62513296f6116373bf73fb7116e9e1
Grahame Grieve <grahameg@gmail.com>
2019-04-24 06:19:47 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -4,15 +4,15 @@ REM make sure you are committed
 echo
 
 echo ===============================================================
-echo upgrade and release fhir.core from 3.7.26-SNAPSHOT to 3.7.27-SNAPSHOT
+echo upgrade and release fhir.core from 3.7.27-SNAPSHOT to 3.7.28-SNAPSHOT
 echo ===============================================================
 pause
 
-call mvn versions:set -DnewVersion=3.7.27-SNAPSHOT
+call mvn versions:set -DnewVersion=3.7.28-SNAPSHOT
 call git commit -a -m "Release new version"
 call git push origin master
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --find "3.7.26-SNAPSHOT" --replace "3.7.27-SNAPSHOT"
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --excludeFileMask "*.dll, *.exe" --find "3.7.26-SNAPSHOT" --replace "3.7.27-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --excludeFileMask "*.dll, *.exe" --includeSubDirectories --find "3.7.27-SNAPSHOT" --replace "3.7.28-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --excludeFileMask "*.dll, *.exe" --find "3.7.27-SNAPSHOT" --replace "3.7.28-SNAPSHOT"
 call mvn deploy
 
 echo ===============================================================
