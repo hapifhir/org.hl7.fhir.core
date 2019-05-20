05c5069c4c47e791644a91b52007c27587b234e0
Grahame Grieve <grahameg@gmail.com>
2019-05-20 16:54:07 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -4,18 +4,18 @@ REM make sure you are committed
 echo
 
 echo ===============================================================
-echo upgrade and release fhir.core from 3.7.32-SNAPSHOT to 3.7.33-SNAPSHOT
+echo upgrade and release fhir.core from 3.7.33-SNAPSHOT to 3.7.34-SNAPSHOT
 echo ===============================================================
 pause
 
-call mvn versions:set -DnewVersion=3.7.33-SNAPSHOT
+call mvn versions:set -DnewVersion=3.7.34-SNAPSHOT
 call git commit -a -m "Release new version"
 call git push origin master
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --includeSubDirectories --find "3.7.32-SNAPSHOT" --replace "3.7.33-SNAPSHOT"
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\fhir-ig-publisher" --fileMask "*.xml" --includeSubDirectories --find "3.7.32-SNAPSHOT" --replace "3.7.33-SNAPSHOT"
-call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "3.7.32-SNAPSHOT" --replace "3.7.33-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.java" --includeSubDirectories --find "3.7.33-SNAPSHOT" --replace "3.7.34-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\fhir-ig-publisher" --fileMask "*.xml" --includeSubDirectories --find "3.7.33-SNAPSHOT" --replace "3.7.34-SNAPSHOT"
+call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xml" --find "3.7.33-SNAPSHOT" --replace "3.7.34-SNAPSHOT"
 call mvn deploy
-call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java core" -m "New Java Core v3.7.33-SNAPSHOT released." --config-file zuliprc
+call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java core" -m "New Java Core v3.7.34-SNAPSHOT released." --config-file zuliprc
 
 echo ===============================================================
 echo all done
