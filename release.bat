11149af19c44c75b66c4a86112cf25644373fd75
Grahame Grieve <grahameg@gmail.com>
2019-09-15 02:35:50 +1000

    Release new version

--- release.bat
+++ release.bat
@@ -18,7 +18,11 @@ call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\build" --fileMask "*.xm
 call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\fhir-ig-publisher" --fileMask "*.xml" --find "%oldver%-SNAPSHOT" --replace "%newver%-SNAPSHOT"
 call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\latest-ig-publisher" --fileMask "*.html" --find "%oldver%" --replace "%newver%"
 call "C:\tools\fnr.exe" --cl --dir "C:\work\org.hl7.fhir\latest-ig-publisher" --fileMask "*.json" --find "%oldver%" --replace "%newver%"
-call mvn deploy
+call mvn deploy -Dmaven.test.redirectTestOutputToFile=false -DdeployAtEnd=true 
+IF %ERRORLEVEL% NEQ 0 ( 
+  GOTO DONE
+)
+
 copy org.hl7.fhir.validation.cli\target\org.hl7.fhir.validation.cli-%newver%-SNAPSHOT.jar ..\latest-ig-publisher\org.hl7.fhir.validator.jar
 cd ..\latest-ig-publisher
 call git commit -a -m "Release new version %newver%-SNAPSHOT"
@@ -26,6 +30,7 @@ call git push origin master
 cd ..\org.hl7.fhir.core
 call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java core" -m "New Java Core v%newver%-SNAPSHOT released. New Validator at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=ca.uhn.hapi.fhir&a=org.hl7.fhir.validation.cli&v=%newver%-SNAPSHOT&e=jar, and also deployed at https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar" --config-file zuliprc
 
+:DONE
 echo ===============================================================
 echo all done
 echo ===============================================================
