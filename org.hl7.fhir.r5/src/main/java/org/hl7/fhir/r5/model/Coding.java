293f314ba0c7d953570b2fc37f18450befcc7b2f
Grahame Grieve <grahameg@gmail.com>
2019-11-29 13:25:41 +1100

    add Coding.toString()

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Coding.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Coding.java
@@ -555,8 +555,17 @@ public class Coding extends Type implements IBaseCoding, ICompositeType, ICoding
       public boolean is(String system, String code) {
         return hasSystem() && hasCode() &&  this.getSystem().equals(system) && this.getCode().equals(code);
       }
-      
 
+      public String toString() {
+        String base = getSystem();
+        if (hasVersion())
+          base = base+"|"+getVersion();
+        base = base + "#"+getCode();
+        if (hasDisplay())
+          base = base+": "+getDisplay();
+        return base;
+        
+      }
 // end addition
 
 }
