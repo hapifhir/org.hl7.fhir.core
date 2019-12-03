053f86d60d85297748bf11e2fe675810d1228d78
Grahame Grieve <grahameg@gmail.com>
2019-12-04 05:59:22 +1100

    remove spurious debugging code.

--- release.bat
+++ release.bat
@@ -12,6 +12,9 @@ echo check versions and make sure committed...
 pause
 
 call mvn versions:set -DnewVersion=%newver%-SNAPSHOT
+
+pause
+
 call git commit -a -m "Release new version"
 call git push origin master
 call "C:\tools\fnr.exe" -dir "C:\work\org.hl7.fhir\build" -fileMask "*.xml" -find "%oldver%-SNAPSHOT" -replace "%newver%-SNAPSHOT" -count 8
