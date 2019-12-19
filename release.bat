58f4264a655e90b1bf8ea6e0714d0c67c51acf36
Grahame Grieve <grahameg@gmail.com>
2019-12-19 12:57:52 +1100

    validate fixed/pattern types

--- release.bat
+++ release.bat
@@ -32,7 +32,7 @@ call git commit -a -m "Release new version %newver%-SNAPSHOT"
 call git push origin master
 cd ..\org.hl7.fhir.core
 call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java core" -m "New Java Core v%newver%-SNAPSHOT released. New Validator at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=ca.uhn.hapi.fhir&a=org.hl7.fhir.validation.cli&v=%newver%-SNAPSHOT&e=jar, and also deployed at https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar" --config-file zuliprc
-call python c:\tools\zulip-api\zulip\zulip\send.py --stream tooling/releases --subject "Validator" -m "New Java Validator released (v%newver%) at https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar (and, for maven users, https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=ca.uhn.hapi.fhir&a=org.hl7.fhir.validation.cli&v=%newver%-SNAPSHOT&e=jar" --config-file zuliprc
+call python c:\tools\zulip-api\zulip\zulip\send.py --stream tooling/releases --subject "Validator" -m "New Validator @ https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar (v%newver%)" --config-file zuliprc
 
 :DONE
 echo ===============================================================
