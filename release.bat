7bc95ade0921129b70acad025c3a1e7247de7cf3
Grahame Grieve <grahameg@gmail.com>
2019-12-21 07:07:27 +1100

    fix to compile

--- release.bat
+++ release.bat
@@ -28,8 +28,6 @@ call git commit -a -m "Release new version %newver%-SNAPSHOT"
 call git push origin master
 cd ..\org.hl7.fhir.core
 
-call mvn eclipse:eclipse
-
 call python c:\tools\zulip-api\zulip\zulip\send.py --stream committers/notification --subject "java core" -m "New Java Core v%newver%-SNAPSHOT released. New Validator at https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=ca.uhn.hapi.fhir&a=org.hl7.fhir.validation.cli&v=%newver%-SNAPSHOT&e=jar, and also deployed at https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar" --config-file zuliprc
 call python c:\tools\zulip-api\zulip\zulip\send.py --stream tooling/releases --subject "Validator" -m "New Validator @ https://fhir.github.io/latest-ig-publisher/org.hl7.fhir.validator.jar (v%newver%)" --config-file zuliprc
 
