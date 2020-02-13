490aef6f9b499f4bfb9a71f55425ffc4063ae45d
Ken Stevens <khstevens@gmail.com>
2020-02-12 20:39:37 -0500

    fixed Meta.getSecurity(String,String) bug

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Meta.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Meta.java
@@ -671,7 +671,7 @@ public class Meta extends DataType implements IBaseMetaType {
    * <code>null</code> if none 
    */
   public Coding getSecurity(String theSystem, String theCode) { 
-    for (Coding next : getTag()) {
+    for (Coding next : getSecurity()) {
       if (ca.uhn.fhir.util.ObjectUtil.equals(next.getSystem(), theSystem) && ca.uhn.fhir.util.ObjectUtil.equals(next.getCode(), theCode)) { 
         return next; 
       } 
