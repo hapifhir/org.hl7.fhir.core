82f927ede3f31cd41f1eabfea8022a8df18b56bb
Grahame Grieve <grahameg@gmail.com>
2020-03-17 11:15:53 +1100

    add cross version snapshot generation tests

---
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/conversion/tests/CrossVersionLibraryTests.java
@@ -0,0 +1,12 @@
+package org.hl7.fhir.conversion.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+   SnapShotGenerationTestsX.class})
+public class CrossVersionLibraryTests {
+
+}
