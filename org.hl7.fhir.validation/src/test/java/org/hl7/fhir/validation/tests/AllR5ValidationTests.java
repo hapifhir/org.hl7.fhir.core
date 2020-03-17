335b5587b3aa0fef3643f8d05236282f86df4536
Grahame Grieve <grahameg@gmail.com>
2020-03-17 11:15:12 +1100

    add cross version snapshot tests

--- org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/AllR5ValidationTests.java
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/AllR5ValidationTests.java
@@ -1,11 +1,13 @@
 package org.hl7.fhir.validation.tests;
 
+import org.hl7.fhir.conversion.tests.SnapShotGenerationTestsX;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
+  SnapShotGenerationTestsX.class,
   ValidationTestSuite.class, 
   ValidationEngineTests.class,
   JsonSchemaTests.class,
