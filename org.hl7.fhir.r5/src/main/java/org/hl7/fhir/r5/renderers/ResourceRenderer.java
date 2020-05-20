da626616f60bb940baea743bc30d9a24233fe50d
Grahame Grieve <grahameg@gmail.com>
2020-05-20 21:43:39 +1000

    enhance API for external use

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/renderers/ResourceRenderer.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/renderers/ResourceRenderer.java
@@ -48,6 +48,12 @@ public abstract class ResourceRenderer extends DataRenderer {
     this.rcontext = rcontext;
   }
 
+  public XhtmlNode build(DomainResource dr) throws FHIRFormatError, DefinitionException, FHIRException, IOException, EOperationOutcome {
+    XhtmlNode x = new XhtmlNode(NodeType.Element, "div");
+    render(x, dr);
+    return x;
+    
+  }
   /**
    * given a resource, update it's narrative with the best rendering available
    * 
