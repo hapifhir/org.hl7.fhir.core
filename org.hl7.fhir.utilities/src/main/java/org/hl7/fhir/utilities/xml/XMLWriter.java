8b7eb2f1e0b4602e30e18e03a10869255016bbe1
Oliver Egger <oliver.egger@ahdis.ch>
2020-06-10 13:30:04 +0200

    revert, but escape only < '  ' char

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xml/XMLWriter.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xml/XMLWriter.java
@@ -246,7 +246,7 @@ public class XMLWriter extends OutputStreamWriter implements IXMLWriter {
 					write(element[0]);
 					write("=\"");
 					if (element[1] != null)
-						write(XMLUtil.escapeXML(element[1], charset, false));
+						write(xmlEscape(element[1]));
 					write("\"");
 				}
 			}
@@ -254,7 +254,20 @@ public class XMLWriter extends OutputStreamWriter implements IXMLWriter {
 		return col;
 	}
 
-	/* (non-Javadoc)
+	 protected String xmlEscape(String s) {
+     StringBuilder b = new StringBuilder();
+     for (char c : s.toCharArray()) {
+       if (c < ' ') {
+         b.append("&#x");
+         b.append(Integer.toHexString(c).toUpperCase());
+         b.append(";");
+       } else
+         b.append(c);
+     }
+     return b.toString();
+   }
+
+	 /* (non-Javadoc)
 	 * @see org.eclipse.ohf.utilities.xml.IXMLWriter#attribute(java.lang.String, java.lang.String, java.lang.String, boolean)
 	 */
 	@Override
