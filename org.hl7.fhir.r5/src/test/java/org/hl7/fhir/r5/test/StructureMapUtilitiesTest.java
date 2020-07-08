381525fc174f7d241b477ff31e3e4c617c9e4483
Oliver Egger <oliver.egger@ahdis.ch>
2020-07-08 09:44:51 +0200

    extend tests for structuremaps text serializing

--- org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/test/StructureMapUtilitiesTest.java
+++ org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/test/StructureMapUtilitiesTest.java
@@ -15,18 +15,17 @@ import org.hl7.fhir.r5.utils.StructureMapUtilities;
 import org.hl7.fhir.r5.utils.StructureMapUtilities.ITransformerServices;
 import org.hl7.fhir.utilities.cache.FilesystemPackageCacheManager;
 import org.hl7.fhir.utilities.cache.ToolsVersion;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-@Disabled // org.hl7.fhir.exceptions.FHIRException: Unable to resolve package id hl7.fhir.core#4.0.0
 public class StructureMapUtilitiesTest implements ITransformerServices {
 
   static private SimpleWorkerContext context;
-
-  //  @BeforeAll
+  
+  @BeforeAll
   static public void setUp() throws Exception {
     FilesystemPackageCacheManager pcm = new FilesystemPackageCacheManager(true, ToolsVersion.TOOLS_VERSION);
-    context = SimpleWorkerContext.fromPackage(pcm.loadPackage("hl7.fhir.core", "4.0.0"));
+    context = SimpleWorkerContext.fromPackage(pcm.loadPackage("hl7.fhir.r4.core", "4.0.1"));
   }
 
   @Test
@@ -38,6 +37,26 @@ public class StructureMapUtilitiesTest implements ITransformerServices {
     // StructureMap/ActivityDefinition3to4: StructureMap.group[3].rule[2].name error id value '"expression"' is not valid
     assertEquals("expression", structureMap.getGroup().get(2).getRule().get(1).getName());
   }
+  
+  @Test
+  public void testSyntax() throws IOException, FHIRException {
+    StructureMapUtilities scu = new StructureMapUtilities(context, this);
+    String fileMap = TestingUtilities.loadTestResource("r5", "fml", "syntax.map");
+    StructureMap structureMap = scu.parse(fileMap, "Syntax");
+
+    assertEquals("syntax", structureMap.getName());
+    assertEquals("http://github.com/FHIR/fhir-test-cases/r5/fml/syntax", structureMap.getUrl());
+    assertEquals("Patient", structureMap.getStructure().get(0).getAlias());
+    assertEquals("http://hl7.org/fhir/StructureDefinition/Patient", structureMap.getStructure().get(0).getUrl());
+    assertEquals("Source Documentation", structureMap.getStructure().get(0).getDocumentation());
+    assertEquals("http://hl7.org/fhir/StructureDefinition/Patient", structureMap.getStructure().get(0).getUrl());
+    assertEquals("http://hl7.org/fhir/StructureDefinition/Basic", structureMap.getStructure().get(1).getUrl());
+    assertEquals("Target Documentation", structureMap.getStructure().get(1).getDocumentation());
+        
+    System.out.println(StructureMapUtilities.render(structureMap));
+
+  }
+
 
   @Override
   public void log(String message) {
