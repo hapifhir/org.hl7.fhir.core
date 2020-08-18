2d6e7711b09060f8906133958fb2913bd991a7a1
Grahame Grieve <grahameg@gmail.com>
2020-08-18 17:58:52 +1000

    Remove old comparison code

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/ComparisonService.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/ComparisonService.java
@@ -2,7 +2,6 @@ package org.hl7.fhir.validation.cli.services;
 
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.r5.conformance.CapabilityStatementUtilities;
-import org.hl7.fhir.r5.conformance.ProfileComparer;
 import org.hl7.fhir.r5.formats.IParser;
 import org.hl7.fhir.r5.formats.JsonParser;
 import org.hl7.fhir.r5.formats.XmlParser;
@@ -11,7 +10,6 @@ import org.hl7.fhir.r5.model.CapabilityStatement;
 import org.hl7.fhir.r5.model.Resource;
 import org.hl7.fhir.r5.model.StructureDefinition;
 import org.hl7.fhir.r5.utils.EOperationOutcome;
-import org.hl7.fhir.r5.utils.KeyGenerator;
 import org.hl7.fhir.utilities.TextFile;
 import org.hl7.fhir.utilities.Utilities;
 import org.hl7.fhir.utilities.validation.ValidationMessage;
@@ -54,26 +52,26 @@ public class ComparisonService {
     String nameLeft = chooseName(args, "leftName", resLeft);
     String nameRight = chooseName(args, "rightName", resRight);
     System.out.println("Comparing CapabilityStatements " + left + " to " + right);
-    CapabilityStatementUtilities pc = new CapabilityStatementUtilities(validator.getContext(), dest, new KeyGenerator("http://fhir.org/temp/" + UUID.randomUUID().toString().toLowerCase()));
-    CapabilityStatement capL = (CapabilityStatement) resLeft;
-    CapabilityStatement capR = (CapabilityStatement) resRight;
-    CapabilityStatementUtilities.CapabilityStatementComparisonOutput output = pc.isCompatible(nameLeft, nameRight, capL, capR);
-
-    String destTxt = Utilities.path(dest, "output.txt");
-    System.out.println("Generating output to " + destTxt + "...");
-    StringBuilder b = new StringBuilder();
-    for (ValidationMessage msg : output.getMessages()) {
-      b.append(msg.summary());
-      b.append("\r\n");
-    }
-    TextFile.stringToFile(b.toString(), destTxt);
-    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-union.xml")), output.getSuperset());
-    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-intersection.xml")), output.getSubset());
-    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "OperationOutcome-issues.xml")), output.getOutcome());
-    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-union.json")), output.getSuperset());
-    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-intersection.json")), output.getSubset());
-    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "OperationOutcome-issues.json")), output.getOutcome());
-
+//    CapabilityStatementUtilities pc = new CapabilityStatementUtilities(validator.getContext(), dest, new KeyGenerator("http://fhir.org/temp/" + UUID.randomUUID().toString().toLowerCase()));
+//    CapabilityStatement capL = (CapabilityStatement) resLeft;
+//    CapabilityStatement capR = (CapabilityStatement) resRight;
+//    CapabilityStatementUtilities.CapabilityStatementComparisonOutput output = pc.isCompatible(nameLeft, nameRight, capL, capR);
+//
+//    String destTxt = Utilities.path(dest, "output.txt");
+//    System.out.println("Generating output to " + destTxt + "...");
+//    StringBuilder b = new StringBuilder();
+//    for (ValidationMessage msg : output.getMessages()) {
+//      b.append(msg.summary());
+//      b.append("\r\n");
+//    }
+//    TextFile.stringToFile(b.toString(), destTxt);
+//    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-union.xml")), output.getSuperset());
+//    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-intersection.xml")), output.getSubset());
+//    new XmlParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "OperationOutcome-issues.xml")), output.getOutcome());
+//    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-union.json")), output.getSuperset());
+//    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "CapabilityStatement-intersection.json")), output.getSubset());
+//    new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).compose(new FileOutputStream(Utilities.path(dest, "OperationOutcome-issues.json")), output.getOutcome());
+//
     String destHtml = Utilities.path(dest, "public/index.html");
     File htmlFile = new File(destHtml);
     Desktop.getDesktop().browse(htmlFile.toURI());
@@ -81,15 +79,15 @@ public class ComparisonService {
   }
 
   public static void compareStructureDefinitions(String dest, ValidationEngine validator, String left, String right, StructureDefinition resLeft, StructureDefinition resRight) throws IOException, FHIRException, EOperationOutcome {
-    System.out.println("Comparing StructureDefinitions " + left + " to " + right);
-    ProfileComparer pc = new ProfileComparer(validator.getContext(), dest);
-    StructureDefinition sdL = resLeft;
-    StructureDefinition sdR = resRight;
-    pc.compareProfiles(sdL, sdR);
-    System.out.println("Generating output to " + dest + "...");
-    File htmlFile = new File(pc.generate());
-    Desktop.getDesktop().browse(htmlFile.toURI());
-    System.out.println("Done");
+//    System.out.println("Comparing StructureDefinitions " + left + " to " + right);
+//    ProfileComparer pc = new ProfileComparer(validator.getContext(), dest);
+//    StructureDefinition sdL = resLeft;
+//    StructureDefinition sdR = resRight;
+//    pc.compareProfiles(sdL, sdR);
+//    System.out.println("Generating output to " + dest + "...");
+//    File htmlFile = new File(pc.generate());
+//    Desktop.getDesktop().browse(htmlFile.toURI());
+//    System.out.println("Done");
   }
 
   private static String chooseName(String[] args, String name, CanonicalResource mr) {
