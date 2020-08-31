8e4daefc52d85579fc177d7ebe055bcf7940de3b
Grahame Grieve <grahameg@gmail.com>
2020-08-31 13:22:42 +1000

    Add parent tracking for Validation support of nested bundles

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/Utilities.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/Utilities.java
@@ -57,6 +57,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
@@ -1369,4 +1370,30 @@ public class Utilities {
 
     return byteArrays;
   }
+  
+  public static String presentDuration(long duration) {
+    duration = duration / 1000000;
+    String res = "";    // ;
+    long days       = TimeUnit.MILLISECONDS.toDays(duration);
+    long hours      = TimeUnit.MILLISECONDS.toHours(duration) -
+        TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
+    long minutes    = TimeUnit.MILLISECONDS.toMinutes(duration) -
+        TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration));
+    long seconds    = TimeUnit.MILLISECONDS.toSeconds(duration) -
+        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration));
+    long millis     = TimeUnit.MILLISECONDS.toMillis(duration) -
+        TimeUnit.SECONDS.toMillis(TimeUnit.MILLISECONDS.toSeconds(duration));
+
+    if (days > 0)
+      res = String.format("%dd %02d:%02d:%02d.%04d", days, hours, minutes, seconds, millis);
+    else if (hours > 0)
+      res = String.format("%02d:%02d:%02d.%04d", hours, minutes, seconds, millis);
+    else //
+      res = String.format("%02d:%02d.%04d", minutes, seconds, millis);
+//    else
+//      res = String.format("%02d.%04d", seconds, millis);
+    return res;
+  }
+
+
 }
