5bf9f08b17075541ddf70b70c2073c63e4264af4
Mark Iantorno <markiantorno@gmail.com>
2020-11-24 09:20:43 -0500

    Http client update (#378)
    * wip
    
    * tests all pass with log outputs identical to original...is it better now? I don't know.
    
    * proxy works, adding a test and docs next
    
    * more cleanup and test fixes
    
    * left in testing file
    
    * didn't need that code anymore

---
+++ org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/utils/client/network/ClientTest.java
@@ -0,0 +1,146 @@
+package org.hl7.fhir.r5.utils.client.network;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.hl7.fhir.r5.formats.JsonParser;
+import org.hl7.fhir.r5.model.*;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ClientTest {
+
+  private static final long TIMEOUT = 5000;
+
+  private MockWebServer server;
+  private HttpUrl serverUrl;
+  private Client client;
+
+  private Address address = new Address()
+    .setCity("Toronto")
+    .setState("Ontario")
+    .setCountry("Canada");
+  private HumanName humanName = new HumanName()
+    .addGiven("Mark")
+    .setFamily("Iantorno");
+  private Patient patient = new Patient()
+    .addName(humanName)
+    .addAddress(address)
+    .setGender(Enumerations.AdministrativeGender.MALE);
+
+  @BeforeEach
+  void setup() {
+    setupMockServer();
+    client = new Client();
+  }
+
+  void setupMockServer() {
+    server = new MockWebServer();
+    serverUrl = server.url("/v1/endpoint");
+  }
+
+  byte[] generateResourceBytes(Resource resource) throws IOException {
+    return new JsonParser().composeBytes(resource);
+  }
+
+  @Test
+  @DisplayName("GET request, happy path.")
+  void test_get_happy_path() throws IOException, URISyntaxException {
+    server.enqueue(
+      new MockResponse()
+        .setBody(new String(generateResourceBytes(patient)))
+    );
+    ResourceRequest<Resource> resourceRequest = client.issueGetResourceRequest(new URI(serverUrl.toString()),
+      "json", null, TIMEOUT);
+    Assertions.assertTrue(resourceRequest.isSuccessfulRequest());
+    Assertions.assertTrue(patient.equalsDeep(resourceRequest.getPayload()),
+      "GET request returned resource does not match expected.");
+  }
+
+  @Test
+  @DisplayName("GET request, test client retries after timeout failure.")
+  void test_get_retries_with_timeout() throws IOException, URISyntaxException {
+    int failedAttempts = new Random().nextInt(5) + 1;
+    System.out.println("Simulating <" + failedAttempts + "> failed connections (timeouts) before success.");
+    for (int i = 0; i < failedAttempts; i++) {
+      server.enqueue(
+        new MockResponse()
+          .setHeadersDelay(TIMEOUT * 10, TimeUnit.MILLISECONDS)
+          .setBody(new String(generateResourceBytes(patient)))
+      );
+    }
+    server.enqueue(new MockResponse().setBody(new String(generateResourceBytes(patient))));
+    client.setRetryCount(failedAttempts + 1);
+
+    ResourceRequest<Resource> resourceRequest = client.issueGetResourceRequest(new URI(serverUrl.toString()),
+      "json", null, TIMEOUT);
+    Assertions.assertTrue(resourceRequest.isSuccessfulRequest());
+    Assertions.assertTrue(patient.equalsDeep(resourceRequest.getPayload()),
+      "GET request returned resource does not match expected.");
+  }
+
+  @Test
+  @DisplayName("GET request, test client retries after bad response.")
+  void test_get_retries_with_unsuccessful_response() throws IOException, URISyntaxException {
+    int failedAttempts = new Random().nextInt(5) + 1;
+    System.out.println("Simulating <" + failedAttempts + "> failed connections (bad response codes) before success.");
+    for (int i = 0; i < failedAttempts; i++) {
+      server.enqueue(
+        new MockResponse()
+          .setResponseCode(400 + i)
+          .setBody(new String(generateResourceBytes(patient)))
+      );
+    }
+    server.enqueue(new MockResponse().setBody(new String(generateResourceBytes(patient))));
+    client.setRetryCount(failedAttempts + 1);
+
+    ResourceRequest<Resource> resourceRequest = client.issueGetResourceRequest(new URI(serverUrl.toString()),
+      "json", null, TIMEOUT);
+    Assertions.assertTrue(resourceRequest.isSuccessfulRequest());
+    Assertions.assertTrue(patient.equalsDeep(resourceRequest.getPayload()),
+      "GET request returned resource does not match expected.");
+  }
+
+  @Test
+  @DisplayName("PUT request, test payload received by server matches sent.")
+  void test_put() throws IOException, URISyntaxException, InterruptedException {
+    byte[] payload = ByteUtils.resourceToByteArray(patient, true, false);
+    // Mock server response of 200, with the same resource payload returned that we included in the PUT request
+    server.enqueue(
+      new MockResponse()
+        .setResponseCode(200)
+        .setBody(new String(payload))
+    );
+
+    ResourceRequest<Resource> request = client.issuePutRequest(new URI(serverUrl.toString()), payload,
+      "xml", null, TIMEOUT);
+    RecordedRequest recordedRequest = server.takeRequest();
+    Assertions.assertArrayEquals(payload, recordedRequest.getBody().readByteArray(),
+      "PUT request payload does not match send data.");
+  }
+
+  @Test
+  @DisplayName("POST request, test payload received by server matches sent.")
+  void test_post() throws IOException, URISyntaxException, InterruptedException {
+    byte[] payload = ByteUtils.resourceToByteArray(patient, true, false);
+    // Mock server response of 200, with the same resource payload returned that we included in the PUT request
+    server.enqueue(
+      new MockResponse()
+        .setResponseCode(200)
+        .setBody(new String(payload))
+    );
+
+    ResourceRequest<Resource> request = client.issuePostRequest(new URI(serverUrl.toString()), payload,
+      "xml", null, TIMEOUT);
+    RecordedRequest recordedRequest = server.takeRequest();
+    Assertions.assertArrayEquals(payload, recordedRequest.getBody().readByteArray(),
+      "POST request payload does not match send data.");
+  }
+}
