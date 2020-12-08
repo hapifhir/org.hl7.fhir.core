6b1a92e66535d4f0072737e22fa83227137be811
Grahame Grieve <grahameg@gmail.com>
2020-12-09 10:06:37 +1100

    Fix output message when there's nothing to validate

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/ValidationService.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/ValidationService.java
@@ -81,8 +81,12 @@ public class ValidationService {
       if (r instanceof Bundle)
         for (Bundle.BundleEntryComponent e : ((Bundle) r).getEntry())
           ec = ec + displayOperationOutcome((OperationOutcome) e.getResource(), ((Bundle) r).getEntry().size() > 1) + ec;
-      else
+      else if (r == null) {
+        ec = ec + 1;
+        System.out.println("No output from validation - nothing to validate");
+      } else {
         ec = displayOperationOutcome((OperationOutcome) r, false);
+      }
     } else {
       IParser x;
       if (cliContext.getOutput() != null && cliContext.getOutput().endsWith(".json")) {
