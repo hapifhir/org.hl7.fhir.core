8fac290ebedd788315727a7e141a69c0c9bf2720
Grahame Grieve <grahameg@gmail.com>
2021-02-08 09:33:36 +1100

    #426 - fix serialization of NULL enumerations

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/patterns/Event.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/patterns/Event.java
@@ -130,6 +130,7 @@ public interface Event extends PatternBase {
             case COMPLETED: return "completed";
             case ENTEREDINERROR: return "entered-in-error";
             case UNKNOWN: return "unknown";
+            case NULL: return null;
             default: return "?";
           }
         }
@@ -143,6 +144,7 @@ public interface Event extends PatternBase {
             case COMPLETED: return "http://hl7.org/fhir/event-status";
             case ENTEREDINERROR: return "http://hl7.org/fhir/event-status";
             case UNKNOWN: return "http://hl7.org/fhir/event-status";
+            case NULL: return null;
             default: return "?";
           }
         }
@@ -156,6 +158,7 @@ public interface Event extends PatternBase {
             case COMPLETED: return "The event has now concluded.";
             case ENTEREDINERROR: return "This electronic record should never have existed, though it is possible that real-world decisions were based on it.  (If real-world activity has occurred, the status should be \"cancelled\" rather than \"entered-in-error\".).";
             case UNKNOWN: return "The authoring/source system does not know which of the status values currently applies for this event.  Note: This concept is not to be used for \"other\" - one of the listed statuses is presumed to apply,  but the authoring/source system does not know which.";
+            case NULL: return null;
             default: return "?";
           }
         }
@@ -169,6 +172,7 @@ public interface Event extends PatternBase {
             case COMPLETED: return "Completed";
             case ENTEREDINERROR: return "Entered in Error";
             case UNKNOWN: return "Unknown";
+            case NULL: return null;
             default: return "?";
           }
         }
