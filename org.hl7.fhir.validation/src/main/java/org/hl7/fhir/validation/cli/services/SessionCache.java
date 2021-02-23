7380e357968d9b7483de190cb60e5680c5be5a3a
markiantorno <markiantorno@gmail.com>
2021-02-23 13:27:11 -0500

    adding javadocs to cache

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/SessionCache.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/SessionCache.java
@@ -7,6 +7,10 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * SessionCache for storing and retrieving ValidationEngine instances, so callers do not have to re-instantiate a new
+ * instance for each validation request.
+ */
 public class SessionCache {
 
   protected static final long TIME_TO_LIVE = 60;
@@ -18,37 +22,73 @@ public class SessionCache {
     cachedSessions = new PassiveExpiringMap<>(TIME_TO_LIVE, TIME_UNIT);
   }
 
+  /**
+   * @param sessionLength the constant amount of time an entry is available before it expires. A negative value results
+   *                      in entries that NEVER expire. A zero value results in entries that ALWAYS expire.
+   * @param sessionLengthUnit the unit of time for the timeToLive parameter, must not be null
+   */
   public SessionCache(long sessionLength, TimeUnit sessionLengthUnit) {
     cachedSessions = new PassiveExpiringMap<>(sessionLength, sessionLengthUnit);
   }
 
-  public String cacheSession(ValidationEngine validator) {
+  /**
+   * Stores the initialized {@link ValidationEngine} in the cache. Returns the session id that will be associated with
+   * this instance.
+   * @param validationEngine {@link ValidationEngine}
+   * @return The {@link String} id associated with the stored instance.
+   */
+  public String cacheSession(ValidationEngine validationEngine) {
     String generatedId = generateID();
-    cachedSessions.put(generatedId, validator);
+    cachedSessions.put(generatedId, validationEngine);
     return generatedId;
   }
 
-  public String cacheSession(String sessionId, ValidationEngine validator) {
+  /**
+   * Stores the initialized {@link ValidationEngine} in the cache with the passed in id as the key. If a null key is
+   * passed in, a new key is generated and returned.
+   * @param sessionId The {@link String} key to associate with this stored {@link ValidationEngine}
+   * @param validationEngine The {@link ValidationEngine} instance to cache.
+   * @return The {@link String} id that will be associated with the stored {@link ValidationEngine}
+   */
+  public String cacheSession(String sessionId, ValidationEngine validationEngine) {
     if(sessionId == null) {
-      sessionId = cacheSession(validator);
+      sessionId = cacheSession(validationEngine);
     } else {
-      cachedSessions.put(sessionId, validator);
+      cachedSessions.put(sessionId, validationEngine);
     }
     return sessionId;
   }
 
+  /**
+   * Checks if the passed in {@link String} id exists in the set of stored session id.
+   * @param sessionId The {@link String} id to search for.
+   * @return {@link Boolean#TRUE} if such id exists.
+   */
   public boolean sessionExists(String sessionId) {
     return cachedSessions.containsKey(sessionId);
   }
 
+  /**
+   * Returns the stored {@link ValidationEngine} associated with the passed in session id, if one such instance exists.
+   * @param sessionId The {@link String} session id.
+   * @return The {@link ValidationEngine} associated with the passed in id, or null if none exists.
+   */
   public ValidationEngine fetchSessionValidatorEngine(String sessionId) {
     return cachedSessions.get(sessionId);
   }
 
+  /**
+   * Returns the set of stored session ids.
+   * @return {@link Set} of session ids.
+   */
   public Set<String> getSessionIds() {
     return cachedSessions.keySet();
   }
 
+  /**
+   * Session ids generated internally are UUID {@link String}.
+   * @return A new {@link String} session id.
+   */
   private String generateID() {
     return UUID.randomUUID().toString();
   }
