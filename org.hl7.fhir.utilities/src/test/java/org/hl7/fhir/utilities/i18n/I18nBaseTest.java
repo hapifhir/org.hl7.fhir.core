1a1278b396439e0afaf3755a33ef8a23ad1ba497
Mark Iantorno <markiantorno@gmail.com>
2021-06-22 20:29:27 -0400

    Fixes to display umlauts correctly in validation messaging. (#542)
    * Fixes to display umlauts correctly in validation messaging.
    
    * adding tests

--- org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/i18n/I18nBaseTest.java
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/i18n/I18nBaseTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 class I18nBaseTest {
 
@@ -41,5 +45,39 @@ class I18nBaseTest {
   void testFormatMessageForNonExistentMessage() {
     I18nTestClass testClass = new I18nTestClass();
     Assertions.assertEquals(BAD_STRING_ARG, testClass.formatMessage(BAD_STRING_ARG, ARG_1));
+  }
+
+  @Test
+  @DisplayName("Test umlauts display correctly.")
+  void testTheGermansAreComing() {
+    I18nTestClass testClass = new I18nTestClass();
+    ResourceBundle loadedBundle = ResourceBundle.getBundle("Messages", Locale.GERMAN);
+    testClass.setLocale(Locale.GERMAN);
+    String result = testClass.formatMessage(I18nConstants.BUNDLE_BUNDLE_ENTRY_NOFIRST, ARG_1);
+    //Ensure the umlaut is displayed correctly. If not right, will show: Ã¼, not ü
+    Assertions.assertEquals("Documents oder Messages müssen mindestens einen Eintrag enthalten", result);
+  }
+
+  @Test
+  @DisplayName("Test German localization file contains no umlauts.")
+  void testThatNoOneHasMessedWithTheGermans() {
+    I18nTestClass testClass = new I18nTestClass();
+    InputStream is = getClass().getClassLoader().getResourceAsStream("Messages_de.properties");
+    try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader)) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          System.out.println("Searching for umlauts -> " + line);
+          Assertions.assertFalse(stringContainsItemFromList(line, UMLAUTS));
+        }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private final String[] UMLAUTS = {"Ä", "Ö", "Ü", "ä", "ö", "ü"};
+
+  public static boolean stringContainsItemFromList(String inputStr, String[] items) {
+    return Arrays.stream(items).anyMatch(inputStr::contains);
   }
 }
