6bf58e678e65b4bae6e5af0c89827a57bb4524f0
Lukas Schmierer <lukas.schmierer@medizin.uni-leipzig.de>
2021-10-04 16:06:07 +0200

    change import comment to be compatible with graphql-tools

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/utils/GraphQLSchemaGenerator.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/utils/GraphQLSchemaGenerator.java
@@ -116,7 +116,7 @@ public class GraphQLSchemaGenerator {
   public void generateResource(OutputStream stream, StructureDefinition sd, List<SearchParameter> parameters, EnumSet<FHIROperationType> operations) throws IOException, FHIRException {
     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stream));
     writer.write("# FHIR GraphQL Schema. Version "+version+"\r\n\r\n");
-    writer.write("# import the types from 'types.graphql'\r\n\r\n");
+    writer.write("# import * from 'types.graphql'\r\n\r\n");
     generateType(writer, sd);
     if (operations.contains(FHIROperationType.READ))
       generateIdAccess(writer, sd.getName());
