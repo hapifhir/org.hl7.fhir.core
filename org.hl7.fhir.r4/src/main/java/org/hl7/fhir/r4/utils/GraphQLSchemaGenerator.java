04a72c6e2938b0be43873827b46d593cdac1b4dd
Lukas Schmierer <lukas.schmierer@medizin.uni-leipzig.de>
2021-10-04 16:03:17 +0200

    use ElementBaseInput instead of ElementBase for input types

--- org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/utils/GraphQLSchemaGenerator.java
+++ org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/utils/GraphQLSchemaGenerator.java
@@ -247,6 +247,11 @@ public class GraphQLSchemaGenerator {
     writer.write("}\r\n");
     writer.write("\r\n");
     
+    writer.write("input ElementBaseInput {\r\n");
+    writer.write("  id : ID\r\n");
+    writer.write("  extension: [ExtensionInput]\r\n");
+    writer.write("}\r\n");
+    writer.write("\r\n");
   }
 
   private void generateType(BufferedWriter writer, StructureDefinition sd) throws IOException {
@@ -326,10 +331,15 @@ public class GraphQLSchemaGenerator {
         b.append(tail(child.getPath(), suffix));
         if (suffix)
           b.append(Utilities.capitalize(typeDetails.getWorkingCode()));
-        if (!child.getMax().equals("1"))
-          b.append(": [ElementBase]\r\n");
-        else
-          b.append(": ElementBase\r\n");
+        if (!child.getMax().equals("1")) {
+          b.append(": [ElementBase");
+          b.append(suffixS);
+          b.append("]\r\n");
+        } else {
+          b.append(": ElementBase");
+          b.append(suffixS);
+          b.append("\r\n");
+        }
       } else
         b.append("\r\n");
     } else {
