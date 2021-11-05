ce074c6b6f9443ed68d4ed22b2b7447cf3f5e6a9
tyge.folke.nielsen <tyge.folke.nielsen@@systematic.com>
2021-11-05 13:50:19 +0100

    FUT1-6371 fix R4 PlanDefinition search parameter definition - path changed to PlanDefinition.repeat(action).definition to traverse action tree when including

--- org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/model/PlanDefinition.java
+++ org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/model/PlanDefinition.java
@@ -9920,7 +9920,7 @@ public class PlanDefinition extends MetadataResource {
    * Path: <b>PlanDefinition.action.definition[x]</b><br>
    * </p>
    */
-  @SearchParamDefinition(name = "definition", path = "PlanDefinition.action.definition", description = "Activity or plan definitions used by plan definition", type = "reference", target = {
+  @SearchParamDefinition(name = "definition", path = "PlanDefinition.repeat(action).definition", description = "Activity or plan definitions used by plan definition", type = "reference", target = {
       ActivityDefinition.class, PlanDefinition.class, Questionnaire.class })
   public static final String SP_DEFINITION = "definition";
   /**
