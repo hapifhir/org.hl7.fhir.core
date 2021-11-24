ef759acbf4fe15539b8a67ce27729a3b231e5f45
dotasek <dotasek.dev@gmail.com>
2021-11-24 13:29:33 -0500

    Refactor FhirReqestBuilderTests

--- org.hl7.fhir.dstu3/src/test/java/org/hl7/fhir/dstu3/utils/client/network/FhirRequestBuilderTests.java
+++ org.hl7.fhir.dstu3/src/test/java/org/hl7/fhir/dstu3/utils/client/network/FhirRequestBuilderTests.java
@@ -5,6 +5,7 @@ import org.hl7.fhir.dstu3.formats.IParser;
 
 import org.hl7.fhir.utilities.ToolingClientLogger;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalMatchers;
@@ -54,55 +55,46 @@ public class FhirRequestBuilderTests {
   @BeforeEach
   public void beforeEach() {
     Mockito.doReturn(client).when(fhirRequestBuilder).getHttpClient();
+    fhirRequestBuilder.withLogger(logger);
   }
 
-  @Test
-  public void testExecuteLogging() throws IOException {
+  @Nested
+  class RequestLoggingTests {
 
-    Mockito.doReturn(response).when(mockCall).execute();
-    Mockito.doReturn(mockCall).when(client).newCall(ArgumentMatchers.any());
+    @BeforeEach
+    public void beforeEach() throws IOException {
+      Mockito.doReturn(response).when(mockCall).execute();
+      Mockito.doReturn(mockCall).when(client).newCall(ArgumentMatchers.any());
 
-    Mockito.doReturn(null).when(fhirRequestBuilder).unmarshalReference(ArgumentMatchers.any(), ArgumentMatchers.isNull());
+      Mockito.doReturn(null).when(fhirRequestBuilder).unmarshalReference(ArgumentMatchers.any(), ArgumentMatchers.isNull());
+    }
 
-    fhirRequestBuilder.withLogger(logger);
-    fhirRequestBuilder.execute();
+    @Test
+    public void testExecuteLogging() throws IOException {
+      fhirRequestBuilder.execute();
+      Mockito.verify(logger).logRequest(ArgumentMatchers.eq("GET"), ArgumentMatchers.eq(DUMMY_URL), ArgumentMatchers.anyList(), ArgumentMatchers.isNull());
+    }
 
-    Mockito.verify(logger).logRequest(ArgumentMatchers.eq("GET"), ArgumentMatchers.eq(DUMMY_URL), ArgumentMatchers.anyList(), ArgumentMatchers.isNull());
-  }
+    @Test
+    public void testExecuteBatchLogging() throws IOException {
+      fhirRequestBuilder.executeAsBatch();
+      Mockito.verify(logger).logRequest(ArgumentMatchers.eq("GET"), ArgumentMatchers.eq(DUMMY_URL), ArgumentMatchers.anyList(), ArgumentMatchers.isNull());
+    }
 
-  @Test
-  public void testExecuteBatchLogging() throws IOException {
-
-    Mockito.doReturn(response).when(mockCall).execute();
-    Mockito.doReturn(mockCall).when(client).newCall(ArgumentMatchers.any());
-
-    Mockito.doReturn(null).when(fhirRequestBuilder).unmarshalFeed(ArgumentMatchers.any(), ArgumentMatchers.isNull());
-
-    fhirRequestBuilder.withLogger(logger);
-    fhirRequestBuilder.executeAsBatch();
-
-    Mockito.verify(logger).logRequest(ArgumentMatchers.eq("GET"), ArgumentMatchers.eq(DUMMY_URL), ArgumentMatchers.anyList(), ArgumentMatchers.isNull());
   }
 
   @Test
   public void testUnmarshallReferenceLogging() {
-
     IParser parser = Mockito.mock(IParser.class);
-
     Mockito.doReturn(parser).when(fhirRequestBuilder).getParser(ArgumentMatchers.eq("json"));
 
-    fhirRequestBuilder.withLogger(logger);
     fhirRequestBuilder.unmarshalReference(response, "json");
-
     Mockito.verify(logger).logResponse(ArgumentMatchers.eq("200"), ArgumentMatchers.anyList(), AdditionalMatchers.aryEq(RESPONSE_BODY_STRING.getBytes()));
   }
 
   @Test
   public void testUnmarshallFeedLogging() {
-
-    fhirRequestBuilder.withLogger(logger);
     fhirRequestBuilder.unmarshalFeed(response, "application/json");
-
     Mockito.verify(logger).logResponse(ArgumentMatchers.eq("200"), ArgumentMatchers.anyList(), AdditionalMatchers.aryEq(RESPONSE_BODY_STRING.getBytes()));
   }
 
