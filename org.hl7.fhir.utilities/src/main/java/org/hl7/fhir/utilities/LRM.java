b93a6aa1ffbdea77f0e026d810f7ae70ca46424b
dotasek <dotasek.dev@gmail.com>
2021-11-24 16:59:16 -0500

    Add example bidi character file

---
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/LRM.java
@@ -0,0 +1,13 @@
+package org.hl7.fhir.utilities;
+
+/**
+ * ‎
+ * Bidi characters above
+ */
+public class LRM
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
