fb8e2cfa66f6a598e3bb8e7e349ecbd75cd5d10d
dotasek <david.otasek@smilecdr.com>
2022-03-11 17:32:09 -0500

    WIP Start building tests

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Date40_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Date40_50.java
@@ -5,13 +5,15 @@ import org.hl7.fhir.exceptions.FHIRException;
 
 public class Date40_50 {
   public static org.hl7.fhir.r5.model.DateType convertDate(org.hl7.fhir.r4.model.DateType src) throws FHIRException {
-    org.hl7.fhir.r5.model.DateType tgt = src.hasValue() ? new org.hl7.fhir.r5.model.DateType(src.getValue()) : new org.hl7.fhir.r5.model.DateType();
+    org.hl7.fhir.r5.model.DateType tgt = new org.hl7.fhir.r5.model.DateType();
+    tgt.setValueAsString(src.getValueAsString());
     ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
     return tgt;
   }
 
   public static org.hl7.fhir.r4.model.DateType convertDate(org.hl7.fhir.r5.model.DateType src) throws FHIRException {
-    org.hl7.fhir.r4.model.DateType tgt = src.hasValue() ? new org.hl7.fhir.r4.model.DateType(src.getValue()) : new org.hl7.fhir.r4.model.DateType();
+    org.hl7.fhir.r4.model.DateType tgt = new org.hl7.fhir.r4.model.DateType();
+    tgt.setValueAsString(src.getValueAsString());
     ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
     return tgt;
   }
