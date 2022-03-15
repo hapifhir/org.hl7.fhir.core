4068db73ffc4fba3ba07ac308027964953cdd5c0
dotasek <david.otasek@smilecdr.com>
2022-03-15 12:35:50 -0400

    WIP Add tests for Primitive Types + use getValueAsString for conversion

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Date40_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Date40_50.java
@@ -5,15 +5,13 @@ import org.hl7.fhir.exceptions.FHIRException;
 
 public class Date40_50 {
   public static org.hl7.fhir.r5.model.DateType convertDate(org.hl7.fhir.r4.model.DateType src) throws FHIRException {
-    org.hl7.fhir.r5.model.DateType tgt = new org.hl7.fhir.r5.model.DateType();
-    tgt.setValueAsString(src.getValueAsString());
+    org.hl7.fhir.r5.model.DateType tgt = src.hasValue() ? new org.hl7.fhir.r5.model.DateType(src.getValueAsString()) : new org.hl7.fhir.r5.model.DateType();
     ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
     return tgt;
   }
 
   public static org.hl7.fhir.r4.model.DateType convertDate(org.hl7.fhir.r5.model.DateType src) throws FHIRException {
-    org.hl7.fhir.r4.model.DateType tgt = new org.hl7.fhir.r4.model.DateType();
-    tgt.setValueAsString(src.getValueAsString());
+    org.hl7.fhir.r4.model.DateType tgt = src.hasValue() ? new org.hl7.fhir.r4.model.DateType(src.getValueAsString()) : new org.hl7.fhir.r4.model.DateType();
     ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
     return tgt;
   }
---
+++ org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/VersionConvertorPrimitiveTypeTests.java
@@ -0,0 +1,135 @@
+package org.hl7.fhir.convertors;
+
+import org.hl7.fhir.convertors.factory.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class VersionConvertorPrimitiveTypeTests {
+
+  private static String[] DEFAULT_DATE_PRECISION_STRINGS = {
+    "1933", "1933-01", "1933-01-02"
+  };
+
+  private static String[] SECOND_DATE_PRECISION_STRINGS =
+    Stream.concat(Arrays.stream(DEFAULT_DATE_PRECISION_STRINGS),Stream.of("1933-01-02T13:45:12"))
+      .toArray(size -> (String[]) Array.newInstance(String.class, size));
+
+  private static String[] MILLISECOND_DATE_PRECISION_STRINGS =
+    Stream.concat(Arrays.stream(SECOND_DATE_PRECISION_STRINGS),Stream.of("1933-01-02T13:45:12.3", "1933-01-02T13:45:12.3456"))
+      .toArray(size -> (String[]) Array.newInstance(String.class, size));
+
+  private static <K,L>  Stream<Arguments> getDateTimeArgs(Class<? extends VersionConvertorFactory> versionConverterFactoryClazz, Class<K> firstTypeClazz, Class<?> firstTypeMethodClazz, Class<L> secondTypeClazz, Class<?> secondTypeMethodClazz, String[] testStrings) {
+
+    return Arrays.stream(testStrings).map( it -> Arguments.of(versionConverterFactoryClazz,
+      firstTypeClazz, firstTypeMethodClazz, it,
+      secondTypeClazz, secondTypeMethodClazz, it
+    ));
+  }
+
+  private static Stream<Arguments> getParams() {
+      return Stream.of(
+        //Date
+        getDateTimeArgs(VersionConvertorFactory_10_30.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.dstu3.model.DateType.class, org.hl7.fhir.dstu3.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_40.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r4.model.DateType.class, org.hl7.fhir.r4.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_50.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r5.model.DateType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_14_30.class,
+          org.hl7.fhir.dstu2016may.model.DateType.class, org.hl7.fhir.dstu2016may.model.Type.class,
+          org.hl7.fhir.dstu3.model.DateType.class, org.hl7.fhir.dstu3.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_14_40.class,
+          org.hl7.fhir.dstu2016may.model.DateType.class, org.hl7.fhir.dstu2016may.model.Type.class,
+          org.hl7.fhir.r4.model.DateType.class, org.hl7.fhir.r4.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_14_50.class,
+          org.hl7.fhir.dstu2016may.model.DateType.class, org.hl7.fhir.dstu2016may.model.Type.class,
+          org.hl7.fhir.r5.model.DateType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_30_40.class,
+          org.hl7.fhir.dstu3.model.DateType.class, org.hl7.fhir.dstu3.model.Type.class,
+          org.hl7.fhir.r4.model.DateType.class, org.hl7.fhir.r4.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_30_50.class,
+          org.hl7.fhir.dstu3.model.DateType.class, org.hl7.fhir.dstu3.model.Type.class,
+          org.hl7.fhir.r5.model.DateType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_40_50.class,
+          org.hl7.fhir.r4.model.DateType.class, org.hl7.fhir.r4.model.Type.class,
+          org.hl7.fhir.r5.model.DateType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+
+        //Date DateTime
+        getDateTimeArgs(VersionConvertorFactory_10_30.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.dstu3.model.DateTimeType.class, org.hl7.fhir.dstu3.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_40.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r4.model.DateTimeType.class, org.hl7.fhir.r4.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_50.class,
+          org.hl7.fhir.dstu2.model.DateType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r5.model.DateTimeType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_30.class,
+          org.hl7.fhir.dstu2.model.DateTimeType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.dstu3.model.DateType.class, org.hl7.fhir.dstu3.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_40.class,
+          org.hl7.fhir.dstu2.model.DateTimeType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r4.model.DateType.class, org.hl7.fhir.r4.model.Type.class,
+          DEFAULT_DATE_PRECISION_STRINGS),
+        getDateTimeArgs(VersionConvertorFactory_10_50.class,
+          org.hl7.fhir.dstu2.model.DateTimeType.class, org.hl7.fhir.dstu2.model.Type.class,
+          org.hl7.fhir.r5.model.DateType.class, org.hl7.fhir.r5.model.DataType.class,
+          DEFAULT_DATE_PRECISION_STRINGS)
+
+      ).flatMap(i -> i);
+
+  }
+
+  @ParameterizedTest(name = "Test index: {index} ConvertorFactory={0} First Class={1} First Value={3} Second Class={4} Second Value={6}")
+  @MethodSource("getParams")
+  public <K, L> void testValidConversion(Class<VersionConvertorFactory> versionConverterFactoryClazz, Class<K> firstTypeClazz, Class<?> firstTypeMethodClazz, String firstString, Class<L> secondTypeClazz, Class<?> secondTypeMethodClazz, String secondString) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+    testValidConversion(versionConverterFactoryClazz, firstTypeClazz, firstTypeMethodClazz, firstString, secondTypeClazz, secondString);
+    testValidConversion(versionConverterFactoryClazz, secondTypeClazz, secondTypeMethodClazz, secondString, firstTypeClazz, firstString);
+  }
+
+  public <K, L> void testValidConversion(Class<VersionConvertorFactory> versionConverterFactoryClazz, Class<K> srcTypeClazz, Class<?> srcTypeMethodClazz, String srcString, Class<L> tgtTypeClazz, String tgtString) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    K srcInstance = srcTypeClazz.getDeclaredConstructor().newInstance();
+
+    Method srcSetValueAsStringMethod = srcTypeClazz.getMethod("setValueAsString", String.class);
+    Method srcGetValueAsString =  srcTypeClazz.getMethod("getValueAsString");
+
+    Method convertTypeMethod = versionConverterFactoryClazz.getMethod("convertType", srcTypeMethodClazz);
+
+    Method tgtGetValueAsString = tgtTypeClazz.getMethod("getValueAsString");
+    srcSetValueAsStringMethod.invoke(srcInstance, srcString);
+
+    String srcValueAsString = (String) srcGetValueAsString.invoke(srcInstance);
+    Assertions.assertEquals(srcString, srcValueAsString);
+
+    L tgtInstance = (L) convertTypeMethod.invoke(null, srcInstance);
+
+    String tgtValueAsString = (String) tgtGetValueAsString.invoke(tgtInstance);
+
+    Assertions.assertEquals( tgtString, tgtValueAsString);
+  }
+}
