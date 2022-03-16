2d1254fcacff6cd6565e3961c03edca5c2cad006
dotasek <david.otasek@smilecdr.com>
2022-03-16 17:09:54 -0400

    Return null for conversions where isEmpty() == true

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv30_50/datatypes30_50/Type30_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv30_50/datatypes30_50/Type30_50.java
@@ -14,7 +14,7 @@ public class Type30_50 {
   }
 
   public org.hl7.fhir.r5.model.DataType convertType(org.hl7.fhir.dstu3.model.Type src) throws FHIRException {
-    if (src == null) return null;
+    if (src == null || src.isEmpty()) return null;
     if (src instanceof org.hl7.fhir.dstu3.model.Base64BinaryType)
       return Base64Binary30_50.convertBase64Binary((org.hl7.fhir.dstu3.model.Base64BinaryType) src);
     if (src instanceof org.hl7.fhir.dstu3.model.BooleanType)
@@ -120,7 +120,7 @@ public class Type30_50 {
   }
 
   public org.hl7.fhir.dstu3.model.Type convertType(org.hl7.fhir.r5.model.DataType src) throws FHIRException {
-    if (src == null) return null;
+    if (src == null || src.isEmpty()) return null;
     if (src instanceof org.hl7.fhir.r5.model.Base64BinaryType)
       return Base64Binary30_50.convertBase64Binary((org.hl7.fhir.r5.model.Base64BinaryType) src);
     if (src instanceof org.hl7.fhir.r5.model.BooleanType)
