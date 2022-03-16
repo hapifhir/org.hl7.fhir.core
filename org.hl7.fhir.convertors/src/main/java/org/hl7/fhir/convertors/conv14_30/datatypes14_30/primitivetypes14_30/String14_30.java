62df25971e7b7986f60a796c35b7ba1d5f5b7bd8
dotasek <david.otasek@smilecdr.com>
2022-03-16 17:41:56 -0400

    Add changes for String

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv14_30/datatypes14_30/primitivetypes14_30/String14_30.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv14_30/datatypes14_30/primitivetypes14_30/String14_30.java
@@ -5,15 +5,13 @@ import org.hl7.fhir.exceptions.FHIRException;
 
 public class String14_30 {
   public static org.hl7.fhir.dstu3.model.StringType convertString(org.hl7.fhir.dstu2016may.model.StringType src) throws FHIRException {
-    org.hl7.fhir.dstu3.model.StringType tgt = new org.hl7.fhir.dstu3.model.StringType();
-    if (src.hasValue()) tgt.setValue(src.getValue());
+    org.hl7.fhir.dstu3.model.StringType tgt = src.hasValue() ? new org.hl7.fhir.dstu3.model.StringType(src.getValueAsString()) : new org.hl7.fhir.dstu3.model.StringType();
     ConversionContext14_30.INSTANCE.getVersionConvertor_14_30().copyElement(src, tgt);
     return tgt;
   }
 
   public static org.hl7.fhir.dstu2016may.model.StringType convertString(org.hl7.fhir.dstu3.model.StringType src) throws FHIRException {
-    org.hl7.fhir.dstu2016may.model.StringType tgt = new org.hl7.fhir.dstu2016may.model.StringType();
-    if (src.hasValue()) tgt.setValue(src.getValue());
+    org.hl7.fhir.dstu2016may.model.StringType tgt = src.hasValue() ? new org.hl7.fhir.dstu2016may.model.StringType(src.getValueAsString()) : new org.hl7.fhir.dstu2016may.model.StringType();
     ConversionContext14_30.INSTANCE.getVersionConvertor_14_30().copyElement(src, tgt);
     return tgt;
   }
