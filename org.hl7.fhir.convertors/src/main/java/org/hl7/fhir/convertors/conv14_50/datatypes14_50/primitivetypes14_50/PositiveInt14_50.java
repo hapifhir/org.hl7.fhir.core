2c7def2515f9e1b7dedc87b509af453ce35e40c1
dotasek <david.otasek@smilecdr.com>
2022-03-17 13:53:01 -0400

    Add changes for PositiveInt

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv14_50/datatypes14_50/primitivetypes14_50/PositiveInt14_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv14_50/datatypes14_50/primitivetypes14_50/PositiveInt14_50.java
@@ -5,15 +5,13 @@ import org.hl7.fhir.exceptions.FHIRException;
 
 public class PositiveInt14_50 {
   public static org.hl7.fhir.r5.model.PositiveIntType convertPositiveInt(org.hl7.fhir.dstu2016may.model.PositiveIntType src) throws FHIRException {
-    org.hl7.fhir.r5.model.PositiveIntType tgt = new org.hl7.fhir.r5.model.PositiveIntType();
-    if (src.hasValue()) tgt.setValue(src.getValue());
+    org.hl7.fhir.r5.model.PositiveIntType tgt = src.hasValue() ? new org.hl7.fhir.r5.model.PositiveIntType(src.getValueAsString()) : new org.hl7.fhir.r5.model.PositiveIntType();
     ConversionContext14_50.INSTANCE.getVersionConvertor_14_50().copyElement(src, tgt);
     return tgt;
   }
 
   public static org.hl7.fhir.dstu2016may.model.PositiveIntType convertPositiveInt(org.hl7.fhir.r5.model.PositiveIntType src) throws FHIRException {
-    org.hl7.fhir.dstu2016may.model.PositiveIntType tgt = new org.hl7.fhir.dstu2016may.model.PositiveIntType();
-    if (src.hasValue()) tgt.setValue(src.getValue());
+    org.hl7.fhir.dstu2016may.model.PositiveIntType tgt = src.hasValue() ? new org.hl7.fhir.dstu2016may.model.PositiveIntType(src.getValueAsString()) : new org.hl7.fhir.dstu2016may.model.PositiveIntType();
     ConversionContext14_50.INSTANCE.getVersionConvertor_14_50().copyElement(src, tgt);
     return tgt;
   }
