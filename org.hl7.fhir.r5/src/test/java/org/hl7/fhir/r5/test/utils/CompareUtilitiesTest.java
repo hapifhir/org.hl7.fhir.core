f906a73184776e1e3c44652d80f4ebeec6e620ec
dotasek <david.otasek@smilecdr.com>
2022-03-31 15:12:17 -0400

    Add tests for CompareUtilities

--- org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/test/utils/CompareUtilitiesTest.java
+++ org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/test/utils/CompareUtilitiesTest.java
@@ -1,24 +1,96 @@
 package org.hl7.fhir.r5.test.utils;
 
+import org.apache.commons.io.IOUtils;
+import org.hl7.fhir.utilities.tests.BaseTestingUtilities;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CompareUtilitiesTest {
 
-  public static final String EXPECTED_XML_PATH = Paths.get("src","test","resources", "testUtilities", "expected.xml").toAbsolutePath().toString();
+  public static final Path ROOT_TEST_PATH = Paths.get("src","test","resources", "testUtilities");
 
-  public static final String ACTUAL_DIFF_XML_PATH = Paths.get("src","test","resources", "testUtilities", "actualDiffText.xml").toAbsolutePath().toString();
+  public static final Path ROOT_XML_TEST_PATH = ROOT_TEST_PATH.resolve("xml");
+  public static final Path ROOT_JSON_TEST_PATH = ROOT_TEST_PATH.resolve("json");
 
-  @Test
-  public void testCheckXMLIsSame() throws Exception {
-    String expected = CompareUtilities.checkXMLIsSame(EXPECTED_XML_PATH , EXPECTED_XML_PATH);
-    assertNull(expected);
 
-    String actualDiff = CompareUtilities.checkXMLIsSame(EXPECTED_XML_PATH , ACTUAL_DIFF_XML_PATH);
-    assertNotNull(actualDiff);
+  public String getResourceAsString(String path) throws IOException {
+    InputStream inputStream = new FileInputStream(path);
+    String contents = IOUtils.toString(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+    return contents;
   }
+
+  private static Stream<Arguments> getCompareXMLParams() {
+      return Stream.of(
+        Arguments.of("expected.xml", "expected.xml", null),
+        Arguments.of("expected.xml", "actualDiffText.xml", "actualDiffText.xml.error"),
+        Arguments.of("expected.xml", "actualMissingAttribute.xml", "actualMissingAttribute.xml.error"),
+        Arguments.of("expected.xml", "actualDiffAttribute.xml", "actualDiffAttribute.xml.error"),
+        Arguments.of("expected.xml", "actualDiffNodeType.xml", "actualDiffNodeType.xml.error"),
+        Arguments.of("expected.xml", "actualDiffTextEmpty.xml", "actualDiffTextEmpty.xml.error"),
+        Arguments.of("expected.xml", "actualExtraNode.xml", "actualExtraNode.xml.error"),
+        Arguments.of("expected.xml", "actualMissingNode.xml", "actualMissingNode.xml.error"),
+        Arguments.of("expected.xml", "actualDiffNamespace.xml", "actualDiffNamespace.xml.error"),
+        Arguments.of("expected.xml", "actualDiffLocalName.xml", "actualDiffLocalName.xml.error")
+        );
+  }
+
+  @ParameterizedTest
+  @MethodSource("getCompareXMLParams")
+  public void testCompareXML(String expectedFileName, String actualFileName, String expectedOutputFileName) throws Exception {
+    final String expectedXMLPath = ROOT_XML_TEST_PATH.resolve(expectedFileName).toAbsolutePath().toString();
+    final String actualXMLPath = ROOT_XML_TEST_PATH.resolve(actualFileName).toAbsolutePath().toString();
+
+    final String actualOutput = CompareUtilities.checkXMLIsSame(expectedXMLPath, actualXMLPath);
+
+    if (expectedOutputFileName == null) {
+      assertNull(actualOutput);
+    } else {
+      final String expectedOutputPath = ROOT_XML_TEST_PATH.resolve(expectedOutputFileName).toAbsolutePath().toString();
+      String expectedOutput = getResourceAsString(expectedOutputPath);
+      assertEquals(expectedOutput, actualOutput);
+    }
+  }
+
+ private static Stream<Arguments> getCompareJSONParams() {
+     return Stream.of(
+       Arguments.of("expected.json", "expected.json", null),
+       Arguments.of("expected.json", "actualDiffValue.json", "actualDiffValue.json.error"),
+       Arguments.of("expected.json", "actualDiffType.json", "actualDiffType.json.error"),
+       Arguments.of("expected.json", "actualDiffArrayContent.json", "actualDiffArrayContent.json.error"),
+       Arguments.of("expected.json", "actualDiffBoolean.json", "actualDiffBoolean.json.error"),
+       Arguments.of("expected.json", "actualMissingProperty.json", "actualMissingProperty.json.error"),
+       Arguments.of("expected.json", "actualDiffArraySize.json", "actualDiffArraySize.json.error"),
+       Arguments.of("expected.json", "actualDiffNumber.json", "actualDiffNumber.json.error"),
+       Arguments.of("expected.json", "actualMissingSubProperty.json", "actualMissingSubProperty.json.error"),
+       Arguments.of("expected.json", "actualExtraProperty.json", "actualExtraProperty.json.error")
+       );
+ }
+
+ @ParameterizedTest
+ @MethodSource("getCompareJSONParams")
+ public void testCompareJSON(String expectedFileName, String actualFileName, String expectedOutputFileName) throws IOException {
+   final String expectedXMLPath = ROOT_JSON_TEST_PATH.resolve(expectedFileName).toAbsolutePath().toString();
+   final String actualXMLPath = ROOT_JSON_TEST_PATH.resolve(actualFileName).toAbsolutePath().toString();
+
+   final String actualOutput = CompareUtilities.checkJsonSrcIsSame(getResourceAsString(expectedXMLPath), getResourceAsString(actualXMLPath), false);
+   if (expectedOutputFileName == null) {
+     assertNull(actualOutput);
+   } else {
+     final String expectedOutputPath = ROOT_JSON_TEST_PATH.resolve(expectedOutputFileName).toAbsolutePath().toString();
+     String expectedOutput = getResourceAsString(expectedOutputPath);
+     assertEquals(expectedOutput, actualOutput);
+   }
+ }
 }
