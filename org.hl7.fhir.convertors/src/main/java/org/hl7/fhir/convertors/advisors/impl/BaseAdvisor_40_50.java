7432b40c8496f98c49cace46b5ae5a5eca71b4e1
Grahame Grieve <grahameg@gmail.ccom>
2022-05-19 17:05:37 +1000

    Handle scope on TestScript R4 <-> r5 conversion

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/advisors/impl/BaseAdvisor_40_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/advisors/impl/BaseAdvisor_40_50.java
@@ -1,8 +1,16 @@
 package org.hl7.fhir.convertors.advisors.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
 import org.hl7.fhir.convertors.advisors.interfaces.BaseAdvisor50;
+import org.hl7.fhir.exceptions.FHIRException;
 
 public class BaseAdvisor_40_50 extends BaseAdvisor50<org.hl7.fhir.r4.model.Extension> {
+
+  private static final List<String> TestScriptIgnoredUrls = Arrays.asList("http://hl7.org/fhir/5.0/StructureDefinition/extension-TestScript.scope");
 
   public BaseAdvisor_40_50() {
   }
@@ -10,4 +18,16 @@ public class BaseAdvisor_40_50 extends BaseAdvisor50<org.hl7.fhir.r4.model.Exten
   public BaseAdvisor_40_50(Boolean failFast) {
     this.failFast = failFast;
   }
+  
+
+  @Override
+  public boolean ignoreExtension(@Nonnull String path,
+                                 @Nonnull String url) throws FHIRException {
+    List<String> paths = Arrays.asList(path.split(","));
+    if ((paths.get(paths.size() - 1).equals("TestScript")) && (TestScriptIgnoredUrls.contains(url))) {
+      return true;
+    } else
+      return false;
+  }
+
 }
