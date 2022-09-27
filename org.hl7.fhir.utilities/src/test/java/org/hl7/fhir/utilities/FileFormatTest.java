46a473aae587e74d5d89cdc9246e73b2f67cf096
dotasek <david.otasek@smilecdr.com>
2022-09-27 17:43:33 -0400

    Print warning for wrong file encoding

---
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/FileFormatTest.java
@@ -0,0 +1,33 @@
+package org.hl7.fhir.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class FileFormatTest {
+  @Test
+  public void testCurrentFileFormat() throws IOException {
+    ByteArrayOutputStream bo = new ByteArrayOutputStream();
+
+    FileFormat.checkCharsetAndWarnIfNotUTF8(new PrintStream(bo));
+
+    bo.flush();
+    String allWrittenLines = new String(bo.toByteArray());
+
+    if (Charset.defaultCharset().equals(StandardCharsets.UTF_8)) {
+      assertEquals(0, allWrittenLines.length());
+    }
+    else
+    {
+      assertThat(allWrittenLines, containsString("WARNING"));
+    }
+  }
+}
