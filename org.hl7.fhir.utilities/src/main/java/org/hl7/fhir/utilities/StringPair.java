9dd886ee2b540b8641e743f4adf2165cb8e0b2c2
Grahame Grieve <grahameg@gmail.ccom>
2022-10-13 06:49:08 +1100

    package fixes for r4/r4b package conversion

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/StringPair.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/StringPair.java
@@ -1,20 +1,20 @@
-package org.hl7.fhir.r5.utils.structuremap;
+package org.hl7.fhir.utilities;
 
 public class StringPair {
-  private String var;
-  private String desc;
+  private String name;
+  private String value;
 
-  public StringPair(String var, String desc) {
+  public StringPair(String name, String value) {
     super();
-    this.var = var;
-    this.desc = desc;
+    this.name = name;
+    this.value = value;
   }
 
-  public String getVar() {
-    return var;
+  public String getName() {
+    return name;
   }
 
-  public String getDesc() {
-    return desc;
+  public String getValue() {
+    return value;
   }
 }
