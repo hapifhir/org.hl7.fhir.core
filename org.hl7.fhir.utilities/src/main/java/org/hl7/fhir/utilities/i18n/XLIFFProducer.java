793abed5e4a461c93adc25525227f64564567e9b
Grahame Grieve <grahameg@gmail.ccom>
2022-10-19 20:47:18 +1100

    More work on language production

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/XLIFFProducer.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/XLIFFProducer.java
@@ -20,32 +20,29 @@ public class XLIFFProducer extends LanguageFileProducer {
   public void start(String fileName, String contextId, String contextDesc, String baseLang, String targetLang) {
     this.fileName = fileName;
     xml = new StringBuilder();
-    ln("<xliff xmlns=\"urn:oasis:names:tc:xliff:document:2.0\" version=\"2.0\"");
-    ln("  srcLang=\""+baseLang+"\" trgLang=\""+targetLang+"\">");
-    ln("  <file id=\""+contextId+"\">");
-    ln("    <notes>");
-    ln("      <note id=\"n1\">"+Utilities.escapeXml(contextDesc)+"</note>");
-    ln("    </notes>");
+    ln("<?xml version=\"1.0\" ?>\r\n");
+    ln("<xliff xmlns=\"urn:oasis:names:tc:xliff:document:2.0\" version=\"2.0\">");
+    ln("  <file source-language=\""+baseLang+"\" target-language=\""+targetLang+"\" id=\""+contextId+"\" original=\""+contextDesc+"\" datatype=\"KEYVALUEJSON\">");
+    ln("    <body>");
   }
 
   @Override
   public void makeEntry(String id, String ref, String context, String source, String target) {
     i++;
-    ln("  <unit id=\""+id+"\">");
+    ln("      <trans-unit id=\""+id+"\" resname=\""+ref+"\">");
     if (context != null) {
-      ln("  <notes>");
-      ln("    <note id=\"n"+i+"\">"+Utilities.escapeXml(context)+"</note>");
-      ln("  </notes>");
+      ln("        <notes>");
+      ln("          <note id=\"n"+i+"\">"+Utilities.escapeXml(context)+"</note>");
+      ln("        </notes>");
     }
-    ln("    <segment id=\""+ref+"\">");
-    ln("      <source><pc id=\"src"+i+"\">"+Utilities.escapeXml(source)+"</source>");
-    ln("      <target><pc id=\"tgt"+i+"\">"+Utilities.escapeXml(target)+"</target>");
-    ln("    </segment>");
-    ln("  </unit>");
+    ln("        <source>"+Utilities.escapeXml(source)+"</source>");
+    ln("        <target>"+Utilities.escapeXml(target)+"</target>");
+    ln("      </trans-unit>");
   }
 
   @Override
   public void finish() throws IOException {
+    ln("    </body>");
     ln("  </file>");
     ln("</xliff>");
     TextFile.stringToFile(xml.toString(), Utilities.path(getFolder(), fileName+".xliff"));
