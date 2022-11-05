87a5b50662a72ea6874122279e35eee347f4a8ce
Grahame Grieve <grahameg@gmail.ccom>
2022-11-05 11:28:44 +1100

    Fix issue with ActivityDefinition.kind = TASK not converting properly

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv43_50/resources43_50/ActivityDefinition43_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv43_50/resources43_50/ActivityDefinition43_50.java
@@ -295,6 +295,9 @@ public class ActivityDefinition43_50 {
       case SUPPLYREQUEST:
         tgt.setValue(org.hl7.fhir.r5.model.ActivityDefinition.RequestResourceTypes.SUPPLYREQUEST);
         break;
+      case TASK:
+        tgt.setValue(org.hl7.fhir.r5.model.ActivityDefinition.RequestResourceTypes.TASK);
+        break;
       case VISIONPRESCRIPTION:
         tgt.setValue(org.hl7.fhir.r5.model.ActivityDefinition.RequestResourceTypes.VISIONPRESCRIPTION);
         break;
@@ -349,6 +352,9 @@ public class ActivityDefinition43_50 {
         break;
       case SUPPLYREQUEST:
         tgt.setValue(org.hl7.fhir.r4b.model.ActivityDefinition.RequestResourceType.SUPPLYREQUEST);
+        break;
+      case TASK:
+        tgt.setValue(org.hl7.fhir.r4b.model.ActivityDefinition.RequestResourceType.TASK);
         break;
       case VISIONPRESCRIPTION:
         tgt.setValue(org.hl7.fhir.r4b.model.ActivityDefinition.RequestResourceType.VISIONPRESCRIPTION);
