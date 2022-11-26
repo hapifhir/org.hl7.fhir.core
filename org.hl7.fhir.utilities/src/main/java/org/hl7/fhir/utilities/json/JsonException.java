c0afaf461aaf2a5e68b5594c374d4c36e65bb44d
Grahame Grieve <grahameg@gmail.ccom>
2022-11-25 22:37:18 -0300

    minor improvements to json library

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/json/JsonException.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/json/JsonException.java
@@ -5,7 +5,7 @@ import org.hl7.fhir.exceptions.FHIRException;
 public class JsonException extends FHIRException {
 
   public JsonException(String msg) {
-    // TODO Auto-generated constructor stub
+    super(msg);
   }
 
 }
