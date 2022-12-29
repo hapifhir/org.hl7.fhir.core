9d6623df482e755237187bc66e3040cdfd3fa098
Grahame Grieve <grahameg@gmail.ccom>
2022-12-29 17:16:40 +1300

    more work

--- org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/profiles/PETests.java
+++ org.hl7.fhir.r5/src/test/java/org/hl7/fhir/r5/profiles/PETests.java
@@ -7,11 +7,14 @@ import org.hl7.fhir.r5.context.IWorkerContext;
 import org.hl7.fhir.r5.formats.IParser.OutputStyle;
 import org.hl7.fhir.r5.formats.JsonParser;
 import org.hl7.fhir.r5.model.Coding;
+import org.hl7.fhir.r5.model.Enumerations.ObservationStatus;
 import org.hl7.fhir.r5.model.Observation;
 import org.hl7.fhir.r5.model.Resource;
 import org.hl7.fhir.r5.profilemodel.PEDefinition;
 import org.hl7.fhir.r5.profilemodel.PEInstance;
 import org.hl7.fhir.r5.profilemodel.PEType;
+import org.hl7.fhir.r5.profilemodel.gen.ProfileExample;
+import org.hl7.fhir.r5.profilemodel.gen.ProfileExample.LOINCCodesForCholesterolInSerumPlasma;
 import org.hl7.fhir.r5.profilemodel.PEBuilder;
 import org.hl7.fhir.r5.profilemodel.PEBuilder.PEElementPropertiesPolicy;
 import org.hl7.fhir.r5.profilemodel.PEInstance.PEInstanceDataKind;
@@ -346,6 +349,11 @@ public class PETests {
     Assertions.assertEquals("TestProfile.complex.slice2[0]", slice2.getPath());
     Assertions.assertEquals(PEInstanceDataKind.Primitive, slice2.getDataKind());
     Assertions.assertEquals("A string value", slice2.getPrimitiveAsString());
+    
+    
+    ProfileExample ex = new ProfileExample(obs);
+    Assertions.assertEquals(ObservationStatus.FINAL, ex.getStatus());
+    Assertions.assertEquals(LOINCCodesForCholesterolInSerumPlasma.L14647_2, ex.getSimple());
   }
 
   
