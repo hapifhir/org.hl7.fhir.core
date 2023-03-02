b18e541b5f40cde6fdf6f08775aaf8ff8f184197
Grahame Grieve <grahameg@gmail.com>
2023-03-03 10:35:33 +1100

    Gg 202303 r5 fixes (#1148)
    * update test package dependency
    
    * Add back code missed when regenerating R5
    
    * fix NPE
    
    * update conversions for discovered issues for R5
    
    ---------
    
    Co-authored-by: Grahame Grieve <grahameg@gmail.ccom>

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Url40_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv40_50/datatypes40_50/primitive40_50/Url40_50.java
@@ -2,6 +2,8 @@ package org.hl7.fhir.convertors.conv40_50.datatypes40_50.primitive40_50;
 
 import org.hl7.fhir.convertors.context.ConversionContext40_50;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r4.model.UrlType;
+import org.hl7.fhir.r5.model.DataType;
 
 public class Url40_50 {
   public static org.hl7.fhir.r5.model.UrlType convertUrl(org.hl7.fhir.r4.model.UrlType src) throws FHIRException {
@@ -15,4 +17,29 @@ public class Url40_50 {
     ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
     return tgt;
   }
+  
+  public static org.hl7.fhir.r5.model.UrlType convertUriToUrl(org.hl7.fhir.r4.model.UriType src) throws FHIRException {
+    org.hl7.fhir.r5.model.UrlType tgt = src.hasValue() ? new org.hl7.fhir.r5.model.UrlType(src.getValueAsString()) : new org.hl7.fhir.r5.model.UrlType();
+    ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
+    return tgt;
+  }
+
+  public static org.hl7.fhir.r4.model.UriType convertUrlToUri(org.hl7.fhir.r5.model.UrlType src) throws FHIRException {
+    org.hl7.fhir.r4.model.UriType tgt = src.hasValue() ? new org.hl7.fhir.r4.model.UriType(src.getValueAsString()) : new org.hl7.fhir.r4.model.UriType();
+    ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
+    return tgt;
+  }
+  
+  public static org.hl7.fhir.r5.model.UriType convertUrlToUri(org.hl7.fhir.r4.model.UrlType src) throws FHIRException {
+    org.hl7.fhir.r5.model.UriType tgt = src.hasValue() ? new org.hl7.fhir.r5.model.UriType(src.getValueAsString()) : new org.hl7.fhir.r5.model.UriType();
+    ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
+    return tgt;
+  }
+
+  public static org.hl7.fhir.r4.model.UrlType convertUriToUrl(org.hl7.fhir.r5.model.UriType src) throws FHIRException {
+    org.hl7.fhir.r4.model.UrlType tgt = src.hasValue() ? new org.hl7.fhir.r4.model.UrlType(src.getValueAsString()) : new org.hl7.fhir.r4.model.UrlType();
+    ConversionContext40_50.INSTANCE.getVersionConvertor_40_50().copyElement(src, tgt);
+    return tgt;
+  }
+
 }
