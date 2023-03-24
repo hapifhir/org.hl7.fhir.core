467e921e515033d29a356935c66130a9e4ba6c75
Grahame Grieve <grahameg@gmail.ccom>
2023-03-24 23:09:01 +1100

    fix NPEs

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Element.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/model/Element.java
@@ -427,6 +427,8 @@ public abstract class Element extends Base implements IBaseHasExtensions, IBaseE
       return null; 
     if (ext.size() > 1) 
       throw new FHIRException("Multiple matching extensions found for extension '"+theUrl+"'");
+    if (!ext.get(0).hasValue())
+      return null;
     if (!ext.get(0).getValue().isPrimitive())
       throw new FHIRException("Extension '"+theUrl+"' could not be converted to a string");
     return ext.get(0).getValue().primitiveValue();
