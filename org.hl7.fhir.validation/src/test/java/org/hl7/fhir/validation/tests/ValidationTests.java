4f8a8108a45729fe0ed7db638ee6120af01413d9
Grahame Grieve <grahameg@gmail.ccom>
2023-03-29 20:58:06 +1100

    reduce memory use in tests?

--- org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/ValidationTests.java
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/ValidationTests.java
@@ -78,6 +78,7 @@ import org.hl7.fhir.validation.instance.InstanceValidator;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -134,6 +135,11 @@ public class ValidationTests implements IEvaluationContext, IValidatorResourceFe
     this.content = content;
   }
 
+  @AfterAll
+  public void cleanup() {
+    ve = null;
+  }
+  
   @SuppressWarnings("deprecation")
   @Test
   public void test() throws Exception {
