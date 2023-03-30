177713410cdc43ba274d4ae81bb4bafc6847bf8d
Grahame Grieve <grahameg@gmail.ccom>
2023-03-30 23:30:23 +1100

    Work around issue with R5 path in extensions pack

---
+++ org.hl7.fhir.r4b/src/main/java/org/hl7/fhir/r4b/utils/DataTypeVisitor.java
@@ -0,0 +1,69 @@
+package org.hl7.fhir.r4b.utils;
+
+import org.hl7.fhir.r4b.model.Base;
+import org.hl7.fhir.r4b.model.DataType;
+import org.hl7.fhir.r4b.model.Property;
+import org.hl7.fhir.r4b.model.Resource;
+import org.hl7.fhir.utilities.Utilities;
+
+public class DataTypeVisitor {
+
+  public interface IDatatypeVisitor<T extends DataType> {
+    Class<T> classT();
+    boolean visit(String path, T node);
+  }
+
+  private boolean anyFalse;
+  private boolean anyTrue;
+  private int nodeCount;
+  private int selectedCount;
+  
+  public <T extends DataType> void visit(Resource resource, IDatatypeVisitor<T> visitor) {
+    visitNode(resource.fhirType(), resource, visitor);
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T extends DataType> void visitNode(String path, Base node, IDatatypeVisitor<T> visitor) {
+    nodeCount++;
+    if (node instanceof DataType && visitor.classT().isInstance(node)) {
+      selectedCount++;
+      boolean ok = visitor.visit(path, (T) node);
+      if (ok) {
+        anyTrue = true;
+      } else {
+        anyFalse = true;
+      }
+    }
+    for (Property p : node.children()) {
+      if (p.isList()) {
+        int i = 0;
+        for (Base b : p.getValues()) {
+          visitNode(path+"."+p.getName()+"["+i+"]", b, visitor);
+          i++;
+        }
+      } else {
+        for (Base b : p.getValues()) {
+          visitNode(path+"."+p.getName(), b, visitor);
+        }
+      }
+    }
+  }
+
+  public boolean isAnyFalse() {
+    return anyFalse;
+  }
+
+  public boolean isAnyTrue() {
+    return anyTrue;
+  }
+
+  public int getNodeCount() {
+    return nodeCount;
+  }
+
+  public int getSelectedCount() {
+    return selectedCount;
+  }
+  
+  
+}
