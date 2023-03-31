6d9a63d3d4d157d2b03b83adc5ac29abebcce41a
James Agnew <jamesagnew@gmail.com>
2023-03-31 18:17:55 -0400

    Make XhtmlNode validation case insensitive

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xhtml/XhtmlNode.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xhtml/XhtmlNode.java
@@ -1,5 +1,6 @@
 package org.hl7.fhir.utilities.xhtml;
 
+import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 /*
@@ -36,8 +37,11 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.exceptions.FHIRFormatError;
@@ -51,6 +55,20 @@ import ca.uhn.fhir.model.primitive.XhtmlDt;
 @ca.uhn.fhir.model.api.annotation.DatatypeDef(name="xhtml")
 public class XhtmlNode extends XhtmlFluent implements IBaseXhtml {
   private static final long serialVersionUID = -4362547161441436492L;
+  private static final Set<String> ALLOWED_ATTRIBUTES = Set.of("title", "style", "class", "id", "lang", "xml:lang", "dir", "accesskey", "tabindex",
+    // tables
+    "span", "width", "align", "valign", "char", "charoff", "abbr", "axis", "headers", "scope", "rowspan", "colspan");
+  private static final Set<String> ALLOWED_QUALIFIED_ATTRIBUTES = Set.of("div.xmlns", "blockquote.cite", "q.cite",
+    "a.charset", "a.type", "a.name", "a.href", "a.hreflang", "a.rel", "a.rev", "a.shape", "a.coords", "img.border", "img.src",
+    "img.alt", "img.longdesc", "img.height", "img.width", "img.usemap", "img.ismap", "map.name", "area.shape",
+    "area.coords", "area.href", "area.nohref", "area.alt", "table.summary", "table.width", "table.border",
+    "table.frame", "table.rules", "table.cellspacing", "table.cellpadding", "pre.space", "td.nowrap");
+  private static final Set<String> ALLOWED_TAGS = Set.of("p", "br", "div", "h1", "h2", "h3", "h4", "h5", "h6", "a", "span", "b", "em", "i", "strong",
+    "small", "big", "tt", "dfn", "q", "var", "abbr", "acronym", "cite", "blockquote", "hr", "address", "bdo", "kbd", "sub", "sup",
+    "ul", "ol", "li", "dl", "dt", "dd", "pre", "table", "caption", "colgroup", "col", "thead", "tr", "tfoot", "tbody", "th", "td",
+    "code", "samp", "img", "map", "area");
+  private static final Set<String> BANNED_TAGS_IN_LINK = Set.of("a");
+  private static final Set<String> BANNED_TAGS_IN_PARAGRAPH = Set.of("div", "blockquote", "table", "ol", "ul", "p");
 
 
   public static class Location implements Serializable {
@@ -159,34 +177,28 @@ public class XhtmlNode extends XhtmlFluent implements IBaseXhtml {
   public void validate(List<String> errors, String path, boolean inResource, boolean inPara, boolean inLink) {
     if (nodeType == NodeType.Element || nodeType == NodeType.Document) {
       path = Utilities.noString(path) ? name : path+"/"+name;
+      String nameLc = defaultString(name).toLowerCase(Locale.US);
+
       if (inResource) {
-        if (!Utilities.existsInList(name, "p", "br", "div", "h1", "h2", "h3", "h4", "h5", "h6", "a", "span", "b", "em", "i", "strong",
-            "small", "big", "tt", "small", "dfn", "q", "var", "abbr", "acronym", "cite", "blockquote", "hr", "address", "bdo", "kbd", "q", "sub", "sup",
-            "ul", "ol", "li", "dl", "dt", "dd", "pre", "table", "caption", "colgroup", "col", "thead", "tr", "tfoot", "tbody", "th", "td",
-            "code", "samp", "img", "map", "area")) {
+        if (!ALLOWED_TAGS.contains(nameLc)) {
           errors.add("Error at "+path+": Found "+name+" in a resource");          
         }      
         if (hasAttributes()) {
           for (String an : attributes.keySet()) {
-            boolean ok = an.startsWith("xmlns") || Utilities.existsInList(an,
-                "title", "style", "class", "ID", "lang", "xml:lang", "dir", "accesskey", "tabindex",
-                // tables
-                "span", "width", "align", "valign", "char", "charoff", "abbr", "axis", "headers", "scope", "rowspan", "colspan") ||
-                Utilities.existsInList(name + "." + an, "a.href", "a.name", "img.src", "img.border", "div.xmlns", "blockquote.cite", "q.cite",
-                    "a.charset", "a.type", "a.name", "a.href", "a.hreflang", "a.rel", "a.rev", "a.shape", "a.coords", "img.src",
-                    "img.alt", "img.longdesc", "img.height", "img.width", "img.usemap", "img.ismap", "map.name", "area.shape",
-                    "area.coords", "area.href", "area.nohref", "area.alt", "table.summary", "table.width", "table.border",
-                    "table.frame", "table.rules", "table.cellspacing", "table.cellpadding", "pre.space", "td.nowrap"
-                    );
-            if (!ok)
-              errors.add("Error at "+path+": Found attribute "+name+"."+an+" in a resource");          
+            String attributeLc = an.toLowerCase(Locale.US);
+            boolean ok = an.startsWith("xmlns") ||
+              ALLOWED_ATTRIBUTES.contains(attributeLc) ||
+              ALLOWED_QUALIFIED_ATTRIBUTES.contains(nameLc + "." + attributeLc);
+            if (!ok) {
+              errors.add("Error at " + path + ": Found attribute " + name + "." + an + " in a resource");
+            }
           }
         }
       }
-      if (inPara && Utilities.existsInList(name, "div",  "blockquote", "table", "ol", "ul", "p")) {
+      if (inPara && BANNED_TAGS_IN_PARAGRAPH.contains(nameLc)) {
         errors.add("Error at "+path+": Found "+name+" inside an html paragraph");
       }
-      if (inLink && Utilities.existsInList(name, "a")) {
+      if (inLink && BANNED_TAGS_IN_LINK.contains(nameLc)) {
         errors.add("Error at "+path+": Found an <a> inside an <a> paragraph");
       }
 
--- org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/tests/XhtmlNodeTest.java
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/tests/XhtmlNodeTest.java
@@ -3,6 +3,7 @@ package org.hl7.fhir.utilities.tests;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.exceptions.FHIRFormatError;
@@ -12,8 +13,12 @@ import org.hl7.fhir.utilities.xhtml.XhtmlNode;
 import org.hl7.fhir.utilities.xhtml.XhtmlParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class XhtmlNodeTest {
 
@@ -28,12 +33,12 @@ public class XhtmlNodeTest {
     // Entity that appears in XHTML not not in XML
     XhtmlNode node = new XhtmlNode();
     node.setValueAsString("<div>&reg;</div>");
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">®</div>", node.getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">®</div>", node.getValueAsString());
 
     // Entity that appears in both
     node = new XhtmlNode();
     node.setValueAsString("<div>&lt;</div>");
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">&lt;</div>", node.getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">&lt;</div>", node.getValueAsString());
   }
 
   /**
@@ -43,24 +48,24 @@ public class XhtmlNodeTest {
   public void testLangAttributePreserved() {
     XhtmlNode dt = new XhtmlNode();
     dt.setValueAsString("<div xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\">help i'm a bug</div>");
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\">help i'm a bug</div>", dt.getValueAsString());
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\">help i'm a bug</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\">help i'm a bug</div>", dt.getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\">help i'm a bug</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
   }
 
   @Test
   public void testParseRsquo() {
     XhtmlNode dt = new XhtmlNode();
     dt.setValueAsString("It&rsquo;s January again");
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">It’s January again</div>", dt.getValueAsString());
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">It’s January again</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">It’s January again</div>", dt.getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">It’s January again</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
   }
 
   @Test
   public void testProcessingInstructionNotPreserved() {
     XhtmlNode dt = new XhtmlNode();
     dt.setValueAsString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><div xmlns=\"http://www.w3.org/1999/xhtml\">help i'm a bug</div>");
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">help i'm a bug</div>", dt.getValueAsString());
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">help i'm a bug</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">help i'm a bug</div>", dt.getValueAsString());
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\">help i'm a bug</div>", new XhtmlNode().setValue(dt.getValue()).getValueAsString());
   }
 
   @Test
@@ -75,7 +80,7 @@ public class XhtmlNodeTest {
     String output = node.getValueAsString();
 //    ourLog.info(output);
 
-    Assertions.assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\"><img src=\"http://pbs.twimg.com/profile_images/544507893991485440/r_vo3uj2_bigger.png\" alt=\"Twitter Avatar\"/>@fhirabend</div>", output);
+    assertEquals("<div xmlns=\"http://www.w3.org/1999/xhtml\"><img src=\"http://pbs.twimg.com/profile_images/544507893991485440/r_vo3uj2_bigger.png\" alt=\"Twitter Avatar\"/>@fhirabend</div>", output);
   }
 
   @Test
@@ -119,24 +124,24 @@ public class XhtmlNodeTest {
   public void testParseSvg() throws FHIRFormatError, IOException {
     XhtmlNode x = new XhtmlParser().parse(BaseTestingUtilities.loadTestResource("xhtml", "svg.html"), "svg");
 
-    Assertions.assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(1).getAttributes().get("xmlns"));
-    Assertions.assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
+    assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(1).getAttributes().get("xmlns"));
+    assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
   }
 
   @Test
   public void testParseSvgNotRoot() throws FHIRFormatError, IOException {
     XhtmlNode x = new XhtmlParser().parse(BaseTestingUtilities.loadTestResource("xhtml", "non-root-svg.html"), "div");
 
-    Assertions.assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns"));
-    Assertions.assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
+    assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns"));
+    assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
   }
 
   @Test
   public void testParseNamespacedSvgNotRoot() throws FHIRFormatError, IOException {
     XhtmlNode x = new XhtmlParser().parse(BaseTestingUtilities.loadTestResource("xhtml", "namespaced-non-root-svg.html"), "div");
 
-    Assertions.assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns"));
-    Assertions.assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
+    assertEquals("http://www.w3.org/2000/svg", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns"));
+    assertEquals("http://www.w3.org/1999/xlink", x.getChildNodes().get(0).getChildNodes().get(1).getAttributes().get("xmlns:xlink"));
   }
 
 
@@ -147,8 +152,23 @@ public class XhtmlNodeTest {
    
     
     String xml = new XhtmlComposer(false, false).compose(x);
-    Assertions.assertEquals(src.trim(), xml.trim());
+    assertEquals(src.trim(), xml.trim());
   }
 
+  @ParameterizedTest
+  @CsvSource({
+    "<div><SPAN ID=\"foo\">hello</SPAN></div> , true",
+    "<div><span id=\"foo\">hello</span></div> , true",
+    "<div><SPAN ONCLICK=\"hello()\">hello</SPAN></div> , false",
+    "<div><span onclick=\"hello()\">hello</span></div> , false"
+  })
+  public void testValidateIsCaseInsensitive(String theHtml, boolean theExpectedValid) {
+    XhtmlNode node = new XhtmlNode();
+    node.setValueAsString(theHtml);
+    ArrayList<String> errors = new ArrayList<>();
+    node.validate(errors, "", true, false, false);
+
+    assertEquals(theExpectedValid, errors.isEmpty());
+  }
   
 }
