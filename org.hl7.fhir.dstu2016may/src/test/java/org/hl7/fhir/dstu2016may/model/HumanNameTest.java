aec25d37886f27d402a87efcf14f5e89d258d431
Amir Toosi <54131619+amir-kt@users.noreply.github.com>
2023-04-13 04:31:58 +1000

    Fix HumanName getNameAsSingleString never returning text element (#1207)
    * Fix HumanName getNameAsSingleString never returning text element
    
    * Add test + gentle rename in joinStringsSpaceSeparated
    
    * removed unused variable from unit test. added fix for getNameAsSingleString to r5.
    
    * changed variable names in joinStringSpaceSeparated to more meaningful names
    
    * Add tests and fixes for dstu2016may, dstu3, and r4b + gentle refactor
    
    ---------
    
    Co-authored-by: dotasek <david.otasek@smilecdr.com>

---
+++ org.hl7.fhir.dstu2016may/src/test/java/org/hl7/fhir/dstu2016may/model/HumanNameTest.java
@@ -0,0 +1,28 @@
+package org.hl7.fhir.dstu2016may.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HumanNameTest {
+
+  @Test
+  public void getNameAsSingleStringWithoutFamilyElement() {
+    final String expected = "dummy value";
+    HumanName humanName = new HumanName()
+      .setTextElement(new StringType(expected));
+
+    String actual = humanName.getNameAsSingleString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void getNameAsSingleStringWithFamilyElement() {
+    final String expected = "good value";
+    HumanName humanName = new HumanName()
+      .addFamily(expected);
+
+    String actual = humanName.getNameAsSingleString();
+    assertEquals(expected, actual);
+  }
+}
---
+++ org.hl7.fhir.dstu3/src/test/java/org/hl7/fhir/dstu3/model/HumanNameTest.java
@@ -0,0 +1,28 @@
+package org.hl7.fhir.dstu3.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HumanNameTest {
+
+  @Test
+  public void getNameAsSingleStringWithoutFamilyElement() {
+    final String expected = "dummy value";
+    HumanName humanName = new HumanName()
+      .setTextElement(new StringType(expected));
+
+    String actual = humanName.getNameAsSingleString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void getNameAsSingleStringWithFamilyElement() {
+    final String expected = "good value";
+    HumanName humanName = new HumanName()
+      .setFamily(expected);
+
+    String actual = humanName.getNameAsSingleString();
+    assertEquals(expected, actual);
+  }
+}
