60bd0d5ec10d5d8bf7986a26a3f758b3675007d8
dotasek <david.otasek@smilecdr.com>
2023-04-14 09:36:52 -0400

    WIP SubscriptionTopic conversion tests + initial work

---
+++ org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/conv43_50/SubscriptionTopic43_50Test.java
@@ -0,0 +1,35 @@
+package org.hl7.fhir.convertors.conv43_50;
+
+import org.hl7.fhir.convertors.factory.VersionConvertorFactory_43_50;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SubscriptionTopic43_50Test {
+
+  @Test
+  @DisplayName("Test r5 -> r4 SubscriptionTopic conversion.")
+  public void testR5_R4() throws IOException {
+    InputStream r5_input = this.getClass().getResourceAsStream("/subscription_topic_50.json");
+
+    org.hl7.fhir.r5.model.SubscriptionTopic r5_actual = (org.hl7.fhir.r5.model.SubscriptionTopic) new org.hl7.fhir.r5.formats.JsonParser().parse(r5_input);
+    org.hl7.fhir.r4b.model.Resource r4_conv = VersionConvertorFactory_43_50.convertResource(r5_actual);
+
+    org.hl7.fhir.r4b.formats.XmlParser r4_parser = new org.hl7.fhir.r4b.formats.XmlParser();
+
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    r4_parser.compose(stream, r4_conv);
+
+    org.hl7.fhir.r4b.model.Resource r4_streamed = new org.hl7.fhir.r4b.formats.XmlParser().parse(new ByteArrayInputStream(stream.toByteArray()));
+    org.hl7.fhir.r5.model.Resource r5_conv = VersionConvertorFactory_43_50.convertResource(r4_streamed);
+
+    assertTrue(r5_actual.equalsDeep(r5_conv), "should be the same");
+  }
+
+}
