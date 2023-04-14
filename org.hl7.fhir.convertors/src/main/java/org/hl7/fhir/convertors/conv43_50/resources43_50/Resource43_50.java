a734a2f819dae350de0348b6e895ba7f21b5c1e1
dotasek <david.otasek@smilecdr.com>
2023-04-14 09:36:52 -0400

    WIP SubscriptionTopic conversion tests + initial work

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv43_50/resources43_50/Resource43_50.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/conv43_50/resources43_50/Resource43_50.java
@@ -2,6 +2,7 @@ package org.hl7.fhir.convertors.conv43_50.resources43_50;
 
 import org.hl7.fhir.convertors.advisors.impl.BaseAdvisor_43_50;
 import org.hl7.fhir.convertors.context.ConversionContext43_50;
+import org.hl7.fhir.convertors.conv43_50.SubscriptionTopic43_50;
 import org.hl7.fhir.convertors.conv43_50.datatypes43_50.primitive43_50.Code43_50;
 import org.hl7.fhir.convertors.conv43_50.datatypes43_50.primitive43_50.Id43_50;
 import org.hl7.fhir.convertors.conv43_50.datatypes43_50.primitive43_50.Uri43_50;
@@ -9,6 +10,7 @@ import org.hl7.fhir.convertors.conv43_50.datatypes43_50.special43_50.Extension43
 import org.hl7.fhir.convertors.conv43_50.datatypes43_50.special43_50.Meta43_50;
 import org.hl7.fhir.convertors.conv43_50.datatypes43_50.special43_50.Narrative43_50;
 import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.r5.model.SubscriptionTopic;
 
 public class Resource43_50 {
 
@@ -244,6 +246,8 @@ public class Resource43_50 {
       return StructureDefinition43_50.convertStructureDefinition((org.hl7.fhir.r4b.model.StructureDefinition) src);
     if (src instanceof org.hl7.fhir.r4b.model.StructureMap)
       return StructureMap43_50.convertStructureMap((org.hl7.fhir.r4b.model.StructureMap) src);
+    if (src instanceof org.hl7.fhir.r4b.model.SubscriptionTopic)
+      return SubscriptionTopic43_50.convertSubscriptionTopic((org.hl7.fhir.r4b.model.SubscriptionTopic)src);
     if (src instanceof org.hl7.fhir.r4b.model.Substance)
       return Substance43_50.convertSubstance((org.hl7.fhir.r4b.model.Substance) src);
     if (src instanceof org.hl7.fhir.r4b.model.SupplyDelivery)
@@ -478,6 +482,8 @@ public class Resource43_50 {
       return StructureDefinition43_50.convertStructureDefinition((org.hl7.fhir.r5.model.StructureDefinition) src);
     if (src instanceof org.hl7.fhir.r5.model.StructureMap)
       return StructureMap43_50.convertStructureMap((org.hl7.fhir.r5.model.StructureMap) src);
+    if (src instanceof org.hl7.fhir.r5.model.SubscriptionTopic)
+      return SubscriptionTopic43_50.convertSubscriptionTopic((org.hl7.fhir.r5.model.SubscriptionTopic)src);
     if (src instanceof org.hl7.fhir.r5.model.Substance)
       return Substance43_50.convertSubstance((org.hl7.fhir.r5.model.Substance) src);
     if (src instanceof org.hl7.fhir.r5.model.SupplyDelivery)
