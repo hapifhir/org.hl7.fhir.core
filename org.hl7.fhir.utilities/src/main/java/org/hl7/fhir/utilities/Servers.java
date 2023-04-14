48e86fa9055371f3dbdeb77688bcf9bf94e18da9
Grahame Grieve <grahameg@gmail.ccom>
2023-04-14 22:39:48 +1000

    fix failing tests

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/Servers.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/Servers.java
@@ -6,5 +6,8 @@ public class Servers {
   public static final String TX_SERVER_DEV = "http://tx-dev.fhir.org";
   public static final String TX_SERVER_LOCAL = "http://local.fhir.org";
   
+  public static boolean isTxFhirOrg(String s) {
+    return Utilities.existsInList(s.replace("https://", "http://"), TX_SERVER_PROD, TX_SERVER_DEV, TX_SERVER_LOCAL);
+  }
 
 }
