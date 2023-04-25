3ed7813973e044088b6e24bad42eec97acba1ff1
Grahame Grieve <grahameg@gmail.ccom>
2023-04-25 21:51:31 +1000

    language translations support in validator

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/elementmodel/LanguageUtils.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/elementmodel/LanguageUtils.java
@@ -1,9 +1,18 @@
 package org.hl7.fhir.r5.elementmodel;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hl7.fhir.r5.context.ContextUtilities;
 import org.hl7.fhir.r5.context.IWorkerContext;
+import org.hl7.fhir.utilities.TextFile;
+import org.hl7.fhir.utilities.Utilities;
 import org.hl7.fhir.utilities.i18n.LanguageFileProducer;
 import org.hl7.fhir.utilities.i18n.LanguageFileProducer.LanguageProducerLanguageSession;
 import org.hl7.fhir.utilities.i18n.LanguageFileProducer.TextUnit;
+import org.hl7.fhir.utilities.i18n.LanguageFileProducer.TranslationUnit;
 
 /**
  * in here:
@@ -21,6 +30,7 @@ import org.hl7.fhir.utilities.i18n.LanguageFileProducer.TextUnit;
 public class LanguageUtils {
 
   IWorkerContext context;
+  private List<String> crlist;
   
   
   public LanguageUtils(IWorkerContext context) {
@@ -29,27 +39,150 @@ public class LanguageUtils {
   }
 
   public void generateTranslations(Element resource, LanguageProducerLanguageSession session) {
-    translate(resource, session);
+    translate(null, resource, session);
   }
   
-  private void translate(Element element, LanguageProducerLanguageSession langSession) {
+  
+  private void translate(Element parent, Element element, LanguageProducerLanguageSession langSession) {
     if (element.isPrimitive() && isTranslatable(element)) {
       String base = element.primitiveValue();
       if (base != null) {
-        String translation = element.getTranslation(langSession.getTargetLang());
-        langSession.entry(new TextUnit(element.getPath(), base, translation));
+        String translation = getSpecialTranslation(parent, element, langSession.getTargetLang());
+        if (translation == null) {
+          translation = element.getTranslation(langSession.getTargetLang());
+        }
+        langSession.entry(new TextUnit(pathForElement(element), base, translation));
       }
     }
     for (Element c: element.getChildren()) {
-      translate(c, langSession);
+      if (!c.getName().equals("designation")) {
+        translate(element, c, langSession);
+      }
     }
   }
   
-  private boolean isTranslatable(Element element) {
-    return element.getProperty().isTranslatable();
+
+  private String getSpecialTranslation(Element parent, Element element, String targetLang) {
+    if (parent == null) {
+      return null;
+    }
+    if (Utilities.existsInList(pathForElement(parent), "CodeSystem.concept", "CodeSystem.concept.concept") && "CodeSystem.concept.display".equals(pathForElement(element))) {
+      return getDesignationTranslation(parent, targetLang);
+    }
+    if (Utilities.existsInList(pathForElement(parent), "ValueSet.compose.include.concept") && "ValueSet.compose.include.concept.display".equals(pathForElement(element))) {
+      return getDesignationTranslation(parent, targetLang);
+    }
+    if (Utilities.existsInList(pathForElement(parent), "ValueSet.expansion.contains", "ValueSet.expansion.contains.contains") && "ValueSet.expansion.contains.display".equals(pathForElement(element))) {
+      return getDesignationTranslation(parent, targetLang);
+    }
+    return null;
   }
 
-  public static boolean matches(String dstLang, String srcLang) {
+  private String getDesignationTranslation(Element parent, String targetLang) {
+    for (Element e : parent.getChildren("designation")) {
+      String lang = e.getNamedChildValue("language");
+      if (langsMatch(targetLang, lang)) {
+        return e.getNamedChildValue("value");
+      }
+    }
+    return null;
+  }
+
+  private boolean isTranslatable(Element element) {    
+    return element.getProperty().isTranslatable() && !Utilities.existsInList(pathForElement(element), "CanonicalResource.version");
+  }
+
+  private String pathForElement(Element element) {
+    // special case support for metadata elements prior to R5:
+    if (crlist == null) {
+      crlist = new ContextUtilities(context).getCanonicalResourceNames();
+    }
+    String bp = element.getBasePath();
+    if (crlist.contains(element.getProperty().getStructure().getType())) {
+      String fp = bp.replace(element.getProperty().getStructure().getType()+".", "CanonicalResource.");
+      if (Utilities.existsInList(fp,
+         "CanonicalResource.url", "CanonicalResource.identifier", "CanonicalResource.version", "CanonicalResource.name", 
+         "CanonicalResource.title", "CanonicalResource.status", "CanonicalResource.experimental", "CanonicalResource.date",
+         "CanonicalResource.publisher", "CanonicalResource.contact", "CanonicalResource.description", "CanonicalResource.useContext", 
+         "CanonicalResource.jurisdiction"))  {
+        return fp;
+      }
+    }
+    return bp; 
+  }
+  
+  public int importFromTranslations(Element resource, Set<TranslationUnit> translations) {
+    return importFromTranslations(null, resource, translations);
+  }
+  
+  private int importFromTranslations(Element parent, Element element, Set<TranslationUnit> translations) {
+    int t = 0;
+    if (element.isPrimitive() && isTranslatable(element)) {
+      String base = element.primitiveValue();
+      if (base != null) {
+        Set<TranslationUnit> tlist = findTranslations(pathForElement(element), base, translations);
+        for (TranslationUnit translation : tlist) {
+          t++;
+          if (!handleAsSpecial(parent, element, translation)) {
+            element.setTranslation(translation.getLanguage(), translation.getTgtText());
+          }
+        }
+      }
+    }
+    for (Element c: element.getChildren()) {
+      if (!c.getName().equals("designation")) {
+        t = t + importFromTranslations(element, c, translations);
+      }
+    }
+    return t;
+  }
+
+  private boolean handleAsSpecial(Element parent, Element element, TranslationUnit translation) {
+    if (parent == null) {
+      return false;
+    }
+    if (Utilities.existsInList(pathForElement(parent), "CodeSystem.concept", "CodeSystem.concept.concept") && "CodeSystem.concept.display".equals(pathForElement(element))) {
+      return setDesignationTranslation(parent, translation.getLanguage(), translation.getTgtText());
+    }
+    if (Utilities.existsInList(pathForElement(parent), "ValueSet.compose.include.concept") && "ValueSet.compose.include.concept.display".equals(pathForElement(element))) {
+      return setDesignationTranslation(parent, translation.getLanguage(), translation.getTgtText());
+    }
+    if (Utilities.existsInList(pathForElement(parent), "ValueSet.expansion.contains", "ValueSet.expansion.contains.contains") && "ValueSet.expansion.contains.display".equals(pathForElement(element))) {
+      return setDesignationTranslation(parent, translation.getLanguage(), translation.getTgtText());
+    }
+    return false;
+  }
+
+  private boolean setDesignationTranslation(Element parent, String targetLang, String translation) {
+    for (Element e : parent.getChildren("designation")) {
+      String lang = e.getNamedChildValue("language");
+      if (langsMatch(targetLang, lang)) {
+        Element value = e.getNamedChild("value");
+        if (value != null) {
+          value.setValue(translation);
+        } else {
+          e.addElement("value").setValue(translation);
+        }
+        return true;
+      }
+    }
+    Element d = parent.addElement("designation");
+    d.addElement("language").setValue(targetLang);
+    d.addElement("value").setValue(translation);
+    return true;
+  }
+
+  private Set<TranslationUnit> findTranslations(String path, String src, Set<TranslationUnit> translations) {
+    Set<TranslationUnit> res = new HashSet<>();
+    for (TranslationUnit translation : translations) {
+      if (path.equals(translation.getContext()) && src.equals(translation.getSrcText())) {
+        res.add(translation);
+      }
+    }
+    return res;
+  }
+
+  public static boolean langsMatch(String dstLang, String srcLang) {
     return dstLang == null ? false : dstLang.equals(srcLang);
   }
 }
--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/PoGetTextProducer.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/PoGetTextProducer.java
@@ -1,7 +1,10 @@
 package org.hl7.fhir.utilities.i18n;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,6 +12,7 @@ import org.hl7.fhir.utilities.TextFile;
 import org.hl7.fhir.utilities.Utilities;
 import org.hl7.fhir.utilities.i18n.LanguageFileProducer.LanguageProducerLanguageSession;
 import org.hl7.fhir.utilities.i18n.LanguageFileProducer.LanguageProducerSession;
+import org.hl7.fhir.utilities.i18n.LanguageFileProducer.TranslationUnit;
 import org.hl7.fhir.utilities.i18n.PoGetTextProducer.POGetTextLanguageProducerLanguageSession;
 import org.hl7.fhir.utilities.i18n.PoGetTextProducer.POGetTextProducerSession;
 import org.hl7.fhir.utilities.i18n.XLIFFProducer.XLiffLanguageProducerLanguageSession;
@@ -19,6 +23,10 @@ public class PoGetTextProducer extends LanguageFileProducer {
 
   public PoGetTextProducer(String folder) {
     super(folder);
+  }
+
+  public PoGetTextProducer() {
+    super();
   }
 
   @Override
@@ -90,13 +98,49 @@ public class PoGetTextProducer extends LanguageFileProducer {
   }
 
   @Override
-  public List<TextUnit> loadTranslations(String id, String baseLang, String targetLang) throws IOException {
-    List<TextUnit> res = new ArrayList<>();
-    File f = new File(getFileName(id, baseLang, targetLang));
-    if (f.exists()) {
-      
+  public List<TranslationUnit> loadSource(InputStream source) throws IOException {
+    List<TranslationUnit> list = new ArrayList<>();
+    InputStreamReader r = new InputStreamReader(source, "UTF-8"); // leave charset out for default
+    BufferedReader br = new BufferedReader(r);
+    String lang = null;
+    String s;
+    TranslationUnit tu = null;
+    while ((s = Utilities.stripBOM(br.readLine())) != null) {
+       if (!Utilities.noString(s)) {
+         if (s.trim().startsWith("#")) {
+           if (lang == null) {
+             String[] p = s.substring(1).trim().split("\\-\\>");
+             if (p.length != 2) {
+               throw new IOException("Encountered unexpected starting line '"+s+"'");
+             } else {
+               lang = p[1].trim();
+             }
+           } else if (s.startsWith("#:")) {
+             tu = new TranslationUnit(lang, s.substring(2).trim(), null, null);
+           } else {
+             throw new IOException("Encountered unexpected line '"+s+"'");             
+           }
+       } else if (tu != null && s.startsWith("msgid ")) {
+         tu.setSrcText(stripQuotes(s.substring(5).trim()));         
+       } else if (tu != null && s.startsWith("msgstr ")) {
+         tu.setTgtText(stripQuotes(s.substring(6).trim()));
+         if (tu.getTgtText() != null) {
+           list.add(tu);
+         }
+         tu = null;
+       } else {
+         throw new IOException("Encountered unexpected line '"+s+"'");
+       }
+       }
     }
-    return res;
+    return list;
+  }
+
+  private String stripQuotes(String s) {
+    if (s.length() <= 2) {
+      return null;
+    }
+    return s.substring(1, s.length()-1);
   }
 
   private String getFileName(String id, String baseLang, String targetLang) throws IOException {
