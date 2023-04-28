a868e3f6d77657710d1474b87e22cbc7d7087b12
Grahame Grieve <grahameg@gmail.ccom>
2023-04-29 08:41:05 +1000

    Add support for JSON in tx client batch handling

--- org.hl7.fhir.dstu3/src/main/java/org/hl7/fhir/dstu3/utils/client/network/FhirRequestBuilder.java
+++ org.hl7.fhir.dstu3/src/main/java/org/hl7/fhir/dstu3/utils/client/network/FhirRequestBuilder.java
@@ -281,7 +281,7 @@ public class FhirRequestBuilder {
       log(response.code(), response.headers(), body);
       String contentType = response.header("Content-Type");
       if (body != null) {
-        if (contentType.contains(ResourceFormat.RESOURCE_XML.getHeader()) || contentType.contains("text/xml+fhir")) {
+        if (contentType.contains(ResourceFormat.RESOURCE_XML.getHeader()) || contentType.contains(ResourceFormat.RESOURCE_JSON.getHeader()) || contentType.contains("text/xml+fhir")) {
           Resource rf = getParser(format).parse(body);
           if (rf instanceof Bundle)
             feed = (Bundle) rf;
