3a0e68e08ffaa497d93fbb88ed91b6613846a828
dotasek <david.otasek@smilecdr.com>
2023-05-04 12:16:48 -0400

    Tests and fix for Base equals methods

---
+++ org.hl7.fhir.dstu2016may/src/test/java/org/hl7/fhir/dstu2016may/test/ResourceEqualsTests.java
@@ -0,0 +1,65 @@
+package org.hl7.fhir.dstu2016may.test;
+
+
+import org.hl7.fhir.dstu2016may.formats.JsonParser;
+import org.hl7.fhir.dstu2016may.model.Resource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ResourceEqualsTests {
+
+  private JsonParser dstu2016_parser = new JsonParser();;
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "conformance_example_1.json",
+    "immunization_example_1.json",
+    "patient_example_1.json"
+  })
+  public void testEquals(String resourcePath) throws IOException {
+
+    org.hl7.fhir.dstu2016may.model.Resource resourceA = getResource(resourcePath);
+
+    org.hl7.fhir.dstu2016may.model.Resource resourceB = getResource(resourcePath);
+
+    assertTrue(resourceA.equalsShallow(resourceB));
+    assertTrue(resourceA.equalsDeep(resourceB));
+
+  }
+
+  private Resource getResource(String resource) throws IOException {
+    Resource resourceA;
+    InputStream inputA = this.getClass().getResourceAsStream(getResourcePath(resource));
+    resourceA = dstu2016_parser.parse(inputA);
+    return resourceA;
+  }
+
+  @Nonnull
+  private static String getResourcePath(String resource) {
+    return "/" + resource;
+  }
+
+  /* All files pairs contain a single difference, which can be evaluated with a diff. These differences are at various depths in the element tree. */
+  @ParameterizedTest
+  @CsvSource({
+    "conformance_example_1.json,conformance_example_2.json",
+    "immunization_example_1.json,immunization_example_2.json",
+    "patient_example_1.json,patient_example_2.json"
+  })
+  public void testEqualsFalse(String resourceAName, String resourceBName) throws IOException {
+    org.hl7.fhir.dstu2016may.model.Resource resourceA = getResource(resourceAName);
+
+    org.hl7.fhir.dstu2016may.model.Resource resourceB = getResource(resourceBName);
+
+    assertFalse(resourceA.equalsShallow(resourceB));
+    assertFalse(resourceA.equalsDeep(resourceB));
+  }
+}
