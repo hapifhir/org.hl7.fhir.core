5f18101686e864896e18efe1e96c76df76dcfb64
dotasek <david.otasek@smilecdr.com>
2023-05-08 13:00:17 -0400

    Cleanup - delete unused fields

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/advisors/impl/BaseAdvisor_30_40.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/advisors/impl/BaseAdvisor_30_40.java
@@ -10,11 +10,6 @@ import org.hl7.fhir.convertors.advisors.interfaces.BaseAdvisor40;
 import org.hl7.fhir.exceptions.FHIRException;
 
 public class BaseAdvisor_30_40 extends BaseAdvisor40<org.hl7.fhir.dstu3.model.Extension> {
-
-  final List<String> capabilityStatementIgnoredUrls = Collections.singletonList("http://hl7.org/fhir/3.0/StructureDefinition/extension-CapabilityStatement.acceptUnknown");
-
-  final List<String> immunizationIgnoredUrls = Collections.singletonList("http://hl7.org/fhir/3.0/StructureDefinition/extension-Immunization.notGiven");
-
   public BaseAdvisor_30_40() {
   }
 
