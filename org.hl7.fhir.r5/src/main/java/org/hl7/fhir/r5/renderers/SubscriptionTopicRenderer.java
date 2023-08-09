26891968acb60f961cb00f8295c84caa175bdb19
Grahame Grieve <grahameg@gmail.ccom>
2023-08-09 16:27:56 +1000

    fix rendering issue in subscription topic

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/renderers/SubscriptionTopicRenderer.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/renderers/SubscriptionTopicRenderer.java
@@ -87,7 +87,7 @@ public class SubscriptionTopicRenderer extends ResourceRenderer {
       renderTable(td, x);
     }
 
-    if (st.hasResourceTrigger()) {
+    if (st.hasEventTrigger()) {
       TableData td = new TableData("Event Triggers");
       for (SubscriptionTopicEventTriggerComponent rt : st.getEventTrigger()) {
         TableRowData tr = td.addRow();
