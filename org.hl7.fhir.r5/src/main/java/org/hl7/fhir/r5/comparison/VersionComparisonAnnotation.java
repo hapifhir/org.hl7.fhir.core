9cde582732ec6e653e0eb94f919aeedfb3815beb
Grahame Grieve <grahameg@gmail.ccom>
2023-08-14 15:33:24 +1000

    Update Comparers for annotating models with version differences

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/comparison/VersionComparisonAnnotation.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/comparison/VersionComparisonAnnotation.java
@@ -1,69 +1,80 @@
 package org.hl7.fhir.r5.comparison;
 
-import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.hl7.fhir.r5.comparison.CanonicalResourceComparer.CanonicalResourceComparison;
+import org.hl7.fhir.r5.comparison.CanonicalResourceComparer.ChangeAnalysisState;
 import org.hl7.fhir.r5.model.Base;
-import org.hl7.fhir.r5.model.ValueSet.ConceptSetComponent;
-import org.hl7.fhir.r5.model.ValueSet.ValueSetComposeComponent;
+import org.hl7.fhir.r5.model.CanonicalResource;
+import org.hl7.fhir.r5.model.ValueSet;
 import org.hl7.fhir.utilities.xhtml.XhtmlNode;
 
 public class VersionComparisonAnnotation {
 
   public enum AnotationType {
-    Added, Changed, Deleted;
+    NoChange, Added, Changed, Deleted;
   }
 
   public static final String USER_DATA_NAME = "version-annotation";
   
   private AnotationType type;
-//  private String comment;
-//  private String link;
+
   private Map<String, List<Base>> deletedChildren;
 
   private String version;
+
+  private CanonicalResourceComparison<? extends CanonicalResource> comp;
     
   private VersionComparisonAnnotation(AnotationType type, String version) {
     super();
     this.type = type;
     this.version = version;
   }
-//  
-//  private VersionComparisonAnnotation(AnotationType type, String comment) {
-//    super();
-//    this.type = type;
-//    this.comment = comment;
-//  }
-//  private VersionComparisonAnnotation(AnotationType type, String comment, String link) {
-//    super();
-//    this.type = type;
-//    this.comment = comment;
-//    this.link = link;
-//  }
-//  
+  
+
+  public static void annotate(Base base, String version, CanonicalResourceComparison<? extends CanonicalResource> comp) {
+    if (version != null) {
+      VersionComparisonAnnotation vca = new VersionComparisonAnnotation(comp.noChange() ? AnotationType.NoChange : AnotationType.Added, version);
+      vca.comp = comp;
+      base.setUserData(USER_DATA_NAME, vca);
+    }
+  }
+  
+
   public static void markAdded(Base focus, String version) {
-    focus.setUserData(USER_DATA_NAME, new VersionComparisonAnnotation(AnotationType.Added, version));
+    if (version != null) {
+      focus.setUserData(USER_DATA_NAME, new VersionComparisonAnnotation(AnotationType.Added, version));
+    }
+  }
+
+  public static void markChanged(Base focus, String version) {
+    if (version != null) {
+      focus.setUserData(USER_DATA_NAME, new VersionComparisonAnnotation(AnotationType.Changed, version));
+    }
   }
 
   public static void markDeleted(Base parent, String version, String name, Base other) {
-    VersionComparisonAnnotation vca = null;
-    if (parent.hasUserData(USER_DATA_NAME)) {
-      vca = (VersionComparisonAnnotation) parent.getUserData(USER_DATA_NAME);
-      assert vca.type != AnotationType.Added;
-    } else {
-      vca = new VersionComparisonAnnotation(AnotationType.Changed, version);
-      parent.setUserData(USER_DATA_NAME, vca);
+    if (version != null) {
+      VersionComparisonAnnotation vca = null;
+      if (parent.hasUserData(USER_DATA_NAME)) {
+        vca = (VersionComparisonAnnotation) parent.getUserData(USER_DATA_NAME);
+        assert vca.type != AnotationType.Added;
+      } else {
+        vca = new VersionComparisonAnnotation(AnotationType.Changed, version);
+        parent.setUserData(USER_DATA_NAME, vca);
+      }
+      if (vca.deletedChildren == null) {
+        vca.deletedChildren = new HashMap<>();
+      }
+      if (!vca.deletedChildren.containsKey(name)) {
+        vca.deletedChildren.put(name, new ArrayList<>());
+      }
+      other.setUserData(USER_DATA_NAME, new VersionComparisonAnnotation(AnotationType.Deleted, version));
+      vca.deletedChildren.get(name).add(other);
     }
-    if (vca.deletedChildren == null) {
-      vca.deletedChildren = new HashMap<>();
-    }
-    if (!vca.deletedChildren.containsKey(name)) {
-      vca.deletedChildren.put(name, new ArrayList<>());
-    }
-    other.setUserData(USER_DATA_NAME, new VersionComparisonAnnotation(AnotationType.Deleted, version));
-    vca.deletedChildren.get(name).add(other);
   }
 
   public AnotationType getType() {
@@ -72,19 +83,6 @@ public class VersionComparisonAnnotation {
   public void setType(AnotationType type) {
     this.type = type;
   }
-  
-//  public String getComment() {
-//    return comment;
-//  }
-//  public void setComment(String comment) {
-//    this.comment = comment;
-//  }
-//  public String getLink() {
-//    return link;
-//  }
-//  public void setLink(String link) {
-//    this.link = link;
-//  }
   
   public static XhtmlNode render(Base b, XhtmlNode x) {
     if (b.hasUserData(USER_DATA_NAME)) {
@@ -98,20 +96,57 @@ public class VersionComparisonAnnotation {
   private XhtmlNode render(XhtmlNode x) {
     switch (type) {
     case Added:
-      XhtmlNode span = x.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
-      span.img("icon-change-add.png", "icon");
-      span.tx(" Added:");
-      return x;
+      XhtmlNode spanOuter = x.span("border: solid 1px #dddddd; margin: 2px; padding: 2px", null);
+      XhtmlNode spanInner = spanOuter.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-add.png", "icon");
+      spanInner.tx(" Added:");
+      return spanOuter;
     case Changed:
-      span = x.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been changed since version "+version);
-      span.img("icon-change-edit.png", "icon");
-      span.tx(" Changed:");
-      return x;
+      spanOuter = x.span("border: solid 1px #dddddd; margin: 2px; padding: 2px", null);
+      spanInner = spanOuter.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-edit.png", "icon");
+      spanInner.tx(" Changed:");
+      return spanOuter;
     case Deleted:
-      span = x.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been removed since version "+version);
-      span.img("icon-change-remove.png", "icon");
-      span.tx(" Removed:");
-      return x.strikethrough();
+      spanOuter = x.span("border: solid 1px #dddddd; margin: 2px; padding: 2px", null);
+      spanInner = spanOuter.span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-remove.png", "icon");
+      spanInner.tx(" Removed:");
+      return spanOuter.strikethrough();
+    default:
+      return x;
+    }
+  }
+
+  public static XhtmlNode renderDiv(Base b, XhtmlNode x) {
+    if (b.hasUserData(USER_DATA_NAME)) {
+      VersionComparisonAnnotation self = (VersionComparisonAnnotation) b.getUserData(USER_DATA_NAME);
+      return self.renderDiv(x);
+    } else {
+      return x;
+    }
+  }
+  
+  private XhtmlNode renderDiv(XhtmlNode x) {
+    switch (type) {
+    case Added:
+      XhtmlNode divOuter = x.div("border: solid 1px #dddddd; margin: 2px; padding: 2px");
+      XhtmlNode spanInner = divOuter.para().style("margin: 0").span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-add.png", "icon");
+      spanInner.tx(" Added:");
+      return divOuter;
+    case Changed:
+      divOuter = x.div("border: solid 1px #dddddd; margin: 2px; padding: 2px");
+      spanInner = divOuter.para().style("margin: 0").span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-edit.png", "icon");
+      spanInner.tx(" Changed:");
+      return divOuter;
+    case Deleted:
+      divOuter = x.div("border: solid 1px #dddddd; margin: 2px; padding: 2px");
+      spanInner = divOuter.para().style("margin: 0").span("background-color: #fff2ff; border-left: solid 3px #ffa0ff; margin: 2px; padding: 2px", "This content has been added since  "+version);
+      spanInner.img("icon-change-remove.png", "icon");
+      spanInner.tx(" Removed:");
+      return divOuter.strikethrough();
     default:
       return x;
     }
@@ -141,6 +176,15 @@ public class VersionComparisonAnnotation {
       }
     }
     return result;
+  }
+
+  public static CanonicalResourceComparison<? extends CanonicalResource> artifactComparison(Base base) {
+    if (base.hasUserData(USER_DATA_NAME)) {
+      VersionComparisonAnnotation self = (VersionComparisonAnnotation) base.getUserData(USER_DATA_NAME);
+      return self.comp;
+    } else {
+      return null;
+    }
   }
   
 }
