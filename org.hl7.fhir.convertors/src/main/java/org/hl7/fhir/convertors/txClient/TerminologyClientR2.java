495bc0012c60e23dcf02528747b7c7a511ebea66
dotasek <david.otasek@smilecdr.com>
2023-08-24 15:28:05 -0400

    Add version to agent header
    + remove unused getValidationEngine methods in Common

--- org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/txClient/TerminologyClientR2.java
+++ org.hl7.fhir.convertors/src/main/java/org/hl7/fhir/convertors/txClient/TerminologyClientR2.java
@@ -201,6 +201,11 @@ public class TerminologyClientR2 implements ITerminologyClient {
   }
 
   @Override
+  public String getUserAgent() {
+    return client.getUserAgent();
+  }
+
+  @Override
   public String getServerVersion() {
     return client.getServerVersion();
   }
