434ad9dfb5134463d23f0607c2fe4dff5c91fe3f
Grahame Grieve <grahameg@gmail.ccom>
2023-09-20 11:12:50 +1000

    Improved rendering of codes in include when rendering valuesets

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/instance/InstanceValidator.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/instance/InstanceValidator.java
@@ -173,6 +173,8 @@ import org.hl7.fhir.r5.utils.validation.constants.ContainedReferenceValidationPo
 import org.hl7.fhir.r5.utils.validation.constants.IdStatus;
 import org.hl7.fhir.r5.utils.validation.constants.ReferenceValidationPolicy;
 import org.hl7.fhir.utilities.CommaSeparatedStringBuilder;
+import org.hl7.fhir.utilities.HL7WorkGroups;
+import org.hl7.fhir.utilities.HL7WorkGroups.HL7WorkGroup;
 import org.hl7.fhir.utilities.MarkDownProcessor;
 import org.hl7.fhir.utilities.SIDUtilities;
 import org.hl7.fhir.utilities.StandardsStatus;
@@ -5403,82 +5405,18 @@ public class InstanceValidator extends BaseValidator implements IResourceValidat
       }
     }
     if (rule(errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), wg != null, I18nConstants.VALIDATION_HL7_WG_NEEDED, ToolingExtensions.EXT_WORKGROUP)) {
-      String name = nameForWG(wg);
-      String url = urlForWG(wg);
-      if (rule(errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), name != null, I18nConstants.VALIDATION_HL7_WG_UNKNOWN, wg)) {
-        String rpub = "HL7 International / "+name;
+      HL7WorkGroup wgd = HL7WorkGroups.find(wg);      
+      if (rule(errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), wgd != null, I18nConstants.VALIDATION_HL7_WG_UNKNOWN, wg)) {
+        String rpub = "HL7 International / "+wgd.getName();
         if (warning(errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), pub != null, I18nConstants.VALIDATION_HL7_PUBLISHER_MISSING, wg, rpub)) {
           warningOrError(pub.contains("/"), errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), rpub.equals(pub), I18nConstants.VALIDATION_HL7_PUBLISHER_MISMATCH, wg, rpub, pub);
         }
         warning(errors, "2023-09-15", IssueType.BUSINESSRULE, element.line(), element.col(), stack.getLiteralPath(), 
-            Utilities.startsWithInList(url, urls), I18nConstants.VALIDATION_HL7_WG_URL, wg, url);
+            Utilities.startsWithInList( wgd.getLink(), urls), I18nConstants.VALIDATION_HL7_WG_URL, wg, wgd.getLink());
         return true;
       }        
     }      
     return false;
-  }
-
-  private String urlForWG(String wg) {
-    switch (wg) {
-    case "cbcc": return "http://www.hl7.org/Special/committees/homehealth";
-    case "cds": return "http://www.hl7.org/Special/committees/dss";
-    case "cqi": return "http://www.hl7.org/Special/committees/cqi";
-    case "cg": return "http://www.hl7.org/Special/committees/clingenomics";
-    case "dev": return "http://www.hl7.org/Special/committees/healthcaredevices";
-    case "ehr": return "http://www.hl7.org/special/committees/ehr";
-    case "fhir": return "http://www.hl7.org/Special/committees/fiwg";
-    case "fm": return "http://www.hl7.org/Special/committees/fm";
-    case "hsi": return "http://www.hl7.org/Special/committees/hsi";
-    case "ii": return "http://www.hl7.org/Special/committees/imagemgt";
-    case "inm": return "http://www.hl7.org/special/committees/inm";
-    case "mnm": return "http://www.hl7.org/special/committees/mnm";
-    case "its": return "http://www.hl7.org/special/committees/xml";
-    case "oo": return "http://www.hl7.org/Special/committees/orders";
-    case "pa": return "http://www.hl7.org/Special/committees/pafm";
-    case "pc": return "http://www.hl7.org/Special/committees/patientcare";
-    case "pe": return "http://www.hl7.org/Special/committees/patientempowerment";
-    case "pher": return "http://www.hl7.org/Special/committees/pher";
-    case "phx": return "http://www.hl7.org/Special/committees/medication";
-    case "brr": return "http://www.hl7.org/Special/committees/rcrim";
-    case "sd": return "http://www.hl7.org/Special/committees/structure";
-    case "sec": return "http://www.hl7.org/Special/committees/secure";
-    case "us": return "http://www.hl7.org/Special/Committees/usrealm";
-    case "vocab": return "http://www.hl7.org/Special/committees/Vocab";
-    case "aid": return "http://www.hl7.org/Special/committees/java";
-    }
-    return null;
-  }
-
-  private String nameForWG(String wg) {
-    switch (wg) {
-
-    case "cbcc": return "Community Based Collaborative Care";
-    case "cds": return "Clinical Decision Support";
-    case "cqi": return "Clinical Quality Information";
-    case "cg": return "Clinical Genomics";
-    case "dev": return "Health Care Devices";
-    case "ehr": return "Electronic Health Records";
-    case "fhir": return "FHIR Infrastructure";
-    case "fm": return "Financial Management";
-    case "hsi": return "Health Standards Integration";
-    case "ii": return "Imaging Integration";
-    case "inm": return "Infrastructure And Messaging";
-    case "mnm": return "Modeling and Methodology";
-    case "its": return "Implementable Technology Specifications";
-    case "oo": return "Orders and Observations";
-    case "pa": return "Patient Administration";
-    case "pc": return "Patient Care";
-    case "pe": return "Patient Empowerment";
-    case "pher": return "Public Health and Emergency Response";
-    case "phx": return "Pharmacy";
-    case "brr": return "Biomedical Research and Regulation";
-    case "sd": return "Structured Documents";
-    case "sec": return "Security";
-    case "us": return "US Realm Taskforce";
-    case "vocab": return "Terminology Infrastructure";
-    case "aid": return "Application Implementation and Design";
-    }
-    return null;
   }
 
   private boolean statusCodesConsistent(String status, String standardsStatus) {
