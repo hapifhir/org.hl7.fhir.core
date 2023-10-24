3ea42ca1b0967f4737898980f76e07000644fe59
dotasek <david.otasek@smilecdr.com>
2023-10-24 14:29:05 -0400

    Fix test ignore/exclude issues, bump test cases

--- org.hl7.fhir.dstu3/src/test/java/org/hl7/fhir/dstu3/test/support/TestingUtilities.java
+++ org.hl7.fhir.dstu3/src/test/java/org/hl7/fhir/dstu3/test/support/TestingUtilities.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -276,5 +277,7 @@ public class TestingUtilities extends BaseTestingUtilities {
 		return null;
 	}
 
-
+  public static boolean runningAsSurefire() {
+    return "true".equals(System.getProperty("runningAsSurefire") != null ? System.getProperty("runningAsSurefire").toLowerCase(Locale.ENGLISH) : "");
+  }
 }
