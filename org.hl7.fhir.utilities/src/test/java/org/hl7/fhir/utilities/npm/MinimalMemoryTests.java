f427d8654bbf3c281462a2c80fba918997248682
dotasek <dotasek.dev@gmail.com>
2023-12-04 13:21:14 -0500

    Remove console chatter

--- org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/MinimalMemoryTests.java
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/MinimalMemoryTests.java
@@ -13,15 +13,10 @@ public class MinimalMemoryTests {
   public void testFetch() throws IOException {
     File folder = new File(Utilities.path("[tmp]", ".fhir-mm"));
     if (folder.exists()) {
-      System.out.println("MinimalMemoryTests: clearing folder: " + folder.getAbsolutePath());
       Utilities.clearDirectory(folder.getAbsolutePath());
     } else {
-      System.out.println("MinimalMemoryTests: creating folder: " + folder.getAbsolutePath());
-      File createdFolder = Utilities.createDirectory(folder.getAbsolutePath());
-      System.out.println("MinimalMemoryTests: created folder: " + createdFolder.getAbsolutePath());
-      System.out.println("MinimalMemoryTests: new folder exists: " + createdFolder.exists());
+      Utilities.createDirectory(folder.getAbsolutePath());
     }
-    System.out.println("MinimalMemoryTests: passed folder exists: " + folder.exists());
     FilesystemPackageCacheManager pcm = new FilesystemPackageCacheManager.FilesystemPackageCacheManagerBuilder().withCacheFolder(folder.getAbsolutePath()).build();
     pcm.setMinimalMemory(true);
     NpmPackage npm = pcm.loadPackage("hl7.fhir.us.core", "5.0.0");
