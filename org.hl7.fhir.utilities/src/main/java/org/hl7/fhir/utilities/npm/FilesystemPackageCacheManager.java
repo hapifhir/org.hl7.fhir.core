30132d1e345bb98e5fec9c00d22e53b815868fbb
dotasek <dotasek.dev@gmail.com>
2023-12-04 11:44:22 -0500

    Add debug logging for failing test

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/npm/FilesystemPackageCacheManager.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/npm/FilesystemPackageCacheManager.java
@@ -160,7 +160,7 @@ public class FilesystemPackageCacheManager extends BasePackageCacheManager imple
     public FilesystemPackageCacheManagerBuilder withCacheFolder (String cacheFolderPath) throws IOException {
       File cacheFolder = new File(cacheFolderPath);
       if (!this.cacheFolder.exists()) {
-        throw new FHIRException("The folder ''"+cacheFolder+"' could not be found");
+        throw new FHIRException("The folder '"+cacheFolder+"' could not be found");
       }
       return new FilesystemPackageCacheManagerBuilder(cacheFolder, this.packageServers);
     }
