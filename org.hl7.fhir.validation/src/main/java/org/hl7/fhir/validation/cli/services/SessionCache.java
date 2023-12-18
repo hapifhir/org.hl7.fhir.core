0ab5a788fd10b951fa3da666b4c5717dd4626ee3
rpassas <35311744+rpassas@users.noreply.github.com>
2023-12-18 09:54:20 -0500

    configurable sessions expiration (#1501)
    * unit test + interface
    
    * Renaming and moving expiry related methods to implementation
    
    ---------
    
    Co-authored-by: dotasek <david.otasek@smilecdr.com>

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/SessionCache.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/cli/services/SessionCache.java
@@ -1,35 +1,10 @@
 package org.hl7.fhir.validation.cli.services;
 
 import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.collections4.map.PassiveExpiringMap;
 import org.hl7.fhir.validation.ValidationEngine;
 
-/**
- * SessionCache for storing and retrieving ValidationEngine instances, so callers do not have to re-instantiate a new
- * instance for each validation request.
- */
-public class SessionCache {
-
-  protected static final long TIME_TO_LIVE = 60;
-  protected static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
-
-  private final PassiveExpiringMap<String, ValidationEngine> cachedSessions;
-
-  public SessionCache() {
-    cachedSessions = new PassiveExpiringMap<>(TIME_TO_LIVE, TIME_UNIT);
-  }
-
-  /**
-   * @param sessionLength the constant amount of time an entry is available before it expires. A negative value results
-   *                      in entries that NEVER expire. A zero value results in entries that ALWAYS expire.
-   * @param sessionLengthUnit the unit of time for the timeToLive parameter, must not be null
-   */
-  public SessionCache(long sessionLength, TimeUnit sessionLengthUnit) {
-    cachedSessions = new PassiveExpiringMap<>(sessionLength, sessionLengthUnit);
-  }
+public interface SessionCache {
 
   /**
    * Stores the initialized {@link ValidationEngine} in the cache. Returns the session id that will be associated with
@@ -37,11 +12,7 @@ public class SessionCache {
    * @param validationEngine {@link ValidationEngine}
    * @return The {@link String} id associated with the stored instance.
    */
-  public String cacheSession(ValidationEngine validationEngine) {
-    String generatedId = generateID();
-    cachedSessions.put(generatedId, validationEngine);
-    return generatedId;
-  }
+  String cacheSession(ValidationEngine validationEngine);
 
   /**
    * Stores the initialized {@link ValidationEngine} in the cache with the passed in id as the key. If a null key is
@@ -50,59 +21,29 @@ public class SessionCache {
    * @param validationEngine The {@link ValidationEngine} instance to cache.
    * @return The {@link String} id that will be associated with the stored {@link ValidationEngine}
    */
-  public String cacheSession(String sessionId, ValidationEngine validationEngine) {
-    if(sessionId == null) {
-      sessionId = cacheSession(validationEngine);
-    } else {
-      cachedSessions.put(sessionId, validationEngine);
-    }
-    return sessionId;
-  }
+  String cacheSession(String sessionId, ValidationEngine validationEngine);
 
-  /**
-   * When called, this actively checks the cache for expired entries and removes
-   * them.
-   */
-  public void removeExpiredSessions() {
-    /*
-    The PassiveExpiringMap will remove entries when accessing the mapped value
-    for a key, OR when invoking methods that involve accessing the entire map
-    contents. So, we call keySet below to force removal of all expired entries.
-    * */
-    cachedSessions.keySet();
-  }
+
+
 
   /**
    * Checks if the passed in {@link String} id exists in the set of stored session id.
    * @param sessionId The {@link String} id to search for.
    * @return {@link Boolean#TRUE} if such id exists.
    */
-  public boolean sessionExists(String sessionId) {
-    return cachedSessions.containsKey(sessionId);
-  }
+  boolean sessionExists(String sessionId);
 
   /**
    * Returns the stored {@link ValidationEngine} associated with the passed in session id, if one such instance exists.
    * @param sessionId The {@link String} session id.
    * @return The {@link ValidationEngine} associated with the passed in id, or null if none exists.
    */
-  public ValidationEngine fetchSessionValidatorEngine(String sessionId) {
-    return cachedSessions.get(sessionId);
-  }
+  ValidationEngine fetchSessionValidatorEngine(String sessionId);
 
   /**
    * Returns the set of stored session ids.
    * @return {@link Set} of session ids.
    */
-  public Set<String> getSessionIds() {
-    return cachedSessions.keySet();
-  }
-
-  /**
-   * Session ids generated internally are UUID {@link String}.
-   * @return A new {@link String} session id.
-   */
-  private String generateID() {
-    return UUID.randomUUID().toString();
-  }
+  Set<String> getSessionIds();
+    
 }
