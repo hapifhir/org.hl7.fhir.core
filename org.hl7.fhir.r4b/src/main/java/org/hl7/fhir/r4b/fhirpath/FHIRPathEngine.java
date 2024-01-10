ee3400be7d55094f7bbe4356d3b233981ea0cf35
Grahame Grieve <grahameg@gmail.ccom>
2024-01-10 11:18:23 +1100

    fix unicode processing mistakes

--- org.hl7.fhir.r4b/src/main/java/org/hl7/fhir/r4b/fhirpath/FHIRPathEngine.java
+++ org.hl7.fhir.r4b/src/main/java/org/hl7/fhir/r4b/fhirpath/FHIRPathEngine.java
@@ -1767,7 +1767,7 @@ public class FHIRPathEngine {
           break;
         case 'u':
           i++;
-          int uc = Integer.parseInt(s.substring(i, i + 4), 32);
+          int uc = Integer.parseInt(s.substring(i, i + 4), 16);
           b.append(Character.toString(uc));
           i = i + 3;
           break;
