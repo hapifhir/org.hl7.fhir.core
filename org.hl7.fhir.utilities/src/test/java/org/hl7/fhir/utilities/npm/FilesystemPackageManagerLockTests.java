18ff00fcc31e826e8220cc3401313fa9d5b543b1
dotasek <dotasek.dev@gmail.com>
2024-08-27 11:29:41 -0400

    Package Cache Concurrency (#1715)
    * WIP on do-20240722-thread-safety
    
    * Try lock downgrading for better performance
    
    * Refactor FilesystemPackageCacheLockManager
    
    * Tidy up spacing.
    
    * Refactor initCacheFolder. Change read lock to stop asking for a write lock
    
    * Clean up version in addPackageToCache
    
    Since we run checkValidVersionString, version will never be null, and can be final. Also, everything else can be final.
    
    * Make an even crash-ier test for the FilesystemPackageManager
    
    * Move read lock starting point to beginning of addPackageToCache
    
    * Code cleanup
    
    * More code cleanup + enhanced test + more locks
    
    * More aggressive testing + bug fixes and lock file creation
    
    * Basic checks and creation for lockfile
    
    * More testing + smarter check for lock file
    
    * Some renaming and code cleanup
    
    * Some more general tests
    
    * Check whole cache write lock (instantiating)
    
    * Fix references to local files
    
    * A little less repetition than that...
    
    * Fix unclosed channel causing windows failure
    
    * Fix file sync issues when writing .index.json files + others
    
    * Add TRUNCATE_EXISTING and WRITE open options (fix overwrite behavior)
    
    * Revert "Add TRUNCATE_EXISTING and WRITE open options (fix overwrite behavior)"
    
    This reverts commit cfed736cd68213a800bcfe9a7a7fba5993105d9e.
    
    * Revert "Fix file sync issues when writing .index.json files + others"
    
    This reverts commit 4daf7feb935519e769aecfd5ec10b7eef5339bd6.
    
    * Some additional test coverage, since I already wrote it.
    
    * Don't build indexes implicitly during reads.
    
    * Woops. Forgot that a directory can disappear via deletion
    
    * Remove larger package
    
    * Clean up code + remove System.out chatter
    
    * Remove long running test (is tested elsewhere) + clean up code

---
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/FilesystemPackageManagerLockTests.java
@@ -0,0 +1,227 @@
+package org.hl7.fhir.utilities.npm;
+
+import org.hl7.fhir.utilities.TextFile;
+import org.hl7.fhir.utilities.filesystem.ManagedFileAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilesystemPackageManagerLockTests {
+
+  public static final String DUMMY_PACKAGE = "dummy#1.2.3";
+  String cachePath;
+  File cacheDirectory;
+  FilesystemPackageCacheManagerLocks filesystemPackageCacheLockManager;
+
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    cachePath = ManagedFileAccess.fromPath(Files.createTempDirectory("fpcm-multithreadingTest")).getAbsolutePath();
+    cacheDirectory = new File(cachePath);
+    filesystemPackageCacheLockManager = new FilesystemPackageCacheManagerLocks(cacheDirectory);
+
+  }
+
+  @Test
+  public void testBaseCases() throws IOException {
+    filesystemPackageCacheLockManager.getCacheLock().doWriteWithLock(() -> {
+      assertThat(cacheDirectory).exists();
+      assertThat(cacheDirectory).isDirectory();
+      assertThat(cacheDirectory).canWrite();
+      assertThat(cacheDirectory).canRead();
+      return null;
+    });
+
+
+    final  FilesystemPackageCacheManagerLocks.PackageLock packageLock = filesystemPackageCacheLockManager.getPackageLock(DUMMY_PACKAGE);
+    packageLock.doWriteWithLock(() -> {
+      assertThat(packageLock.getLockFile()).exists();
+      return null;
+    });
+    assertThat(packageLock.getLockFile()).doesNotExist();
+
+    packageLock.doReadWithLock(() -> {
+      assertThat(packageLock.getLockFile()).doesNotExist();
+      return null;
+    });
+  }
+
+  @Test void testNoPackageWriteOrReadWhileWholeCacheIsLocked() throws IOException, InterruptedException {
+    final FilesystemPackageCacheManagerLocks.PackageLock packageLock = filesystemPackageCacheLockManager.getPackageLock(DUMMY_PACKAGE);
+
+    AtomicBoolean cacheLockFinished = new AtomicBoolean(false);
+    List<Thread> threadList = new ArrayList<>();
+
+    Thread cacheThread = new Thread(() -> {
+      try {
+        filesystemPackageCacheLockManager.getCacheLock().doWriteWithLock(() -> {
+          try {
+            Thread.sleep(300);
+            cacheLockFinished.set(true);
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+          return null;
+        });
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+    cacheThread.start();
+    Thread.sleep(100);
+    for (int i = 0; i < 5; i++) {
+      threadList.add(new Thread(() -> {
+        try {
+          packageLock.doWriteWithLock(() -> {
+            assertThat(cacheLockFinished.get()).isTrue();
+            return null;
+          });
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }));
+      threadList.add(new Thread(() -> {
+        try {
+          packageLock.doReadWithLock(() -> {
+            assertThat(cacheLockFinished.get()).isTrue();
+            return null;
+          });
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }));
+    }
+
+    for (Thread thread: threadList) {
+      thread.start();
+    }
+    for (Thread thread: threadList) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  @Test void testSinglePackageWriteMultiPackageRead() throws IOException {
+    final FilesystemPackageCacheManagerLocks.PackageLock packageLock = filesystemPackageCacheLockManager.getPackageLock(DUMMY_PACKAGE);
+    AtomicInteger writeCounter = new AtomicInteger(0);
+
+    AtomicInteger readCounter = new AtomicInteger(0);
+    List<Thread> threadList = new ArrayList<>();
+
+    AtomicInteger maxReadThreads = new AtomicInteger();
+
+    for (int i = 0; i < 10; i++) {
+     threadList.add(new Thread(() -> {
+        try {
+          packageLock.doWriteWithLock(() -> {
+            int writeCount = writeCounter.incrementAndGet();
+            assertThat(writeCount).isEqualTo(1);
+            writeCounter.decrementAndGet();
+            return null;
+          });
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }));
+    }
+
+    for (int i = 0; i < 10; i++) {
+      threadList.add(new Thread(() -> {
+        try {
+          packageLock.doReadWithLock(() -> {
+            int readCount = readCounter.incrementAndGet();
+            try {
+              Thread.sleep(100);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            assertThat(readCount).isGreaterThan(0);
+            if (readCount > maxReadThreads.get()) {
+              maxReadThreads.set(readCount);
+            }
+            readCounter.decrementAndGet();
+            return null;
+          });
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }));
+    }
+
+    for (Thread thread: threadList) {
+      thread.start();
+    }
+
+    for (Thread thread: threadList) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    assertThat(maxReadThreads.get()).isGreaterThan(1);
+  }
+
+  @Test
+  public void testReadWhenLockedByFileTimesOut() throws IOException {
+    FilesystemPackageCacheManagerLocks shorterTimeoutManager = filesystemPackageCacheLockManager.withLockTimeout(3L, TimeUnit.SECONDS);
+    final FilesystemPackageCacheManagerLocks.PackageLock packageLock = shorterTimeoutManager.getPackageLock(DUMMY_PACKAGE);
+    File lockFile = createPackageLockFile();
+
+    Exception exception = assertThrows(IOException.class, () -> {
+      packageLock.doReadWithLock(() -> {
+        assertThat(lockFile).exists();
+        return null;
+      });
+    });
+
+    assertThat(exception.getMessage()).contains("Error reading package");
+    assertThat(exception.getCause().getMessage()).contains("Timeout waiting for lock file deletion: " + lockFile.getName());
+  }
+
+  @Test
+  public void testReadWhenLockFileIsDeleted() throws IOException {
+    FilesystemPackageCacheManagerLocks shorterTimeoutManager = filesystemPackageCacheLockManager.withLockTimeout(5L, TimeUnit.SECONDS);
+    final FilesystemPackageCacheManagerLocks.PackageLock packageLock = shorterTimeoutManager.getPackageLock(DUMMY_PACKAGE);
+    File lockFile = createPackageLockFile();
+
+    Thread t = new Thread(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      lockFile.delete();
+    });
+    t.start();
+
+    packageLock.doReadWithLock(() -> {
+      assertThat(lockFile).doesNotExist();
+      return null;
+    });
+
+  }
+
+  private File createPackageLockFile() throws IOException {
+    File lockFile = Path.of(cachePath, DUMMY_PACKAGE + ".lock").toFile();
+    TextFile.stringToFile("", lockFile);
+    return lockFile;
+  }
+
+}
