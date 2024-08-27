875d7feff3b7352ce3f98c276ed440805fc32cb0
dotasek <dotasek.dev@gmail.com>
2024-08-27 11:29:41 -0400

    Package Cache Concurrency (#1715)
    * WIP on do-20240722-thread-safety
    
    * Try lock downgrading for better performance
    
    * Refactor FilesystemPackageCacheLockManager
    
    * Tidy up spacing.
    
    * Refactor initCacheFolder. Change read lock to stop asking for a write lock
    
    * Clean up version in addPackageToCache
    
    Since we run checkValidVersionString, version will never be null, and can be final. Also, everything else can be final.
    
    * Make an even crash-ier test for the FilesystemPackageManager
    
    * Move read lock starting point to beginning of addPackageToCache
    
    * Code cleanup
    
    * More code cleanup + enhanced test + more locks
    
    * More aggressive testing + bug fixes and lock file creation
    
    * Basic checks and creation for lockfile
    
    * More testing + smarter check for lock file
    
    * Some renaming and code cleanup
    
    * Some more general tests
    
    * Check whole cache write lock (instantiating)
    
    * Fix references to local files
    
    * A little less repetition than that...
    
    * Fix unclosed channel causing windows failure
    
    * Fix file sync issues when writing .index.json files + others
    
    * Add TRUNCATE_EXISTING and WRITE open options (fix overwrite behavior)
    
    * Revert "Add TRUNCATE_EXISTING and WRITE open options (fix overwrite behavior)"
    
    This reverts commit cfed736cd68213a800bcfe9a7a7fba5993105d9e.
    
    * Revert "Fix file sync issues when writing .index.json files + others"
    
    This reverts commit 4daf7feb935519e769aecfd5ec10b7eef5339bd6.
    
    * Some additional test coverage, since I already wrote it.
    
    * Don't build indexes implicitly during reads.
    
    * Woops. Forgot that a directory can disappear via deletion
    
    * Remove larger package
    
    * Clean up code + remove System.out chatter
    
    * Remove long running test (is tested elsewhere) + clean up code

--- org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/FilesystemPackageManagerTests.java
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/FilesystemPackageManagerTests.java
@@ -3,37 +3,47 @@ package org.hl7.fhir.utilities.npm;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
 
 import org.hl7.fhir.utilities.filesystem.ManagedFileAccess;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class FilesystemPackageManagerTests {
 
-  private static final String DUMMY_URL_1 = "http://dummy1.org";
-  private static final String DUMMY_URL_2 = "http://dummy2.org";
+  private static final String DUMMY_URL_1 = "https://dummy1.org";
+  private static final String DUMMY_URL_2 = "https://dummy2.org";
 
-  private static final String DUMMY_URL_3 = "http://dummy3.org";
+  private static final String DUMMY_URL_3 = "https://dummy3.org";
 
-  private static final String DUMMY_URL_4 = "http://dummy4.org";
-  private List<PackageServer> dummyPrivateServers = List.of(
+  private static final String DUMMY_URL_4 = "https://dummy4.org";
+  private final List<PackageServer> dummyPrivateServers = List.of(
      new PackageServer(DUMMY_URL_1),
      new PackageServer(DUMMY_URL_2)
   );
 
-  private List<PackageServer> dummyDefaultServers = List.of(
+  private final List<PackageServer> dummyDefaultServers = List.of(
     new PackageServer(DUMMY_URL_3),
     new PackageServer(DUMMY_URL_4)
   );
+
+
 
   @Test
   public void testDefaultServers() throws IOException {
@@ -101,24 +111,56 @@ public class FilesystemPackageManagerTests {
     assertEquals( System.getenv("ProgramData") + "\\.fhir\\packages", folder.getAbsolutePath());
   }
 
-  @Test
-  public void multithreadingTest() throws IOException {
+  /**
+    We repeat the same tests multiple times here, in order to catch very rare edge cases.
+   */
+  public static Stream<Arguments> packageCacheMultiThreadTestParams() {
+    List<Arguments> params = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      params.add(Arguments.of(100, 1));
+      params.add(Arguments.of(10,10));
+      params.add(Arguments.of(100, 10));
+    }
+    return params.stream();
+  }
+
+  @MethodSource("packageCacheMultiThreadTestParams")
+  @ParameterizedTest
+  public void packageCacheMultiThreadTest(final int threadTotal, final int packageCacheManagerTotal) throws IOException {
+
     String pcmPath = ManagedFileAccess.fromPath(Files.createTempDirectory("fpcm-multithreadingTest")).getAbsolutePath();
-    FilesystemPackageCacheManager pcm = new FilesystemPackageCacheManager.Builder().withCacheFolder(pcmPath).build();
+    FilesystemPackageCacheManager[] packageCacheManagers = new FilesystemPackageCacheManager[packageCacheManagerTotal];
+    Random rand = new Random();
 
     final AtomicInteger totalSuccessful = new AtomicInteger();
-
+    final ConcurrentHashMap successfulThreads = new ConcurrentHashMap();
     List<Thread> threads = new ArrayList<>();
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < threadTotal; i++) {
       final int index = i;
       Thread t = new Thread(() -> {
         try {
-          pcm.loadPackage("hl7.fhir.xver-extensions#0.0.12");
+          System.out.println("Thread #" + index + ": " + Thread.currentThread().getId() + " started");
+          final int randomPCM = rand.nextInt(packageCacheManagerTotal);
+          final int randomOperation = rand.nextInt(4);
+          if (packageCacheManagers[randomPCM] == null) {
+            packageCacheManagers[randomPCM] = new FilesystemPackageCacheManager.Builder().withCacheFolder(pcmPath).build();
+          }
+          FilesystemPackageCacheManager pcm = packageCacheManagers[randomPCM];
+          if (randomOperation == 0) {
+            pcm.addPackageToCache("example.fhir.uv.myig", "1.2.3", this.getClass().getResourceAsStream("/npm/dummy-package.tgz"), "https://packages.fhir.org/example.fhir.uv.myig/1.2.3");
+          } else if (randomOperation == 1) {
+            pcm.clear();
+          } else if (randomOperation == 2) {
+            pcm.loadPackageFromCacheOnly("example.fhir.uv.myig", "1.2.3");
+          } else {
+            pcm.removePackage("example.fhir.uv.myig", "1.2.3");
+          }
           totalSuccessful.incrementAndGet();
-          System.out.println("Thread " + index + " completed");
+          successfulThreads.put(Thread.currentThread().getId(), index);
+          System.out.println("Thread #" + index + ": " + Thread.currentThread().getId() + " completed");
         } catch (Exception e) {
           e.printStackTrace();
-          System.err.println("Thread " + index + " failed");
+          System.err.println("Thread #" + index + ": " + Thread.currentThread().getId() + " failed");
         }
       });
       t.start();
@@ -131,6 +173,17 @@ public class FilesystemPackageManagerTests {
 
       }
     });
-    assertEquals(3, totalSuccessful.get());
+
+    printUnsuccessfulThreads(successfulThreads, threads);
+    assertEquals(threadTotal, totalSuccessful.get(), "Not all threads were successful.");
+
+  }
+
+  private void printUnsuccessfulThreads(final ConcurrentHashMap successfulThreads, List<Thread> threads) {
+    for (Thread t : threads) {
+      if (!successfulThreads.containsKey(t.getId())) {
+        System.out.println("Thread #" + t.getId() + " failed");
+      }
+    }
   }
 }
