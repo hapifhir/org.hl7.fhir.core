7d0822a0d895f031b0f0fdac2fe300e65490c997
Lloyd McKenzie <lloyd@lmckenzie.com>
2024-12-08 12:36:50 -0700

    Added test scripts for ValueSet conversion

--- org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/conv40_50/ValueSet40_50Test.java
+++ org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/conv40_50/ValueSet40_50Test.java
@@ -1,28 +1,28 @@
 package org.hl7.fhir.convertors.conv40_50;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.hl7.fhir.convertors.factory.VersionConvertorFactory_40_50;
 import org.hl7.fhir.r5.formats.IParser.OutputStyle;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TestScript40_50Test {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ValueSet40_50Test {
 
 
   @Test
-  @DisplayName("Test TestScript R4 <-> R5 with resource scope extension")
+  @DisplayName("Test ValueSet R4 <-> R5 with property extensions")
   public void testValueExpressionConversion40_10() throws IOException {
-    InputStream r4_stream = this.getClass().getResourceAsStream("/test_script_r4.json");
-    org.hl7.fhir.r4.model.TestScript r4_input = (org.hl7.fhir.r4.model.TestScript) new org.hl7.fhir.r4.formats.JsonParser().parse(r4_stream);
+    InputStream r4_stream = this.getClass().getResourceAsStream("/value_set_r4.json");
+    org.hl7.fhir.r4.model.ValueSet r4_input = (org.hl7.fhir.r4.model.ValueSet) new org.hl7.fhir.r4.formats.JsonParser().parse(r4_stream);
 
-    InputStream r5_stream = this.getClass().getResourceAsStream("/test_script_r5.json");
-    org.hl7.fhir.r5.model.TestScript r5_input = (org.hl7.fhir.r5.model.TestScript) new org.hl7.fhir.r5.formats.JsonParser().parse(r5_stream);
+    InputStream r5_stream = this.getClass().getResourceAsStream("/value_set_r5.json");
+    org.hl7.fhir.r5.model.ValueSet r5_input = (org.hl7.fhir.r5.model.ValueSet) new org.hl7.fhir.r5.formats.JsonParser().parse(r5_stream);
 
-    org.hl7.fhir.r5.model.TestScript r5_output = (org.hl7.fhir.r5.model.TestScript) VersionConvertorFactory_40_50.convertResource(r4_input);
-    org.hl7.fhir.r4.model.TestScript r4_output = (org.hl7.fhir.r4.model.TestScript) VersionConvertorFactory_40_50.convertResource(r5_input);
+    org.hl7.fhir.r5.model.ValueSet r5_output = (org.hl7.fhir.r5.model.ValueSet) VersionConvertorFactory_40_50.convertResource(r4_input);
+    org.hl7.fhir.r4.model.ValueSet r4_output = (org.hl7.fhir.r4.model.ValueSet) VersionConvertorFactory_40_50.convertResource(r5_input);
 
     org.hl7.fhir.r5.formats.JsonParser r5_parser = new org.hl7.fhir.r5.formats.JsonParser();
     String r5_in = r5_parser.setOutputStyle(OutputStyle.PRETTY).composeString(r5_input);
