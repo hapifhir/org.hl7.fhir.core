79bdb3905437513847adfa782591e57cc1ca4649
Grahame Grieve <grahameg@gmail.ccom>
2025-05-21 14:27:40 +0200

    add languages.csv support

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/MagicResources.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/MagicResources.java
@@ -3,12 +3,20 @@ package org.hl7.fhir.utilities;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.hl7.fhir.exceptions.FHIRException;
+
 public class MagicResources {
 
   public static byte[] spdxCodesAsData() throws IOException {
     ClassLoader classLoader = MagicResources.class.getClassLoader();
     InputStream help = classLoader.getResourceAsStream("spdx.json");
     return FileUtilities.streamToBytes(help);
+  }
+
+  public static CSVReader loadLanguagesCSV() throws FHIRException, IOException {
+    ClassLoader classLoader = MagicResources.class.getClassLoader();
+    InputStream source = classLoader.getResourceAsStream("languages.csv");
+    return new CSVReader(source);
   }
   
   
