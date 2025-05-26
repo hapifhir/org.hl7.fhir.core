142863b1844745cb79e77317169375335220e9b0
Corné van Beek <cornevbeek123@live.nl>
2025-05-26 16:34:59 +0200

    Convert DocumentReference.date/indexed/created conform fhir specs (#2013)
    * Convert DocumentReference.date/indexed/created conform fhir specs
    
    * null safety
    
    * use helper methods
    
    * cleanup unused import
    
    * use helper method & cleanup unused function
    
    * fix incorrect date mapping
    
    * improved variable naming
    
    * exclude created extenion when mapping back to dstu3

--- org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/conv30_40/DocumentReference30_40Test.java
+++ org.hl7.fhir.convertors/src/test/java/org/hl7/fhir/convertors/conv30_40/DocumentReference30_40Test.java
@@ -1,9 +1,11 @@
 package org.hl7.fhir.convertors.conv30_40;
 
 import org.hl7.fhir.convertors.factory.VersionConvertorFactory_30_40;
-
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,5 +18,36 @@ public class DocumentReference30_40Test {
     org.hl7.fhir.dstu3.model.DocumentReference tgt = (org.hl7.fhir.dstu3.model.DocumentReference) VersionConvertorFactory_30_40.convertResource(src);
     assertThat(tgt.getDocStatus()).isNull();
     assertThat(tgt.getDocStatusElement().getExtensionString("http://example.org/dummy-extension")).isEqualTo("true");
+  }
+
+  @Test
+  public void testDocumentReferenceConversion30To40() throws IOException {
+
+    InputStream dstu3InputJson = this.getClass().getResourceAsStream("/document_reference_30.json");
+    InputStream r4ExpectedOutputJson = this.getClass().getResourceAsStream("/document_reference_30_converted_to_40.json");
+
+    org.hl7.fhir.dstu3.model.DocumentReference dstu3Input = (org.hl7.fhir.dstu3.model.DocumentReference) new org.hl7.fhir.dstu3.formats.JsonParser().parse(dstu3InputJson);
+    org.hl7.fhir.r4.model.Resource r4Converted = VersionConvertorFactory_30_40.convertResource(dstu3Input);
+
+    org.hl7.fhir.r4.formats.JsonParser r4Parser = new org.hl7.fhir.r4.formats.JsonParser();
+    org.hl7.fhir.r4.model.Resource r4Expected = r4Parser.parse(r4ExpectedOutputJson);
+
+    Assertions.assertTrue(r4Expected.equalsDeep(r4Converted),
+      "Failed comparing\n" + r4Parser.composeString(r4Expected) + "\nand\n" + r4Parser.composeString(r4Converted));
+  }
+
+  @Test
+  void testDocumentReferenceConversion40To30() throws IOException {
+    InputStream r4InputJson = this.getClass().getResourceAsStream("/document_reference_40.json");
+    InputStream dstu3ExpectedOutputJson = this.getClass().getResourceAsStream("/document_reference_40_converted_to_30.json");
+
+    org.hl7.fhir.r4.model.DocumentReference r4input = (org.hl7.fhir.r4.model.DocumentReference) new org.hl7.fhir.r4.formats.JsonParser().parse(r4InputJson);
+    org.hl7.fhir.dstu3.model.Resource dstu3Converted = VersionConvertorFactory_30_40.convertResource(r4input);
+
+    org.hl7.fhir.dstu3.formats.JsonParser dstu3Parser = new org.hl7.fhir.dstu3.formats.JsonParser();
+    org.hl7.fhir.dstu3.model.Resource dstu3Expected = dstu3Parser.parse(dstu3ExpectedOutputJson);
+
+    Assertions.assertTrue(dstu3Expected.equalsDeep(dstu3Converted),
+      "Failed comparing\n" + dstu3Parser.composeString(dstu3Expected) + "\nand\n" + dstu3Parser.composeString(dstu3Converted));
   }
 }
