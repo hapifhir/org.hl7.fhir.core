7ef47fdeb3dc8c521f7a1dae057f7062a411ac4f
Grahame Grieve <grahameg@gmail.ccom>
2025-05-28 00:46:54 +0300

    #1800 fix type heirarchy for XhtmlType

--- org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/model/Base.java
+++ org.hl7.fhir.r4/src/main/java/org/hl7/fhir/r4/model/Base.java
@@ -960,7 +960,7 @@ public abstract class Base implements Serializable, IBase, IElement {
     if (b instanceof Element) {
       return ((Element) b).getXhtml();
     } else if (b instanceof XhtmlType) {
-      return ((XhtmlType) b).getValue();
+      return ((XhtmlType) b).getXhtml();
     } else if (b instanceof StringType) {
       try {
         return new XhtmlParser().parseFragment(((StringType) b).asStringValue());
@@ -979,7 +979,7 @@ public abstract class Base implements Serializable, IBase, IElement {
       return ((Element) b).getValue();
     } else if (b instanceof XhtmlType) {
       try {
-        return new XhtmlComposer(true).compose(((XhtmlType) b).getValue());
+        return new XhtmlComposer(true).compose(((XhtmlType) b).getXhtml());
       } catch (IOException e) {
         return null;
       }
@@ -1046,5 +1046,15 @@ public abstract class Base implements Serializable, IBase, IElement {
 
   public void copyValues(Base dst) {
   }
+
+  /**
+   * return set XHTML if this is an XHTML node, else error
+   * 
+   * @return
+   */
+  public Base setXhtml(XhtmlNode node) {
+    throw new Error("This node does not support xhtml");
+  }
+
 
 }
