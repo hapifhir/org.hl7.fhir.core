9ea5acc5d236c039ec1364ca0e2980674937158e
dotasek <david.otasek@smilecdr.com>
2025-06-23 11:11:15 -0400

    Deprecation removal 12

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/profilemodel/gen/PEGeneratedBase.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/profilemodel/gen/PEGeneratedBase.java
@@ -36,33 +36,5 @@ public class PEGeneratedBase {
   
   protected IWorkerContext workerContext;
 
-  @Deprecated
-  protected PEInstance instance;
-  
-  @Deprecated
-  protected void removeChild(String name) {
-    PEInstance child = instance.child(name);
-    if (child != null) {
-      instance.removeChild(child);
-    }
-  }
-
-  @Deprecated
-  protected void removeChildren(String name) {
-    for (PEInstance child : instance.children(name)) {
-      instance.removeChild(child);
-    }
-  }
-
-  @Deprecated
-  public PEInstance getInstance() {
-    return instance;
-  }
-  
-  @Deprecated
-  public Base getData() {
-    return instance.getBase();
-  }
-  
 }
 
