654095e4d53cacbcc29f3a2edfc58739be7ceb4d
dotasek <david.otasek@smilecdr.com>
2025-07-03 10:59:44 -0400

    Refactor for legibility

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/POSource.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/i18n/POSource.java
@@ -16,16 +16,15 @@ import org.hl7.fhir.utilities.Utilities;
 public class POSource {
 
   private String lang;
-  private List<POObject> entries = new ArrayList<>();
+  private List<POObject> poObjects = new ArrayList<>();
   private List<String> prefixes = new ArrayList<>();
-  
-  
+
   public String getLang() {
     return lang;
   }
 
-  public List<POObject> getEntries() {
-    return entries;
+  public List<POObject> getPOObjects() {
+    return poObjects;
   }
 
   public List<String> getPrefixes() {
@@ -41,40 +40,40 @@ public class POSource {
   }
   
   private static POSource loadPOFile(String[] lines) throws FileNotFoundException, IOException {
-    POSource res = new POSource();
-    POObject obj = null;
+    POSource poSource = new POSource();
+    POObject poObject = null;
     int i = 0;
     while (i < lines.length) {
       String line = lines[i];
       if (Utilities.noString(line)) {
         // else 
       } else if (line.startsWith("#:")) {
-        if (obj == null || obj.getId() != null) {
-          obj = new POObject();
-          res.entries.add(obj);
+        if (poObject == null || poObject.getId() != null) {
+          poObject = new POObject();
+          poSource.poObjects.add(poObject);
         }
-        obj.setId(line.substring(2).trim());  
+        poObject.setId(line.substring(2).trim());
       } else if (line.startsWith("# ") && line.contains(" -> ")) {
-        res.lang = line.substring(line.indexOf("->")+2).trim();
+        poSource.lang = line.substring(line.indexOf("->")+2).trim();
       } else if (line.startsWith("# ")) {
-        if (obj == null || obj.getComment() != null) {
-          obj = new POObject();
-          res.entries.add(obj);
+        if (poObject == null || poObject.getComment() != null) {
+          poObject = new POObject();
+          poSource.poObjects.add(poObject);
         }
-        obj.setComment(line.substring(1).trim());
-        if (obj.getComment().contains("!!when")) {
-          obj.setOldMsgId(obj.getComment().substring(obj.getComment().indexOf("!!when")));
-          obj.setComment(obj.getComment().substring(0, obj.getComment().indexOf("!!when")-1));
-          obj.setOldMsgId(obj.getOldMsgId().substring(obj.getOldMsgId().indexOf(": ")+2).trim());
-          obj.setOldMsgId(obj.getOldMsgId().substring(0, obj.getOldMsgId().length()-1));
+        poObject.setComment(line.substring(1).trim());
+        if (poObject.getComment().contains("!!when")) {
+          poObject.setOldMsgId(poObject.getComment().substring(poObject.getComment().indexOf("!!when")));
+          poObject.setComment(poObject.getComment().substring(0, poObject.getComment().indexOf("!!when")-1));
+          poObject.setOldMsgId(poObject.getOldMsgId().substring(poObject.getOldMsgId().indexOf(": ")+2).trim());
+          poObject.setOldMsgId(poObject.getOldMsgId().substring(0, poObject.getOldMsgId().length()-1));
         }
-      } else if (obj == null) {
-        res.prefixes.add(line);  
+      } else if (poObject == null) {
+        poSource.prefixes.add(line);
       } else if (line.startsWith("#,")) {
         // retired use of #| because it caused problems with the tools
-        String s = line.substring(2).trim();
-        for (String f : s.split("\\,")) {
-          obj.getFlags().add(f.trim());
+        String flags = line.substring(2).trim();
+        for (String flag : flags.split(",")) {
+          poObject.getFlags().add(flag.trim());
         }
       } else if (line.startsWith("#|")) {
         // retired use of #| because it caused problems with the tools
@@ -86,31 +85,31 @@ public class POSource {
           i++;
           s += trimQuotes(lines[i]);
         }
-        obj.setOldMsgId(s);
+        poObject.setOldMsgId(s);
       } else if (line.startsWith("msgid ")) {
         String s = trimQuotes(line.substring(5).trim());
-        if (s.endsWith("("+obj.getId()+")")) {
-          s = s.substring(0, s.length() - (obj.getId().length()+3));
+        if (s.endsWith("("+poObject.getId()+")")) {
+          s = s.substring(0, s.length() - (poObject.getId().length()+3));
         }
         while (i < lines.length-1 && lines[i+1].startsWith("\"")) {
           i++;
           s += trimQuotes(lines[i]);
         }
-        obj.setMsgid(s);
+        poObject.setMsgid(s);
       } else if (line.startsWith("msgid_plural ")) {
         String s = trimQuotes(line.substring(12).trim());
         while (i < lines.length-1 && lines[i+1].startsWith("\"")) {
           i++;
           s += trimQuotes(lines[i]);
         }
-        obj.setMsgidPlural(s);
+        poObject.setMsgidPlural(s);
       } else if (line.startsWith("msgstr ")) {
         String s = trimQuotes(line.substring(6).trim());
         while (i < lines.length-1 && lines[i+1].startsWith("\"")) {
           i++;
           s += trimQuotes(lines[i]);
         }
-        obj.getMsgstr().add(s);
+        poObject.getMsgstr().add(s);
       } else if (line.startsWith("msgctxt ")) {
         // ignore this
       } else if (line.startsWith("msgstr[")) {
@@ -125,17 +124,17 @@ public class POSource {
         while (s.startsWith("!!")) {
           s = s.substring(2);
         }
-        if (c != obj.getMsgstr().size()) {
+        if (c != poObject.getMsgstr().size()) {
           log.warn("index issue");
         } else { // if (!Utilities.noString(s)) {
-          obj.getMsgstr().add(s);          
+          poObject.getMsgstr().add(s);
         }
       } else {
         log.warn("unknown line: "+line);
       }
       i++;
     }
-    return res;
+    return poSource;
   }
 
 
@@ -151,53 +150,53 @@ public class POSource {
   }
 
 
-  public int savePOFile(String dest, int count, int noTrans) throws IOException {
+  public int savePOFile(String poFilePath, int numberOfPlurals, int noTrans) throws IOException {
     // source.prefixes.clear();
     
-    StringBuilder b = new StringBuilder();
-    for (String p : prefixes) {
-      b.append(p);
-      b.append("\r\n");
+    StringBuilder sb = new StringBuilder();
+    for (String prefix : prefixes) {
+      sb.append(prefix);
+      sb.append("\r\n");
     }
-    b.append("\r\n");
-    for (POObject o : entries) {
+    sb.append("\r\n");
+    for (POObject poObject : poObjects) {
       // for POEdit
-      if (o.getOldMsgId() != null) {
-        b.append("# "+o.getComment()+" (!!when last translated was: "+o.getOldMsgId()+")\r\n");        
+      if (poObject.getOldMsgId() != null) {
+        sb.append("# "+poObject.getComment()+" (!!when last translated was: "+poObject.getOldMsgId()+")\r\n");
       } else {
-        b.append("# "+o.getComment()+"\r\n");
+        sb.append("# "+poObject.getComment()+"\r\n");
       }
-      b.append("#: "+o.getId()+"\r\n");
-      if (!o.getFlags().isEmpty()) {
-        b.append("#, "+ CommaSeparatedStringBuilder.join(",", o.getFlags()) + "\r\n");
+      sb.append("#: "+poObject.getId()+"\r\n");
+      if (!poObject.getFlags().isEmpty()) {
+        sb.append("#, "+ CommaSeparatedStringBuilder.join(",", poObject.getFlags()) + "\r\n");
       }
-      if (o.isDuplicate()) {
-        b.append("msgctxt \""+o.getId()+"\"\r\n");        
+      if (poObject.isDuplicate()) {
+        sb.append("msgctxt \""+poObject.getId()+"\"\r\n");
       } 
-      String m = Utilities.noString(o.getMsgid()) ? "-- no content: do not translate #"+(++noTrans )+" --" : o.getMsgid(); 
-      b.append("msgid \""+wrapQuotes(m)+"\"\r\n");
-      if (o.getMsgidPlural() != null) {
-        b.append("msgid_plural \""+wrapQuotes(o.getMsgidPlural())+"\"\r\n"); 
-        while (o.getMsgstr().size() < count) {
-          o.getMsgstr().add("");
+      String m = Utilities.noString(poObject.getMsgid()) ? "-- no content: do not translate #"+(++noTrans )+" --" : poObject.getMsgid();
+      sb.append("msgid \""+wrapQuotes(m)+"\"\r\n");
+      if (poObject.getMsgidPlural() != null) {
+        sb.append("msgid_plural \""+wrapQuotes(poObject.getMsgidPlural())+"\"\r\n");
+        while (poObject.getMsgstr().size() < numberOfPlurals) {
+          poObject.getMsgstr().add("");
         }
-        for (int i = 0; i < o.getMsgstr().size(); i++) {
-          String s = o.getMsgstr().get(i);
+        for (int i = 0; i < poObject.getMsgstr().size(); i++) {
+          String s = poObject.getMsgstr().get(i);
 //          if (tfxMode && Utilities.noString(s)) {
 //            s = Utilities.noString(i == 0 ? o.msgid : o.msgidPlural) ? "-- no content: do not translate --" : "";
 //          }
-          b.append("msgstr["+i+"] \""+wrapQuotes(s)+"\"\r\n");
+          sb.append("msgstr["+i+"] \""+wrapQuotes(s)+"\"\r\n");
         }
       } else {
-        if (o.getMsgstr().size() == 0) {
-          b.append("msgstr \"\"\r\n");                  
+        if (poObject.getMsgstr().size() == 0) {
+          sb.append("msgstr \"\"\r\n");
         } else {
-          b.append("msgstr \""+wrapQuotes(o.getMsgstr().get(0))+"\"\r\n");                            
+          sb.append("msgstr \""+wrapQuotes(poObject.getMsgstr().get(0))+"\"\r\n");
         }
       } 
-      b.append("\r\n");
+      sb.append("\r\n");
     }
-    FileUtilities.stringToFile(b.toString(), dest);
+    FileUtilities.stringToFile(sb.toString(), poFilePath);
     return noTrans;
   }
 
