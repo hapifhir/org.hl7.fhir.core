56feb5f8acfff1be9d714e7a0e1dfa39dbefca70
James Agnew <jamesagnew@gmail.com>
2025-07-14 17:49:44 -0400

    Improve search URL detection

--- org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/instance/InstanceValidatorTests.java
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/instance/InstanceValidatorTests.java
@@ -7,39 +7,50 @@ import org.hl7.fhir.utilities.validation.ValidationOptions;
 import org.hl7.fhir.r5.context.IWorkerContext;
 import org.hl7.fhir.r5.model.CodeableConcept;
 import org.hl7.fhir.r5.model.ValueSet;
+import org.hl7.fhir.validation.BaseValidator;
 import org.hl7.fhir.validation.ValidatorSettings;
 import org.hl7.fhir.validation.instance.utils.NodeStack;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Locale;
+import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
+import static org.apache.commons.lang3.StringUtils.trim;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class InstanceValidatorTests {
+
+  @Mock
+  private IWorkerContext context;
 
   @Test
   public void testCheckCodeOnServerNoStackLocale() throws InstanceValidator.CheckCodeOnServerException {
-      NodeStack stack =mock(NodeStack.class);
-      testCheckCodeOnServer(stack, "ko-KR");
+    when(context.getLocale()).thenReturn(Locale.KOREA);
+    NodeStack stack =mock(NodeStack.class);
+    testCheckCodeOnServer(stack, "ko-KR");
   }
 
   @Test
   public void testCheckCodeOnServerStackLocale() throws InstanceValidator.CheckCodeOnServerException {
-      NodeStack stack =mock(NodeStack.class);
-      when(stack.getWorkingLang()).thenReturn("fr-CA");
-      testCheckCodeOnServer(stack, "fr-CA");
+    NodeStack stack =mock(NodeStack.class);
+    when(stack.getWorkingLang()).thenReturn("fr-CA");
+    testCheckCodeOnServer(stack, "fr-CA");
   }
 
   private void testCheckCodeOnServer(NodeStack stack, String expectedLocale) throws InstanceValidator.CheckCodeOnServerException {
-    IWorkerContext context = mock(IWorkerContext.class);
-    when(context.getLocale()).thenReturn(Locale.KOREA);
     when(context.getVersion()).thenReturn("5.0.1");
     InstanceValidator instanceValidator = new InstanceValidator(context, null, null, null, new ValidatorSettings());
 
@@ -55,13 +66,11 @@ class InstanceValidatorTests {
 
     ValidationOptions options = validationOptionsArgumentCaptor.getValue();
 
-    Assertions.assertEquals(expectedLocale, options.getLanguages().getSource());
+    assertEquals(expectedLocale, options.getLanguages().getSource());
   }
 
   @Test
   void testElementDebug() throws IOException {
-    IWorkerContext context = mock(IWorkerContext.class);
-    when(context.getLocale()).thenReturn(Locale.KOREA);
     when(context.getVersion()).thenReturn("5.0.1");
     InstanceValidator instanceValidator = new InstanceValidator(context, null, null, null, new ValidatorSettings());
 
@@ -71,5 +80,36 @@ class InstanceValidatorTests {
     instanceValidator.debugElement(element, mockLogger);
     verify(mockLogger, times(1)).debug("dummyName" + System.lineSeparator());
   }
+
+  @ParameterizedTest
+  @CsvSource({
+    // Good URLs
+    "Patient?name=simpson                              , true",
+    "Patient?name:exact=simpson                        , true",
+    "Patient?name.chain=simpson                        , true",
+    "Patient?family=simpson&given=homer                , true",
+    "Patient?family=                                   , true",
+    "Patient?family=&given=                            , true",
+    // Bad URLs
+    "?                                                 , false",
+    "name=simpson                                      , false",
+    "?name=simpson                                     , false",
+    "Hello?name=simpson                                , false",
+    "Patient?                                          , false",
+    "Patient?=simpson                                  , false",
+    "Patient?==simpson                                 , false",
+    "Patient?==                                        , false",
+    "Patient?family==simpson                           , false",
+    "Patient?f&mily=simpson                            , false",
+  })
+  void testIsSearchUrl(String theInput, boolean theExpected) {
+    if (theInput.contains("?")) {
+      when(context.getResourceNamesAsSet()).thenReturn(Set.of("Patient"));
+    }
+
+    boolean actual = BaseValidator.isSearchUrl(context, trim(theInput));
+    assertEquals(theExpected, actual);
+  }
+
 
 }
