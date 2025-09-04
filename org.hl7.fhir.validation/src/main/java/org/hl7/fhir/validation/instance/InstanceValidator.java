e4dc432b58ba917fbb5109e8e23bfc3983440209
dotasek <david.otasek@smilecdr.com>
2025-09-04 12:46:04 -0400

    Combine rules to check for empty snapshot

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/instance/InstanceValidator.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/instance/InstanceValidator.java
@@ -7368,17 +7368,14 @@ public class InstanceValidator extends BaseValidator implements IResourceValidat
           url = url.substring(0, url.indexOf("#"));
         }
         p = this.context.fetchResource(StructureDefinition.class, typeProfile);
-        if (rule(errors, NO_RULE_DATE, IssueType.STRUCTURE, ei.line(), ei.col(), ei.getPath(), p != null, I18nConstants.VALIDATION_VAL_UNKNOWN_PROFILE, typeProfile)) {
-          if (!p.getSnapshot().hasElement()) {
-            rule(errors, null, IssueType.INVALID, stack, false, I18nConstants.SNAPSHOT_IS_EMPTY, p.getVersionedUrl());
-          } else {
-            List<ValidationMessage> profileErrors = new ArrayList<ValidationMessage>();
-            validateElement(valContext, profileErrors, p, getElementByTail(p, tail), profile, checkDefn, resource, ei.getElement(), type, localStack, thisIsCodeableConcept, checkDisplay, thisExtension, pct, mode); // we don't track ok here
-            if (hasErrors(profileErrors))
-              badProfiles.put(typeProfile, profileErrors);
-            else
-              goodProfiles.put(typeProfile, profileErrors);
-          }
+        if (rule(errors, NO_RULE_DATE, IssueType.STRUCTURE, ei.line(), ei.col(), ei.getPath(), p != null, I18nConstants.VALIDATION_VAL_UNKNOWN_PROFILE, typeProfile)
+          && rule(errors, NO_RULE_DATE, IssueType.STRUCTURE, ei.line(), ei.col(), ei.getPath(), p.getSnapshot().hasElement(), I18nConstants.VALIDATION_VAL_PROFILE_NOSNAPSHOT, typeProfile)) {
+          List<ValidationMessage> profileErrors = new ArrayList<ValidationMessage>();
+          validateElement(valContext, profileErrors, p, getElementByTail(p, tail), profile, checkDefn, resource, ei.getElement(), type, localStack, thisIsCodeableConcept, checkDisplay, thisExtension, pct, mode); // we don't track ok here
+          if (hasErrors(profileErrors))
+            badProfiles.put(typeProfile, profileErrors);
+          else
+            goodProfiles.put(typeProfile, profileErrors);
         } else {
           ok = false;
         }
