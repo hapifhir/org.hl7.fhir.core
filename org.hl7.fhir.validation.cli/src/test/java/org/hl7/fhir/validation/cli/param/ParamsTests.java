0b81da5f2b31e0697ae27d620d072fd63764a84a
dotasek <david.otasek@smilecdr.com>
2025-10-07 16:51:38 -0400

    Add more params tests

--- org.hl7.fhir.validation.cli/src/test/java/org/hl7/fhir/validation/cli/param/ParamsTests.java
+++ org.hl7.fhir.validation.cli/src/test/java/org/hl7/fhir/validation/cli/param/ParamsTests.java
@@ -4,11 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Locale;
 
-import org.hl7.fhir.utilities.filesystem.ManagedFileAccess;
 import org.hl7.fhir.validation.service.model.ValidationContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,5 +24,12 @@ class ParamsTests {
     Error expectedError = assertThrows(Error.class, () -> Params.hasParamAndValue(new String[]{"-param"}, "-param"));
     assertThat(expectedError.getMessage()).contains("-param");
     assertThat(expectedError.getMessage()).contains("without providing a value");
+  }
+
+  @Test
+  void testMultipleParamAndValue() throws Exception {
+    String[] args = new String[]{"-input", "first", "-input", "second", "meh"};
+    Collection<String> values = Params.getMultiValueParam(args, "-input");
+    assertThat(values).containsExactly("first", "second");
   }
 }
