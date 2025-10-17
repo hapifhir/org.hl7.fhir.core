e7cda4d096bd60293f145da6912dd97980007484
dotasek <david.otasek@smilecdr.com>
2025-10-17 16:15:01 -0400

    Move input to TransformLang param object and parser

--- org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/service/model/TransformLangParameters.java
+++ org.hl7.fhir.validation/src/main/java/org/hl7/fhir/validation/service/model/TransformLangParameters.java
@@ -3,6 +3,8 @@ package org.hl7.fhir.validation.service.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class TransformLangParameters {
@@ -14,6 +16,10 @@ public class TransformLangParameters {
   @SerializedName("tgtLang")
   private
   String tgtLang = null;
+
+  @JsonProperty("inputs")
+  @SerializedName("inputs")
+  private List<String> inputs = new ArrayList<String>();
 
   public String getSrcLang() {
     return srcLang;
@@ -33,20 +39,40 @@ public class TransformLangParameters {
     return this;
   }
 
+  @JsonProperty("inputs")
+  @SerializedName("inputs")
+  public List<String> getInputs() {
+    return inputs;
+  }
+
+  @JsonProperty("inputs")
+  @SerializedName("inputs")
+  public TransformLangParameters setInputs(List<String> inputs) {
+    this.inputs = inputs;
+    return this;
+  }
+
+  public TransformLangParameters addInput(String input) {
+    inputs.add(input);
+    return this;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     TransformLangParameters that = (TransformLangParameters) o;
     return Objects.equals(srcLang, that.srcLang)
-      && Objects.equals(tgtLang, that.tgtLang);
+      && Objects.equals(tgtLang, that.tgtLang)
+      && Objects.equals(inputs, that.inputs);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(
       srcLang,
-      tgtLang);
+      tgtLang,
+      inputs);
   }
 
   @Override
@@ -54,6 +80,7 @@ public class TransformLangParameters {
     return "TransformLangParameters{" +
       "srcLang='" + srcLang + '\'' +
       ", tgtLang='" + tgtLang + '\'' +
+      ", inputs=" + inputs +
       "}";
   }
 }
