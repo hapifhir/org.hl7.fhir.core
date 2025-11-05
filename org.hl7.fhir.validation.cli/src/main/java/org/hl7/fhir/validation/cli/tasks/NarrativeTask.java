e38b2625fe6021a19819e65f68e830b32fa6b042
dotasek <david.otasek@smilecdr.com>
2025-11-05 15:36:59 -0500

    Update NarrativeTask

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/NarrativeTask.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/NarrativeTask.java
@@ -1,9 +1,11 @@
 package org.hl7.fhir.validation.cli.tasks;
 
 import org.hl7.fhir.validation.ValidationEngine;
+import org.hl7.fhir.validation.cli.param.Arg;
 import org.hl7.fhir.validation.cli.param.Params;
 import org.hl7.fhir.validation.cli.param.parsers.NarrativeParametersParser;
-import org.hl7.fhir.validation.service.model.ValidationContext;
+import org.hl7.fhir.validation.cli.param.parsers.OutputParametersParser;
+import org.hl7.fhir.validation.service.model.OutputParameters;
 import org.hl7.fhir.validation.service.ValidationService;
 import org.hl7.fhir.validation.cli.Display;
 import org.slf4j.Logger;
@@ -38,8 +40,33 @@ public class NarrativeTask extends ValidationEngineTask {
   }
 
   @Override
-  public void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine, @Nonnull ValidationContext validationContext, @Nonnull String[] args) throws Exception {
-    validationService.generateNarrative(validationContext, validationEngine);
+  protected NarrativeTaskInstance getValidationEngineTaskInstance(Arg[] args) {
+    return new NarrativeTaskInstance(args);
   }
 
+  protected class NarrativeTaskInstance extends ValidationEngineTaskInstance {
+
+    OutputParameters outputParameters;
+
+    NarrativeTaskInstance(Arg[] args) {
+      super(args);
+    }
+
+    @Override
+    protected boolean usesInstanceValidatorParameters() {
+      return false;
+    }
+
+    @Override
+    protected void buildTaskSpecificParametersFromArgs(Arg[] args) {
+      OutputParametersParser outputParametersParser = new OutputParametersParser();
+      outputParametersParser.parseArgs(args);
+      outputParameters = outputParametersParser.getParameterObject();
+    }
+
+    @Override
+    protected void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine) throws Exception {
+      validationService.generateNarrative(validationEngine, new org.hl7.fhir.validation.service.GenerateNarrativeParameters(validationEngineParameters, sources, outputParameters.getOutput()));
+    }
+  }
 }
