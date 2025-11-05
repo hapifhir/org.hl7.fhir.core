84a911ac7ea9949c9fda27c8effd57544a5e8ece
dotasek <david.otasek@smilecdr.com>
2025-11-05 16:37:55 -0500

    Update InstallTask

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/InstallTask.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/InstallTask.java
@@ -1,9 +1,9 @@
 package org.hl7.fhir.validation.cli.tasks;
 
 import org.hl7.fhir.validation.ValidationEngine;
+import org.hl7.fhir.validation.cli.param.Arg;
 import org.hl7.fhir.validation.cli.param.Params;
 import org.hl7.fhir.validation.cli.param.parsers.InstallParametersParser;
-import org.hl7.fhir.validation.service.model.ValidationContext;
 import org.hl7.fhir.validation.service.ValidationService;
 import org.slf4j.Logger;
 
@@ -37,12 +37,34 @@ public class InstallTask extends ValidationEngineTask {
   }
 
   @Override
-  public void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine, @Nonnull ValidationContext validationContext, @Nonnull String[] args) throws Exception {
-    validationService.install(validationContext.getIgs(), validationEngine);
+  public boolean inferFhirVersion() {
+    return true;
   }
 
   @Override
-  public boolean inferFhirVersion() {
-    return true;
+  protected InstallTaskInstance getValidationEngineTaskInstance(Arg[] args) {
+    return new InstallTaskInstance(args);
+  }
+
+  protected class InstallTaskInstance extends ValidationEngineTaskInstance {
+
+    InstallTaskInstance(Arg[] args) {
+      super(args);
+    }
+
+    @Override
+    protected boolean usesInstanceValidatorParameters() {
+      return false;
+    }
+
+    @Override
+    protected void buildTaskSpecificParametersFromArgs(Arg[] args) {
+
+    }
+
+    @Override
+    protected void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine) throws Exception {
+      validationService.install(validationEngineParameters.getIgs(), validationEngine);
+    }
   }
 }
