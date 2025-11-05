18c341a5e991f4ef9b2d75950f574851804d1eb1
dotasek <david.otasek@smilecdr.com>
2025-11-05 16:34:14 -0500

    Update InstanceFactoryTask

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/InstanceFactoryTask.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/InstanceFactoryTask.java
@@ -1,8 +1,10 @@
 package org.hl7.fhir.validation.cli.tasks;
 
 import org.hl7.fhir.validation.ValidationEngine;
+import org.hl7.fhir.validation.cli.param.Arg;
 import org.hl7.fhir.validation.cli.param.Params;
 import org.hl7.fhir.validation.cli.param.parsers.InstanceFactoryParametersParser;
+import org.hl7.fhir.validation.service.model.InstanceFactoryParameters;
 import org.hl7.fhir.validation.service.model.ValidationContext;
 import org.hl7.fhir.validation.service.ValidationService;
 import org.slf4j.Logger;
@@ -37,8 +39,33 @@ public class InstanceFactoryTask extends ValidationEngineTask {
   }
 
   @Override
-  public void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine, @Nonnull ValidationContext validationContext, @Nonnull String[] args) throws Exception {
-    validationService.instanceFactory(validationContext, validationEngine);
+  protected InstanceFactoryTaskInstance getValidationEngineTaskInstance(Arg[] args) {
+    return new InstanceFactoryTaskInstance(args);
   }
 
+  protected class InstanceFactoryTaskInstance extends ValidationEngineTaskInstance {
+
+    InstanceFactoryParameters instanceFactoryParameters;
+
+    InstanceFactoryTaskInstance(Arg[] args) {
+      super(args);
+    }
+
+    @Override
+    protected boolean usesInstanceValidatorParameters() {
+      return false;
+    }
+
+    @Override
+    protected void buildTaskSpecificParametersFromArgs(Arg[] args) {
+      InstanceFactoryParametersParser instanceFactoryParametersParser = new InstanceFactoryParametersParser();
+      instanceFactoryParametersParser.parseArgs(args);
+      instanceFactoryParameters = instanceFactoryParametersParser.getParameterObject();
+    }
+
+    @Override
+    protected void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine) throws Exception {
+      validationService.instanceFactory(validationEngine, instanceFactoryParameters.getSource());
+    }
+  }
 }
