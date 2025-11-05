390ce0d9a8d16d809ed8cd61ad82bde8447e9eef
dotasek <david.otasek@smilecdr.com>
2025-11-05 10:36:17 -0500

    Update TransformTask

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/TransformTask.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/TransformTask.java
@@ -1,11 +1,18 @@
 package org.hl7.fhir.validation.cli.tasks;
 
 import org.hl7.fhir.validation.ValidationEngine;
+import org.hl7.fhir.validation.cli.param.Arg;
 import org.hl7.fhir.validation.cli.param.Params;
 import org.hl7.fhir.validation.cli.param.parsers.MapParametersParser;
+import org.hl7.fhir.validation.cli.param.parsers.OutputParametersParser;
+import org.hl7.fhir.validation.cli.param.parsers.WatchParametersParser;
+import org.hl7.fhir.validation.service.TransformParameters;
+import org.hl7.fhir.validation.service.model.MapParameters;
+import org.hl7.fhir.validation.service.model.OutputParameters;
 import org.hl7.fhir.validation.service.model.ValidationContext;
 import org.hl7.fhir.validation.service.ValidationService;
 import org.hl7.fhir.validation.cli.Display;
+import org.hl7.fhir.validation.service.model.WatchParameters;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
@@ -43,8 +50,38 @@ public class TransformTask extends ValidationEngineTask {
   }
 
   @Override
-  public void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine, @Nonnull ValidationContext validationContext, @Nonnull String[] args) throws Exception {
-    validationService.transform(validationContext, validationEngine);
+  protected ValidationEngineTaskInstance getValidationEngineTaskInstance(Arg[] args) {
+    return new TransformTaskInstance(args);
   }
 
+  protected class TransformTaskInstance extends ValidationEngineTaskInstance {
+
+    MapParameters mapParameters;
+    OutputParameters outputParameters;
+
+    TransformTaskInstance(Arg[] args) {
+      super(args);
+    }
+
+    @Override
+    protected boolean usesInstanceValidatorParameters() {
+      return false;
+    }
+
+    @Override
+    protected void buildTaskSpecificParametersFromArgs(Arg[] args) {
+        MapParametersParser mapParametersParser = new MapParametersParser();
+        OutputParametersParser outputParametersParser = new OutputParametersParser();
+        mapParametersParser.parseArgs(args);
+        outputParametersParser.parseArgs(args);
+        mapParameters = mapParametersParser.getParameterObject();
+        outputParameters = outputParametersParser.getParameterObject();
+    }
+
+    @Override
+    protected void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine) throws Exception {
+      TransformParameters transformParameters = new TransformParameters(validationEngineParameters,mapParameters,sources,outputParameters);
+      validationService.transform(validationEngine,  transformParameters);
+    }
+  }
 }
