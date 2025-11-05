c0c45da0adfa63569cdb87f0ec97cb0425a5f354
dotasek <david.otasek@smilecdr.com>
2025-11-05 12:50:21 -0500

    Update ScanTask

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/ScanTask.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/tasks/ScanTask.java
@@ -2,9 +2,11 @@ package org.hl7.fhir.validation.cli.tasks;
 
 import org.hl7.fhir.validation.Scanner;
 import org.hl7.fhir.validation.ValidationEngine;
+import org.hl7.fhir.validation.cli.param.Arg;
 import org.hl7.fhir.validation.cli.param.Params;
+import org.hl7.fhir.validation.cli.param.parsers.OutputParametersParser;
 import org.hl7.fhir.validation.cli.param.parsers.ScanParametersParser;
-import org.hl7.fhir.validation.service.model.ValidationContext;
+import org.hl7.fhir.validation.service.model.OutputParameters;
 import org.hl7.fhir.validation.service.ValidationService;
 import org.slf4j.Logger;
 
@@ -39,9 +41,34 @@ public class ScanTask extends ValidationEngineTask {
   }
 
   @Override
-  public void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine, @Nonnull ValidationContext validationContext, @Nonnull String[] args) throws Exception {
-    Scanner validationScanner = new Scanner(validationEngine.getContext(), validationEngine.getValidator(null), validationEngine.getIgLoader(), validationEngine.getFhirPathEngine());
-    validationScanner.validateScan(validationContext.getOutput(), validationContext.getSources());
+  protected ScanTaskInstance getValidationEngineTaskInstance(Arg[] args) {
+    return new ScanTaskInstance(args);
   }
 
+  protected class ScanTaskInstance extends ValidationEngineTaskInstance {
+
+    OutputParameters outputParameters;
+
+    ScanTaskInstance(Arg[] args) {
+      super(args);
+    }
+
+    @Override
+    protected boolean usesInstanceValidatorParameters() {
+      return false;
+    }
+
+    @Override
+    protected void buildTaskSpecificParametersFromArgs(Arg[] args) {
+      OutputParametersParser outputParametersParser = new OutputParametersParser();
+      outputParametersParser.parseArgs(args);
+      outputParameters = outputParametersParser.getParameterObject();
+    }
+
+    @Override
+    protected void executeTask(@Nonnull ValidationService validationService, @Nonnull ValidationEngine validationEngine) throws Exception {
+      Scanner validationScanner = new Scanner(validationEngine.getContext(), validationEngine.getValidator(null), validationEngine.getIgLoader(), validationEngine.getFhirPathEngine());
+      validationScanner.validateScan(outputParameters.getOutput(), sources);
+    }
+  }
 }
