58c96d94c3a7b51b9f6ff9b7d5f6cb951369e066
Grahame Grieve <grahameg@gmail.ccom>
2025-12-02 06:11:29 +0300

    Fix issue with reindexing packages unnecessarily

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/npm/NpmPackage.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/npm/NpmPackage.java
@@ -685,7 +685,7 @@ public class NpmPackage {
   public boolean isIndexed() throws IOException {
     for (NpmPackageFolder folder : folders.values()) {
       JsonObject index = folder.index();
-      if (folder.index() == null || index.forceArray("files").size() == 0) {
+      if (folder.index() == null) {
         return false;
       }
     }
