5a9213a876f533a7a14161b61567378828705057
Grahame Grieve <grahameg@gmail.ccom>
2025-12-02 06:12:23 +0300

    Add support for -sct in validator test cases

--- org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/ValidationTests.java
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/ValidationTests.java
@@ -37,6 +37,7 @@ import org.hl7.fhir.r5.formats.XmlParser;
 import org.hl7.fhir.r5.model.*;
 import org.hl7.fhir.r5.model.OperationOutcome.OperationOutcomeIssueComponent;
 import org.hl7.fhir.r5.terminologies.client.TerminologyClientContext;
+import org.hl7.fhir.r5.terminologies.utilities.SnomedUtilities;
 import org.hl7.fhir.r5.test.utils.TestingUtilities;
 import org.hl7.fhir.r5.utils.OperationOutcomeUtilities;
 import org.hl7.fhir.r5.utils.validation.BundleValidationRule;
@@ -248,6 +249,11 @@ public class ValidationTests implements IHostApplicationServices, IValidatorReso
       val.getSettings().getCertificates().put(content.get("certificate").getAsString(), TestingUtilities.loadTestResourceBytes("validator", content.get("certificate").getAsString()));
     }
 
+    if (content.has("sct")) {
+      vCurr.setSnomedExtension(SnomedUtilities.getCodeFromAlias(content.get("sct").getAsString()));
+    } else {
+      vCurr.setSnomedExtension(null);
+    }
     if (content.has("fetcher") && "standalone".equals(JsonUtilities.str(content, "fetcher"))) {
       val.setFetcher(vCurr);
       vCurr.setFetcher(new StandAloneValidatorFetcher(vCurr.getPcm(), vCurr.getContext(), vCurr));
