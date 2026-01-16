bcfb71e30f4b39bafd6e24d678d10c0e9c42fcc1
dotasek <david.otasek@smilecdr.com>
2026-01-16 16:44:06 -0500

    Normalize text in code blocks

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xhtml/XhtmlToMarkdownConverter.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/xhtml/XhtmlToMarkdownConverter.java
@@ -167,11 +167,15 @@ public class XhtmlToMarkdownConverter {
       case "pre":
         b.append("```\r\n");
         XhtmlNode code = c.getElement("code");
+        String literalText;
         if (code != null) {
-          b.append(code.toLiteralText());
+          literalText = code.toLiteralText();
         } else {
-          b.append(c.toLiteralText());
+          literalText = c.toLiteralText();
         }
+        // Normalize line endings: convert LF to CRLF
+        literalText = literalText.replace("\r\n", "\n").replace("\n", "\r\n");
+        b.append(literalText);
         b.append("\r\n```\r\n");
         break;
       case "para":
