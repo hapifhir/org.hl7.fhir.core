975f944efdad884d74821d2920ebaf501ed74494
dotasek <david.otasek@smilecdr.com>
2026-03-12 10:38:57 -0400

    Fix expected + actual names and ordering

--- org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/FhirValidatorHttpServiceTests.java
+++ org.hl7.fhir.validation/src/test/java/org/hl7/fhir/validation/tests/FhirValidatorHttpServiceTests.java
@@ -101,8 +101,9 @@ class FhirValidatorHttpServiceTest {
     assertEquals(200, response.statusCode());
     assertTrue(response.headers().firstValue("Content-Type").orElse("").contains("application/fhir+json"));
 
-    String ss = response.body().toString();
-    assertEquals(ss, "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><p>All OK</p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td></tr><tr><td>Information</td><td/><td>Informational Note</td><td>All OK</td><td/></tr></table></div>\"},\"issue\":[{\"severity\":\"information\",\"code\":\"informational\",\"details\":{\"text\":\"All OK\"}}]}");
+    String actual = response.body();
+    String expected = "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><p>All OK</p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td></tr><tr><td>Information</td><td/><td>Informational Note</td><td>All OK</td><td/></tr></table></div>\"},\"issue\":[{\"severity\":\"information\",\"code\":\"informational\",\"details\":{\"text\":\"All OK\"}}]}";
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -124,8 +125,9 @@ class FhirValidatorHttpServiceTest {
     assertTrue(response.headers().firstValue("Content-Type").orElse("").contains("application/fhir+xml"));
 
 
-    String ss = response.body().toString();
-    assertEquals(ss, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><OperationOutcome xmlns=\"http://hl7.org/fhir\"><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\"><p class=\"res-header-id\"><b>OperationOutcome </b></p><table class=\"grid\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td><td><b>Source</b></td></tr><tr><td>Error</td><td>Patient</td><td>Invalid Content</td><td>Resource requires an id, but none is present</td><td/><td>InstanceValidator</td></tr><tr><td>Warning</td><td>Patient</td><td>Validation rule failed</td><td>Constraint failed: dom-6: 'A resource should have narrative for robust management' (defined in http://hl7.org/fhir/StructureDefinition/DomainResource) (Best Practice Recommendation)</td><td/><td>InstanceValidator</td></tr></table></div></text><issue><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\"><valueInteger value=\"2\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\"><valueInteger value=\"38\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\"><valueString value=\"InstanceValidator\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\"><valueCode value=\"Resource_RES_ID_Missing\"/></extension><severity value=\"error\"/><code value=\"invalid\"/><details><text value=\"Resource requires an id, but none is present\"/></details><expression value=\"Patient\"/></issue><issue><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\"><valueInteger value=\"2\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\"><valueInteger value=\"38\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\"><valueString value=\"InstanceValidator\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\"><valueCode value=\"http://hl7.org/fhir/StructureDefinition/DomainResource#dom-6\"/></extension><severity value=\"warning\"/><code value=\"invariant\"/><details><text value=\"Constraint failed: dom-6: 'A resource should have narrative for robust management' (defined in http://hl7.org/fhir/StructureDefinition/DomainResource) (Best Practice Recommendation)\"/></details><expression value=\"Patient\"/></issue></OperationOutcome>");
+    String actual = response.body();
+    String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><OperationOutcome xmlns=\"http://hl7.org/fhir\"><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\"><p class=\"res-header-id\"><b>OperationOutcome </b></p><table class=\"grid\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td><td><b>Source</b></td></tr><tr><td>Error</td><td>Patient</td><td>Invalid Content</td><td>Resource requires an id, but none is present</td><td/><td>InstanceValidator</td></tr><tr><td>Warning</td><td>Patient</td><td>Validation rule failed</td><td>Constraint failed: dom-6: 'A resource should have narrative for robust management' (defined in http://hl7.org/fhir/StructureDefinition/DomainResource) (Best Practice Recommendation)</td><td/><td>InstanceValidator</td></tr></table></div></text><issue><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\"><valueInteger value=\"2\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\"><valueInteger value=\"38\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\"><valueString value=\"InstanceValidator\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\"><valueCode value=\"Resource_RES_ID_Missing\"/></extension><severity value=\"error\"/><code value=\"invalid\"/><details><text value=\"Resource requires an id, but none is present\"/></details><expression value=\"Patient\"/></issue><issue><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\"><valueInteger value=\"2\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\"><valueInteger value=\"38\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\"><valueString value=\"InstanceValidator\"/></extension><extension url=\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\"><valueCode value=\"http://hl7.org/fhir/StructureDefinition/DomainResource#dom-6\"/></extension><severity value=\"warning\"/><code value=\"invariant\"/><details><text value=\"Constraint failed: dom-6: 'A resource should have narrative for robust management' (defined in http://hl7.org/fhir/StructureDefinition/DomainResource) (Best Practice Recommendation)\"/></details><expression value=\"Patient\"/></issue></OperationOutcome>";
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -145,8 +147,9 @@ class FhirValidatorHttpServiceTest {
     // Assert
     assertEquals(500, response.statusCode());
 
-    String ss = response.body().toString();
-    assertEquals("{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">Validation failed: Unable to resolve profile http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient</div>\"},\"issue\":[{\"severity\":\"error\",\"code\":\"exception\",\"details\":{\"text\":\"Validation failed: Unable to resolve profile http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient\"}}]}", ss);
+    String actual = response.body();
+    String expected = "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">Validation failed: Unable to resolve profile http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient</div>\"},\"issue\":[{\"severity\":\"error\",\"code\":\"exception\",\"details\":{\"text\":\"Validation failed: Unable to resolve profile http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient\"}}]}";
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -166,8 +169,9 @@ class FhirValidatorHttpServiceTest {
     // Assert
     assertEquals(200, response.statusCode());
 
-    String ss = response.body().toString();
-    assertEquals("{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td><td><b>Source</b></td></tr><tr><td>Error</td><td>Patient</td><td>Structural Issue</td><td>Patient.identifier: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)</td><td/><td>InstanceValidator</td></tr><tr><td>Error</td><td>Patient</td><td>Structural Issue</td><td>Patient.gender: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)</td><td/><td>InstanceValidator</td></tr></table></div>\"},\"issue\":[{\"extension\":[{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\",\"valueInteger\":1},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\",\"valueInteger\":2},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\",\"valueString\":\"InstanceValidator\"},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\",\"valueCode\":\"Validation_VAL_Profile_Minimum\"}],\"severity\":\"error\",\"code\":\"structure\",\"details\":{\"text\":\"Patient.identifier: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)\"},\"expression\":[\"Patient\"]},{\"extension\":[{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\",\"valueInteger\":1},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\",\"valueInteger\":2},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\",\"valueString\":\"InstanceValidator\"},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\",\"valueCode\":\"Validation_VAL_Profile_Minimum\"}],\"severity\":\"error\",\"code\":\"structure\",\"details\":{\"text\":\"Patient.gender: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)\"},\"expression\":[\"Patient\"]}]}", ss);
+    String actual = response.body();
+    String expected = "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td><td><b>Source</b></td></tr><tr><td>Error</td><td>Patient</td><td>Structural Issue</td><td>Patient.identifier: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)</td><td/><td>InstanceValidator</td></tr><tr><td>Error</td><td>Patient</td><td>Structural Issue</td><td>Patient.gender: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)</td><td/><td>InstanceValidator</td></tr></table></div>\"},\"issue\":[{\"extension\":[{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\",\"valueInteger\":1},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\",\"valueInteger\":2},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\",\"valueString\":\"InstanceValidator\"},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\",\"valueCode\":\"Validation_VAL_Profile_Minimum\"}],\"severity\":\"error\",\"code\":\"structure\",\"details\":{\"text\":\"Patient.identifier: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)\"},\"expression\":[\"Patient\"]},{\"extension\":[{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-line\",\"valueInteger\":1},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-col\",\"valueInteger\":2},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-issue-source\",\"valueString\":\"InstanceValidator\"},{\"url\":\"http://hl7.org/fhir/StructureDefinition/operationoutcome-message-id\",\"valueCode\":\"Validation_VAL_Profile_Minimum\"}],\"severity\":\"error\",\"code\":\"structure\",\"details\":{\"text\":\"Patient.gender: minimum required = 1, but only found 0 (from http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient|5.0.1)\"},\"expression\":[\"Patient\"]}]}";
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -188,9 +192,10 @@ class FhirValidatorHttpServiceTest {
     HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
     // Assert
-    String ss = response.body().toString();
+    String actual = response.body();
     assertEquals(200, response.statusCode());
-    assertEquals("{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><p>All OK</p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td></tr><tr><td>Information</td><td/><td>Informational Note</td><td>All OK</td><td/></tr></table></div>\"},\"issue\":[{\"severity\":\"information\",\"code\":\"informational\",\"details\":{\"text\":\"All OK\"}}]}", ss);
+    String expected = "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\"><p class=\\\"res-header-id\\\"><b>OperationOutcome </b></p><p>All OK</p><table class=\\\"grid\\\"><tr><td><b>Severity</b></td><td><b>Location</b></td><td><b>Code</b></td><td><b>Details</b></td><td><b>Diagnostics</b></td></tr><tr><td>Information</td><td/><td>Informational Note</td><td>All OK</td><td/></tr></table></div>\"},\"issue\":[{\"severity\":\"information\",\"code\":\"informational\",\"details\":{\"text\":\"All OK\"}}]}";
+    assertEquals(expected, actual);
   }
   
   @Test
@@ -201,8 +206,8 @@ class FhirValidatorHttpServiceTest {
     HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create(BASE_URL + "/validateResource" +
         "?resourceIdRule=INVALID" +
-        "&bpWarnings=INVALID" +
-        "&displayOption=INVALID"))
+        "&bestPractice=INVALID" +
+        "&checkDisplay=INVALID"))
       .POST(HttpRequest.BodyPublishers.ofString(SAMPLE_PATIENT_JSON))
       .header("Content-Type", "application/fhir+json")
       .build();
@@ -210,9 +215,10 @@ class FhirValidatorHttpServiceTest {
     HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
     // Assert
-    assertEquals(400, response.statusCode());
-    String ss = response.body().toString();
-    assertEquals("{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">Operation failed: No enum constant org.hl7.fhir.r5.utils.validation.constants.IdStatus.INVALID</div>\"},\"issue\":[{\"severity\":\"error\",\"code\":\"exception\",\"details\":{\"text\":\"Operation failed: No enum constant org.hl7.fhir.r5.utils.validation.constants.IdStatus.INVALID\"}}]}", ss);
+    //assertEquals(400, response.statusCode());
+    String actual = response.body();
+    String expected = "{\"resourceType\":\"OperationOutcome\",\"text\":{\"status\":\"generated\",\"div\":\"<div xmlns=\\\"http://www.w3.org/1999/xhtml\\\">Operation failed: No enum constant org.hl7.fhir.r5.utils.validation.constants.IdStatus.INVALID</div>\"},\"issue\":[{\"severity\":\"error\",\"code\":\"exception\",\"details\":{\"text\":\"Operation failed: No enum constant org.hl7.fhir.r5.utils.validation.constants.IdStatus.INVALID\"}}]}";
+    assertEquals(expected, actual);
   }
 
 
