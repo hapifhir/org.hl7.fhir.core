6fb63bf9e56ccb970af04bb8013a6b77d1b4d908
Grahame Grieve <grahameg@gmail.ccom>
2026-03-13 12:44:56 +1100

    fix bug where naming system resolution was a little random

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/terminologies/NamingSystemUtilities.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/terminologies/NamingSystemUtilities.java
@@ -4,6 +4,7 @@ import org.hl7.fhir.r5.context.IWorkerContext;
 import org.hl7.fhir.r5.context.SimpleWorkerContext;
 import org.hl7.fhir.r5.model.NamingSystem;
 import org.hl7.fhir.r5.renderers.utils.RenderingContext;
+import org.hl7.fhir.r5.utils.ResourceSorters;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +16,7 @@ public class NamingSystemUtilities {
     if (result == null) {
       result = new HashMap<>();
       List<NamingSystem> nsl = context.fetchResourcesByType(NamingSystem.class);
+      nsl.sort(new ResourceSorters.CanonicalResourceSortByTypeId());
       for (NamingSystem ns : nsl) {
         for (NamingSystem.NamingSystemUniqueIdComponent uid : ns.getUniqueId()) {
           if (uid.getType() == NamingSystem.NamingSystemIdentifierType.URI && uid.hasValue()) {
