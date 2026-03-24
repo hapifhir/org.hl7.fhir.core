10dc613932fff7fdabd4723b8eebd3b5e443576d
dotasek <david.otasek@smilecdr.com>
2026-03-24 10:20:13 -0400

    Cleanup

--- org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/PackageServerHTTPAuthProviderTests.java
+++ org.hl7.fhir.utilities/src/test/java/org/hl7/fhir/utilities/npm/PackageServerHTTPAuthProviderTests.java
@@ -1,7 +1,6 @@
 package org.hl7.fhir.utilities.npm;
 
 import org.hl7.fhir.utilities.http.HTTPAuthenticationMode;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
