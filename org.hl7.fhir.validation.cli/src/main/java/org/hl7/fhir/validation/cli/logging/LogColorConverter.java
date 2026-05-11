d67af6e58a9e41e06937f1c7bf54b21a2fce0ddb
Grahame Grieve <grahameg@gmail.com>
2026-05-11 16:29:35 +0100

    fix LogColorConverted

--- org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/logging/LogColorConverter.java
+++ org.hl7.fhir.validation.cli/src/main/java/org/hl7/fhir/validation/cli/logging/LogColorConverter.java
@@ -8,25 +8,17 @@ import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
 public class LogColorConverter  extends ForegroundCompositeConverterBase<ILoggingEvent> {
 
   @Override
-  public String convert(ILoggingEvent event) {
-    if (System.console() == null) {
-      return event.getFormattedMessage();
-    }
-    return super.convert(event);
-  }
-
-  @Override
   protected String getForegroundColorCode(ILoggingEvent event) {
-      Level level = event.getLevel();
-      switch (level.toInt()) {
-        case Level.ERROR_INT:
-          return ANSIConstants.BOLD + ANSIConstants.RED_FG; // same as default color scheme
-        case Level.WARN_INT:
-          return ANSIConstants.YELLOW_FG;
-        case Level.INFO_INT:
-          return ANSIConstants.DEFAULT_FG;
-        default:
-          return ANSIConstants.DEFAULT_FG;
-      }
+    Level level = event.getLevel();
+    switch (level.toInt()) {
+      case Level.ERROR_INT:
+        return ANSIConstants.BOLD + ANSIConstants.RED_FG; // same as default color scheme
+      case Level.WARN_INT:
+        return ANSIConstants.YELLOW_FG;
+      case Level.INFO_INT:
+        return ANSIConstants.DEFAULT_FG;
+      default:
+        return ANSIConstants.DEFAULT_FG;
+    }
   }
 }
