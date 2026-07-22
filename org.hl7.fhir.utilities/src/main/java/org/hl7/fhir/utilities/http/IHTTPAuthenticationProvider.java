96723d790a834a329603ce2ab3010916295ceeda
dotasek <david.otasek@smilecdr.com>
2026-07-22 17:35:29 -0400

    More docs

--- org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/http/IHTTPAuthenticationProvider.java
+++ org.hl7.fhir.utilities/src/main/java/org/hl7/fhir/utilities/http/IHTTPAuthenticationProvider.java
@@ -8,15 +8,30 @@ import java.util.Map;
  */
 public interface IHTTPAuthenticationProvider {
 
+  /**
+   *
+   * @param url the url
+   * @return true if the protocol for the url is permitted
+   */
   public boolean isProtocolAllowed(URL url);
 
+  /**
+   *
+   * @param url the url
+   * @return true if this provider can provide headers (generally used for authentication) for the url
+   */
   public boolean canProvideHeaders(URL url);
 
+  /**
+   *
+   * @param url the url
+   * @return the appropriate headers to be passed to the url
+   */
   public Map<String, String> getHeaders(URL url);
 
   /**
-   * Whether {@code url} is explicitly trusted to bypass SSRF protection (private-address,
-   * DNS-rebinding, and metadata-host checks), independent of {@link #isProtocolAllowed(URL)}.
+   * @return true if the {@code url} is explicitly trusted to access non-public urls (this skips private-address,
+   * DNS-rebinding, and metadata-host checks). It is independent of {@link #isProtocolAllowed(URL)}.
    */
   public boolean isPrivateNetworkAllowed(URL url);
 
